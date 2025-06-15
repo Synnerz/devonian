110ece29679bfa62e9e41be16372be57ac5dfa5b
Doc <65479309+DocilElm@users.noreply.github.com>
2025-06-15 10:25:07 -0700

    ClientPlayerEntityMixin: remove null check

--- src/main/java/com/github/synnerz/devonian/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/ClientPlayerEntityMixin.java
@@ -14,7 +14,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ClientPlayerEntityMixin {
     @Inject(method = "dropSelectedItem", at = @At("HEAD"), cancellable = true)
     private void devonian$dropSelectedItem(boolean entireStack, CallbackInfoReturnable<Boolean> cir) {
-        assert MinecraftClient.getInstance().player != null;
         ItemStack stack = MinecraftClient.getInstance().player.getStackInHand(Hand.MAIN_HAND);
         if (stack != null && !stack.isEmpty()) {
             Events.DROP_HAND_ITEM.invoker().onDrop(stack, entireStack, cir);
