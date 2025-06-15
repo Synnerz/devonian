e4c89b6268d992db3d6f7c3e05024349651e0adf
Doc <65479309+DocilElm@users.noreply.github.com>
2025-06-15 10:24:09 -0700

    internal: add command wrappers

---
+++ src/main/kotlin/com/github/synnerz/devonian/utils/BaseCommand.kt
@@ -0,0 +1,58 @@
+package com.github.synnerz.devonian.utils
+
+import com.mojang.brigadier.context.CommandContext
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
+
+// TODO: add args to this api (?)
+
+class BaseCommand(val name: String, val cb: (CommandContext<FabricClientCommandSource>) -> Int) {
+    var subcommands = mutableListOf<BaseSubCommand>()
+
+    /**
+     * - Creates the command with all the sub commands combined, together and then proceeds
+     *  to register the command
+     */
+    fun register() {
+        val command = literal(name).executes(cb)
+        for (subcommand in subcommands) {
+            command.then(subcommand.command())
+        }
+
+        ClientCommandRegistrationCallback.EVENT.register { dispatcher, _ ->
+            dispatcher.register(command)
+        }
+    }
+
+    /**
+     * - Adds a sub command that is then returned as a value from this method call
+     * @param name The name of the sub command (or rather the sub command itself)
+     * @param cb The callback that will trigger whenever this command is ran
+     */
+    fun subcommand(name: String, cb: (CommandContext<FabricClientCommandSource>, List<Any>) -> Int): BaseSubCommand {
+        val subcommand = BaseSubCommand(name, cb)
+
+        subcommands.add(subcommand)
+
+        return subcommand
+    }
+
+    /**
+     * - Adds a sub command that is then returned as a value from this method call
+     * @param name The name of the sub command (or rather the sub command itself)
+     * @param isOptional Whether the arguments for this sub command are optional or not.
+     * By default, it's set to `false` which heavily requires each argument to have been passed through
+     * otherwise it will not trigger.
+     * If however, this is set to `true` it will avoid having to require each of the arguments and
+     * trigger even if one of them is not filled in.
+     * @param cb The callback that will trigger whenever this command is ran
+     */
+    fun subcommand(name: String, isOptional: Boolean, cb: (CommandContext<FabricClientCommandSource>, List<Any>) -> Int): BaseSubCommand {
+        val subcommand = BaseSubCommand(name, cb)
+        subcommand.isOptional = isOptional
+        subcommands.add(subcommand)
+
+        return subcommand
+    }
+}
---
+++ src/main/kotlin/com/github/synnerz/devonian/utils/BaseSubCommand.kt
@@ -0,0 +1,140 @@
+package com.github.synnerz.devonian.utils
+
+import com.mojang.brigadier.arguments.ArgumentType
+import com.mojang.brigadier.arguments.BoolArgumentType
+import com.mojang.brigadier.arguments.DoubleArgumentType
+import com.mojang.brigadier.arguments.FloatArgumentType
+import com.mojang.brigadier.arguments.IntegerArgumentType
+import com.mojang.brigadier.arguments.LongArgumentType
+import com.mojang.brigadier.arguments.StringArgumentType
+import com.mojang.brigadier.builder.ArgumentBuilder
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.mojang.brigadier.context.CommandContext
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
+
+class BaseSubCommand(val name: String, val cb: (CommandContext<FabricClientCommandSource>, List<Any>) -> Int) {
+    val commandArgs = mutableListOf<Pair<String, ArgumentType<*>>>()
+    var isOptional = false
+
+    /**
+     * - Gets this [BaseSubCommand] entire command chain and combines it all together into a single command builder
+     */
+    fun command(): LiteralArgumentBuilder<FabricClientCommandSource>? {
+        val base = literal(name)
+
+        var current: ArgumentBuilder<FabricClientCommandSource, *>? = null
+
+        if (isOptional)
+            base.executes { ctx -> cb(ctx, emptyList()) }
+
+        for ((argName, argType) in commandArgs.asReversed()) {
+            val argBuilder = argument(argName, argType)
+
+            if (current == null) {
+                argBuilder.executes { ctx ->
+                    val args = commandArgs.map { (key, type) -> getArgument(ctx, key, type) }
+                    cb(ctx, args)
+                }
+            } else {
+                argBuilder.then(current)
+            }
+
+            current = argBuilder
+        }
+
+        return if (current != null) base.then(current) else base.executes { ctx -> cb(ctx, emptyList()) }
+    }
+
+    /**
+     * - Adds the specified argument type with its argument name
+     * @param name The name of the argument
+     * @Param type The type of the argument
+     */
+    fun add(name: String, type: ArgumentType<*>) = apply {
+        commandArgs.add(name to type)
+    }
+
+    /**
+     * - Sets a string argument with the specified name
+     * @param name The name of the argument
+     */
+    fun string(name: String) = apply {
+        add(name, StringArgumentType.string())
+    }
+
+    /**
+     * - Sets a greedy string argument with the specified name
+     * - Note: greedy means it'll take all the strings no matter whether there are spaces
+     * @param name The name of the argument
+     */
+    fun greedyString(name: String) = apply {
+        add(name, StringArgumentType.greedyString())
+    }
+
+    /**
+     * - Sets a word argument with the specified name
+     * - Note: a "word" argument is essentially just the "string" argument
+     *  without the string being quotable
+     *  @param name The name of the argument
+     */
+    fun word(name: String) = apply {
+        add(name, StringArgumentType.word())
+    }
+
+    /**
+     * - Sets a boolean argument with the specified name
+     * @param name The name of the argument
+     */
+    fun bool(name: String) = apply {
+        add(name, BoolArgumentType.bool())
+    }
+
+    /**
+     * - Sets a double argument with the specified name and min/max (defaults to min/max values of each)
+     * @param name The name of the argument
+     */
+    @JvmOverloads
+    fun double(name: String, min: Double = Double.MIN_VALUE, max: Double = Double.MAX_VALUE) = apply {
+        add(name, DoubleArgumentType.doubleArg(min, max))
+    }
+
+    /**
+     * - Sets a float argument with the specified name and min/max (defaults to min/max values of each)
+     * @param name The name of the argument
+     */
+    @JvmOverloads
+    fun float(name: String, min: Float = Float.MIN_VALUE, max: Float = Float.MAX_VALUE) = apply {
+        add(name, FloatArgumentType.floatArg(min, max))
+    }
+
+    /**
+     * - Sets an integer argument with the specified name and min/max (defaults to min/max values of each)
+     * @param name The name of the argument
+     */
+    @JvmOverloads
+    fun integer(name: String, min: Int = Int.MIN_VALUE, max: Int = Int.MAX_VALUE) = apply {
+        add(name, IntegerArgumentType.integer(min, max))
+    }
+
+    /**
+     * - Sets a long argument with the specified name and min/max (defaults to min/max values of each)
+     * @param name The name of the argument
+     */
+    @JvmOverloads
+    fun long(name: String, min: Long = Long.MIN_VALUE, max: Long = Long.MAX_VALUE) = apply {
+        add(name, LongArgumentType.longArg(min, max))
+    }
+
+    private fun getArgument(ctx: CommandContext<FabricClientCommandSource>, name: String, type: ArgumentType<*>): Any {
+        return when (type) {
+            is StringArgumentType -> StringArgumentType.getString(ctx, name)
+            is BoolArgumentType -> BoolArgumentType.getBool(ctx, name)
+            is DoubleArgumentType -> DoubleArgumentType.getDouble(ctx, name)
+            is FloatArgumentType -> FloatArgumentType.getFloat(ctx, name)
+            is LongArgumentType -> LongArgumentType.getLong(ctx, name)
+            else -> IntegerArgumentType.getInteger(ctx, name)
+        }
+    }
+}
