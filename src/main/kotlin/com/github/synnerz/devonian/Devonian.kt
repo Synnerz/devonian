9133b2b8b48b554fd1663ec74092b9c0eca65ba8
Doc <65479309+DocilElm@users.noreply.github.com>
2025-06-15 10:26:09 -0700

    add `Config` & load `JsonUtils`

--- src/main/kotlin/com/github/synnerz/devonian/Devonian.kt
+++ src/main/kotlin/com/github/synnerz/devonian/Devonian.kt
@@ -1,8 +1,13 @@
 package com.github.synnerz.devonian
 
+import com.github.synnerz.devonian.config.Config
+import com.github.synnerz.devonian.features.Feature
 import com.github.synnerz.devonian.features.dungeons.BoxStarMob
 import com.github.synnerz.devonian.features.misc.NoCursorReset
+import com.github.synnerz.devonian.features.misc.RemoveFallingBlocks
+import com.github.synnerz.devonian.features.misc.RemoveFireOverlay
 import com.github.synnerz.devonian.features.misc.RemoveParticles
+import com.github.synnerz.devonian.utils.JsonUtils
 import net.fabricmc.api.ClientModInitializer
 import net.minecraft.client.MinecraftClient
 import org.slf4j.LoggerFactory
@@ -10,10 +15,15 @@ import org.slf4j.LoggerFactory
 object Devonian : ClientModInitializer {
     private val logger = LoggerFactory.getLogger("devonian")
 	val minecraft = MinecraftClient.getInstance()
+	val features = mutableListOf<Feature>()
 
 	override fun onInitializeClient() {
 		NoCursorReset.initialize()
 		BoxStarMob.initialize()
 		RemoveParticles.initialize()
+		RemoveFallingBlocks.initialize()
+		RemoveFireOverlay.initialize()
+		JsonUtils.load()
+		Config.initialize()
 	}
 }
---
+++ src/main/kotlin/com/github/synnerz/devonian/config/Config.kt
@@ -0,0 +1,88 @@
+package com.github.synnerz.devonian.config
+
+import com.github.synnerz.devonian.Devonian.features
+import com.github.synnerz.devonian.features.Feature
+import com.github.synnerz.devonian.utils.BaseCommand
+import com.github.synnerz.devonian.utils.ChatUtils
+import net.minecraft.text.ClickEvent
+import net.minecraft.text.MutableText
+import net.minecraft.text.Style
+
+object Config {
+    private val featuresSize get() = features.size
+    private val minPage = 0
+    private val maxPage get() = featuresSize / 10
+    private var currentPage = 0
+
+    fun initialize() {
+        val baseCommand = BaseCommand("devonian") {
+            ChatUtils.sendMessage("&7Settings", true)
+            for (feature in currentFeatures()) {
+                feature.displayChat()
+            }
+            ChatUtils.sendMessageWithId(pageLiteral(), 100)
+            1
+        }
+
+        baseCommand.subcommand("config", true) { _, args ->
+            if (args.isEmpty()) {
+                ChatUtils.sendMessage("&7Settings", true)
+                for (feature in currentFeatures()) {
+                    feature.displayChat()
+                }
+                return@subcommand 1
+            }
+
+            val id = args.first() as Int
+
+            if (id < 256652) {
+                if (id > maxPage - 1 || id < minPage) return@subcommand 1
+                for (feature in currentFeatures())
+                    feature.deleteChat()
+
+                if (id < currentPage) currentPage--.coerceAtLeast(minPage)
+                else if (id != currentPage) currentPage++.coerceAtMost(maxPage - 1)
+
+                for (feature in currentFeatures())
+                    feature.displayChat()
+
+                ChatUtils.removeLines { ChatUtils.chatLineIds[it] == 100 }
+                ChatUtils.sendMessageWithId(pageLiteral(), 100)
+                return@subcommand 1
+            }
+
+            for (feature in currentFeatures()) {
+                feature.onCommand(id)
+            }
+            1
+        }.integer("\$configid")
+
+        baseCommand.register()
+    }
+
+    private fun currentFeatures(): MutableList<Feature> {
+        val list = mutableListOf<Feature>()
+
+        for (n in 0..10) {
+            val idx = n + (currentPage * 10)
+            if (featuresSize < idx + 1) break
+            list.add(features[idx])
+        }
+
+        return list
+    }
+
+    private fun pageLiteral(): MutableText {
+        val previousPage = ChatUtils.literal("&7<<  &f${currentPage + 1}&f/")
+            .setStyle(Style.EMPTY.withClickEvent(
+                ClickEvent.RunCommand("devonian config ${(currentPage - 1).coerceAtLeast(minPage)}"))
+            )
+        val nextPage = ChatUtils.literal("&b${maxPage + 1}  &f>>")
+            .setStyle(
+                Style.EMPTY.withClickEvent(
+                    ClickEvent.RunCommand("devonian config ${(currentPage + 1).coerceAtMost(maxPage - 1)}"))
+            )
+
+        return ChatUtils.literal("   ").append(previousPage).append(nextPage)
+    }
+}
