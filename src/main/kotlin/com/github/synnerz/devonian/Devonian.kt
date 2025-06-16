5d23568905d64cb71df85fdf39c628eff2413eab
Doc <65479309+DocilElm@users.noreply.github.com>
2025-06-16 16:23:15 -0700

    Feature: handle feature's world requirement

--- src/main/kotlin/com/github/synnerz/devonian/Devonian.kt
+++ src/main/kotlin/com/github/synnerz/devonian/Devonian.kt
@@ -1,10 +1,14 @@
 package com.github.synnerz.devonian
 
 import com.github.synnerz.devonian.config.Config
+import com.github.synnerz.devonian.events.AreaEvent
+import com.github.synnerz.devonian.events.EventBus
+import com.github.synnerz.devonian.events.SubAreaEvent
 import com.github.synnerz.devonian.features.Feature
 import com.github.synnerz.devonian.features.dungeons.BoxStarMob
 import com.github.synnerz.devonian.features.misc.*
 import com.github.synnerz.devonian.utils.JsonUtils
+import com.github.synnerz.devonian.utils.Location
 import net.fabricmc.api.ClientModInitializer
 import net.minecraft.client.MinecraftClient
 import org.slf4j.LoggerFactory
@@ -23,5 +27,16 @@ object Devonian : ClientModInitializer {
 		RemoveFireOverlay.initialize()
 		JsonUtils.load()
 		Config.initialize()
+		Location.initialize()
+
+		EventBus.on<AreaEvent> {
+			for (feat in features)
+				feat.onToggle(feat.isEnabled())
+		}
+
+		EventBus.on<SubAreaEvent> {
+			for (feat in features)
+				feat.onToggle(feat.isEnabled())
+		}
 	}
 }
--- src/main/kotlin/com/github/synnerz/devonian/features/Feature.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/Feature.kt
@@ -5,18 +5,20 @@ import com.github.synnerz.devonian.events.Event
 import com.github.synnerz.devonian.events.EventBus
 import com.github.synnerz.devonian.utils.ChatUtils
 import com.github.synnerz.devonian.utils.JsonUtils
+import com.github.synnerz.devonian.utils.Location
 import net.minecraft.text.ClickEvent
 import net.minecraft.text.Style
-import net.minecraft.text.Text
 
-// TODO: make it able to unregister events and re-register
-//  as well as making location based registry
-
-open class Feature(val configName: String) {
+open class Feature @JvmOverloads constructor(
+    val configName: String,
+    val area: String? = null,
+    val subarea: String? = null
+) {
     val minecraft = Devonian.minecraft
     val id = 256652 + Devonian.features.size
     private val style = Style.EMPTY.withClickEvent(ClickEvent.RunCommand("devonian config $id"))
     private var displayed = false
+    private var isRegistered = false
     val events = mutableListOf<EventBus.Dcall>()
 
     init {
@@ -76,18 +78,44 @@ open class Feature(val configName: String) {
         if (!state) {
             for (event in events)
                 event.remove()
+            isRegistered = false
+            return
+        }
+        if (!inEnvironment()) {
+            if (isRegistered)
+                for (event in events)
+                    event.remove()
+            isRegistered = false
             return
         }
 
-        for (event in events)
-            event.add()
+        if (!isRegistered) {
+            for (event in events)
+                event.add()
+        }
+        isRegistered = true
+    }
+
+    fun inArea(): Boolean {
+        if (area == null) return true
+
+        return Location.area == area.lowercase()
+    }
+
+    fun inSubarea(): Boolean {
+        if (subarea == null) return true
+
+        return Location.subarea?.contains(subarea.lowercase()) ?: false
+    }
+
+    fun inEnvironment(): Boolean {
+        if (!inSubarea()) return false
+        if (!inArea()) return false
+
+        return true
     }
 
     inline fun <reified T : Event> on(noinline cb: (T) -> Unit) {
         events.add(EventBus.on<T>(cb, false))
-    }
-
-    fun sendMessage(msg: String) {
-        minecraft.player?.sendMessage(Text.literal(msg), false)
     }
 }
