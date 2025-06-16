6d32279111c604cbaa8151c1eb097fa0933c79b1
Doc <65479309+DocilElm@users.noreply.github.com>
2025-06-16 00:29:35 -0700

    add `Scheduler`

---
+++ src/main/kotlin/com/github/synnerz/devonian/utils/Scheduler.kt
@@ -0,0 +1,43 @@
+package com.github.synnerz.devonian.utils
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.minecraft.client.MinecraftClient
+import net.minecraft.entity.Entity
+
+object Scheduler {
+    private val tasks = mutableListOf<Task>()
+    data class Task(var delay: Int, val cb: () -> Unit)
+
+    init {
+        ClientTickEvents.START_CLIENT_TICK.register {
+            synchronized(tasks) {
+                tasks.removeAll {
+                    if (it.delay-- > 0) return@removeAll false
+
+                    MinecraftClient.getInstance().submit(it.cb)
+
+                    return@removeAll true
+                }
+            }
+        }
+    }
+
+    fun scheduleTask(delay: Int, cb: () -> Unit) {
+        synchronized(tasks) {
+            tasks.add(Task(delay, cb))
+        }
+    }
+
+    @JvmOverloads
+    fun scheduleStandName(entity: Entity, cb: () -> Unit, depth: Int = 0) {
+        if (depth > 10) return
+        scheduleTask(2) {
+            if (entity.name.string !== "Armor Stand") {
+                cb()
+                return@scheduleTask
+            }
+
+            scheduleStandName(entity, cb, depth + 1)
+        }
+    }
+}
