adbc6addfef42a4e28378bc41a9c2c89b89897de
Doc <65479309+DocilElm@users.noreply.github.com>
2025-06-18 09:54:20 -0700

    internal: add `ScreenUtils`

---
+++ src/main/kotlin/com/github/synnerz/devonian/utils/ScreenUtils.kt
@@ -0,0 +1,30 @@
+package com.github.synnerz.devonian.utils
+
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.gui.screen.ingame.HandledScreen
+import net.minecraft.screen.slot.SlotActionType
+
+object ScreenUtils {
+    @JvmOverloads
+    fun click(slot: Int, shift: Boolean = false, button: String = "LEFT") {
+        val minecraft = MinecraftClient.getInstance()
+        val screen = minecraft.currentScreen ?: return
+        val windowId = (screen as HandledScreen<*>).screenHandler?.syncId ?: return
+        val clickMode = when {
+            button == "MIDDLE" -> SlotActionType.CLONE
+            shift -> SlotActionType.QUICK_MOVE
+            else -> SlotActionType.PICKUP
+        }
+        val clickButton = when (button) {
+            "LEFT" -> 0
+            "RIGHT" -> 1
+            "MIDDLE" -> 2
+            else -> 0
+        }
+
+        minecraft.interactionManager?.clickSlot(
+            windowId,
+            slot, clickButton, clickMode, minecraft.player
+        )
+    }
+}
