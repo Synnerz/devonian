f4190cba744c794fadda7ebe62cac1f0c31bfda8
Doc <65479309+DocilElm@users.noreply.github.com>
2025-06-23 22:24:07 -0700

    HideInventoryEffects: change injection point

--- src/main/java/com/github/synnerz/devonian/mixin/StatusEffectsDisplayMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/StatusEffectsDisplayMixin.java
@@ -1,21 +1,22 @@
 package com.github.synnerz.devonian.mixin;
 
 import com.github.synnerz.devonian.features.misc.HideInventoryEffects;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.StatusEffectsDisplay;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(StatusEffectsDisplay.class)
 public class StatusEffectsDisplayMixin {
     @Inject(
-            method = "shouldHideStatusEffectHud",
+            method = "drawStatusEffects(Lnet/minecraft/client/gui/DrawContext;II)V",
             at = @At("HEAD"),
             cancellable = true
     )
-    private void devonian$onInventoryEffects(CallbackInfoReturnable<Boolean> cir) {
+    private void devonian$onInventoryEffects(DrawContext context, int mouseX, int mouseY, CallbackInfo ci) {
         if (!HideInventoryEffects.INSTANCE.isEnabled()) return;
-        cir.setReturnValue(true);
+        ci.cancel();
     }
 }
