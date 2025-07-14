764293348ffb4af0a042b03389500c167b536bc5
Doc <65479309+DocilElm@users.noreply.github.com>
2025-07-13 20:16:21 -0700

    internal: add `Render2D`

---
+++ src/main/kotlin/com/github/synnerz/devonian/utils/render/Render2D.kt
@@ -0,0 +1,51 @@
+package com.github.synnerz.devonian.utils.render
+
+import com.github.synnerz.devonian.Devonian
+import net.minecraft.client.gui.DrawContext
+import net.minecraft.util.Formatting
+import kotlin.math.max
+
+object Render2D {
+    private val formattingRegex = "(?<!\\\\\\\\)&(?=[0-9a-fk-or])".toRegex()
+    val textRenderer = Devonian.minecraft.textRenderer
+    val window get() = Devonian.minecraft.window
+    val mouse = Devonian.minecraft.mouse
+    val screenWidth get() = window.width
+    val screenHeight get() = window.height
+    val scaledWidth get() = window.scaledWidth
+    val scaledHeight get() = window.scaledHeight
+
+    @JvmOverloads
+    fun drawString(ctx: DrawContext, str: String, x: Int, y: Int, scale: Float = 1f, shadow: Boolean = true) {
+        val matrices = ctx.matrices
+        if (scale != 1f) {
+            matrices.push()
+            matrices.scale(scale, scale, 1f)
+        }
+
+        ctx.drawText(
+            textRenderer,
+            str.replace(formattingRegex, "${Formatting.FORMATTING_CODE_PREFIX}"),
+            x,
+            y,
+            -1,
+            shadow
+        )
+
+        if (scale != 1f) matrices.pop()
+    }
+
+    fun String.width(): Int = textRenderer.getWidth(this)
+
+    fun String.height(): Int {
+        val newlines = this.split("\n")
+        if (newlines.size <= 1) return textRenderer.fontHeight
+
+        return textRenderer.fontHeight * newlines.size
+    }
+
+    object Mouse {
+        val x get() = mouse.x * scaledWidth / max(1, screenWidth)
+        val y get() = mouse.y * scaledHeight / max(1, screenHeight)
+    }
+}
