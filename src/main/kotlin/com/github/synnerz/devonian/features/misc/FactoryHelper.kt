a79a81098605e8421d17ff3c478ea1199d733660
Doc <65479309+DocilElm@users.noreply.github.com>
2025-07-23 12:44:03 -0700

    FactoryHelper: get proper choco production

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/FactoryHelper.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/FactoryHelper.kt
@@ -15,7 +15,9 @@ object FactoryHelper : Feature("factoryHelper") {
     private val nextCPSRegex = "^  \\+([\\d.]+)x? Chocolate per second$".toRegex()
     private val currentCPSRegex = "\\+([\\d.]+)x? Chocolate per".toRegex()
     private val chocolateCostRegex = "^([\\d,]+) Chocolate$".toRegex()
-    private val currentProductionRegex = "^([\\d,.]+) per second".toRegex()
+    private val baseChocoRegex = "^Base Chocolate: ([\\d,.]+) per second$".toRegex()
+    private val totalMultiplierRegex = "^Total Multiplier: ([\\d,.]+)x$".toRegex()
+    private val timeTowerMultiRegex = "^ *\\+([\\d.]+)x \\(Time Tower\\)\$".toRegex()
     private val stats = mutableMapOf<Int, RabbitStat>()
     private var inFactory = false
     private var chocolatePurse = 0.0
@@ -37,11 +39,16 @@ object FactoryHelper : Feature("factoryHelper") {
             if (slot > 54) return@on
 
             val itemStack = packet.stack
-            if (itemStack.item != Items.PLAYER_HEAD) return@on
+            if (itemStack.item != Items.PLAYER_HEAD && itemStack.item !== Items.COCOA_BEANS) return@on
             val name = itemStack.customName?.string ?: return@on
             val lore = ItemUtils.lore(itemStack) ?: return@on
 
-            if (name.endsWith("Chocolate")) return@on chocoPurse(name, lore)
+            if (name.endsWith("Chocolate")) {
+                chocolatePurse = chocolateCostRegex.matchEntire(name)?.groupValues?.get(1)?.replace(",", "")?.toDouble() ?: 0.0
+                findBest()
+                return@on
+            }
+            if (name == "Chocolate Production") return@on chocoProduction(lore)
             if (name.startsWith("Coach Jack")) return@on coachStats(slot, lore)
 
             if (!name.startsWith("Rabbit")) return@on
@@ -85,13 +92,30 @@ object FactoryHelper : Feature("factoryHelper") {
         bestSlot = best
     }
 
-    private fun chocoPurse(name: String, lore: List<String>) {
-        chocolatePurse = chocolateCostRegex.matchEntire(name)?.groupValues?.get(1)?.replace(",", "")?.toDouble() ?: 0.0
+    private fun chocoProduction(lore: List<String>) {
+        var timeTower = 0.0
+        var baseChoco = 0.0
+        var totalMultx = 0.0
+
         for (line in lore) {
-            val match = currentProductionRegex.matchEntire(line) ?: continue
-            currentProduction = match.groupValues[1].replace(",", "").toDouble()
+            val baseChocoMatch = baseChocoRegex.matchEntire(line)
+            if (baseChocoMatch != null) {
+                baseChoco = baseChocoMatch.groupValues[1].replace(",", "").toDouble()
+                continue
+            }
+
+            val totalMultxMatch = totalMultiplierRegex.matchEntire(line)
+            if (totalMultxMatch != null) {
+                totalMultx = totalMultxMatch.groupValues[1].replace(",", "").toDouble()
+                continue
+            }
+
+            val match = timeTowerMultiRegex.matchEntire(line) ?: continue
+            timeTower = match.groupValues[1].replace(",", "").toDouble()
         }
-        findBest()
+        if (timeTower != 0.0) totalMultx -= timeTower
+
+        currentProduction = baseChoco * totalMultx
     }
 
     private fun coachStats(slot: Int, lore: List<String>) {
@@ -103,8 +127,6 @@ object FactoryHelper : Feature("factoryHelper") {
         }
         if (cost == 0.0) return
 
-        // FIXME: this can possibly be use during Time Tower activation
-        //  in which case it might give a false Chocolate Production
         val currentProd = currentProduction * 0.01
         stats[slot] = RabbitStat((cost / currentProd).toInt(), cost)
     }
