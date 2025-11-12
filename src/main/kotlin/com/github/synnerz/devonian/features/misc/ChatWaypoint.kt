c1a60c3fc4f91b07b1c80709ef8c4a81a31c0dfe
Ch1ck3nNeedsRNG <chick_is_bored>
2025-11-12 18:32:09 -0500

    feat: change `ChatWaypoint` more permissive regex, specific chats, show username, and temporary settings

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/ChatWaypoint.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/ChatWaypoint.kt
@@ -2,6 +2,7 @@ package com.github.synnerz.devonian.features.misc
 
 import com.github.synnerz.barrl.Context
 import com.github.synnerz.devonian.api.ChatUtils
+import com.github.synnerz.devonian.api.events.ChatChannelEvent
 import com.github.synnerz.devonian.api.events.ChatEvent
 import com.github.synnerz.devonian.api.events.RenderWorldEvent
 import com.github.synnerz.devonian.api.events.WorldChangeEvent
@@ -10,25 +11,43 @@ import java.awt.Color
 import kotlin.math.sqrt
 
 object ChatWaypoint : Feature("chatWaypoint") {
-    private val chatRegex = "^(Co-op|Party)?(?: > )?(?:\\[\\d+] .? ?)?(?:\\[[^]]+] )?(\\w{1,16}): x: (-?\\d+), y: (-?\\d+), z: (-?\\d+) ?$".toRegex()
+    private const val SETTING_ALL_CHAT = true
+    private const val SETTING_OWN_WAYPOINTS = true
+    private const val SETTING_CLEAR_WAYPOINTS_SWAP_SERVER = true
+    private val SETTING_WAYPOINT_COLOR = Color.CYAN
+    private const val SETTING_SHOW_SENDER_NAME = true
+
+    private val coordRegex = "(?:\\s|^)(?:x: )?(-?\\d+)(?:, y:)? (-?\\d+)(?:, z:)? (-?\\d+)(?:\\s|\$)".toRegex()
     private val waypoints = mutableListOf<TimedWaypoint>()
 
     data class TimedWaypoint(val created: Long, val by: String, val x: Double, val y: Double, val z: Double)
 
-    override fun initialize() {
-        on<ChatEvent> { event ->
-            val matches = event.matches(chatRegex) ?: return@on
-            val ( type, username, x1, y1, z1 ) = matches
-            if (type.isEmpty()) return@on
-            val x = x1.toDouble()
-            val y = y1.toDouble()
-            val z = z1.toDouble()
+    fun tryAddWaypoint(name: String, msg: String) {
+        if (!SETTING_OWN_WAYPOINTS) return
 
-            waypoints.add(TimedWaypoint(System.currentTimeMillis(), username, x, y, z))
-            ChatUtils.sendMessage("&aSet waypoint at &b$x, $y, $z", true)
+        val matches = coordRegex.matchEntire(msg) ?: return
+        val (xs, ys, zs) = matches.groupValues.drop(1)
+        val x = xs.toDouble()
+        val y = ys.toDouble()
+        val z = zs.toDouble()
+
+        waypoints.add(TimedWaypoint(System.currentTimeMillis(), name, x, y, z))
+        ChatUtils.sendMessage("&aSet waypoint at &b$x, $y, $z", true)
+    }
+
+    override fun initialize() {
+        on<ChatChannelEvent.AllChatEvent> { event ->
+            if (!SETTING_ALL_CHAT) return@on
+            tryAddWaypoint(event.name, event.userMessage)
+        }
+        on<ChatChannelEvent.PartyChatEvent> { event ->
+            tryAddWaypoint(event.name, event.userMessage)
+        }
+        on<ChatChannelEvent.CoopChatEvent> { event ->
+            tryAddWaypoint(event.name, event.userMessage)
         }
 
-        on<WorldChangeEvent> { waypoints.clear() }
+        on<WorldChangeEvent> { if (SETTING_CLEAR_WAYPOINTS_SWAP_SERVER) waypoints.clear() }
 
         on<RenderWorldEvent> {
             waypoints.removeIf {
@@ -39,7 +58,24 @@ object ChatWaypoint : Feature("chatWaypoint") {
                 val distance = sqrt(dx * dx + dy * dy + dz * dz)
                 if (distance < 5) return@removeIf true
 
-                Context.Immediate?.renderWaypoint(it.x, it.y, it.z, Color.CYAN, title = "%.2fm".format(distance), increase = true, phase = true)
+                Context.Immediate?.renderWaypoint(
+                    it.x,
+                    it.y,
+                    it.z,
+                    SETTING_WAYPOINT_COLOR,
+                    title = "%.2fm".format(distance),
+                    increase = true,
+                    phase = true
+                )
+                if (SETTING_SHOW_SENDER_NAME) Context.Immediate?.renderString(
+                    it.by,
+                    it.x,
+                    it.y,
+                    it.z,
+                    backgroundBox = true,
+                    increase = true,
+                    phase = true
+                )
                 System.currentTimeMillis() - it.created > 60000
             }
         }
