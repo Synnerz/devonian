b0ba56c90040c397a4aeb85819201f948a2f3c21
Ch1ck3nNeedsRNG <chick_is_bored>
2025-11-12 20:59:08 -0500

    feat: `BlockOverlay` add temporary settings, and option for entity boxing

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/BlockOverlay.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/BlockOverlay.kt
@@ -12,24 +12,56 @@ import net.minecraft.world.EmptyBlockView
 import java.awt.Color
 
 object BlockOverlay : Feature("blockOverlay") {
-    private var color: Color = Color.WHITE
+    private const val SETTING_BOX_ENTITY = false;
+    private var SETTING_WIRE_COLOR = Color.WHITE;
+    private val SETTING_FILL_COLOR = Color(0)
 
     override fun initialize() {
         JsonUtils.set("blockOverlayColor", -1)
 
         DevonianCommand.command.subcommand("blockoverlay") { _, args ->
             if (args.isEmpty()) return@subcommand 0
-            color = ColorEnum.valueOf(args.first() as String).color
-            JsonUtils.set("blockOverlayColor", color.rgb)
+            SETTING_WIRE_COLOR = ColorEnum.valueOf(args.first() as String).color
+            JsonUtils.set("blockOverlayColor", SETTING_WIRE_COLOR.rgb)
             ChatUtils.sendMessage("&aSuccessfully set block overlay color to &6${args.first()}", true)
             1
         }.string("color").suggest("color", *ColorEnum.entries.map { it.name }.toTypedArray())
 
         JsonUtils.afterLoad {
-            color = Color(JsonUtils.get<Int>("blockOverlayColor") ?: -1, true)
+            SETTING_WIRE_COLOR = Color(JsonUtils.get<Int>("blockOverlayColor") ?: -1, true)
         }
 
         on<BlockOutlineEvent> { event ->
+            event.cancel()
+
+            val entity = event.blockContext.entity()
+            if (entity != null) {
+                if (SETTING_BOX_ENTITY) {
+                    val pos = entity.getLerpedPos(event.renderContext.tickCounter().getTickProgress(false))
+                    Context.Immediate?.renderBox(
+                        pos.x - entity.width * 0.5f,
+                        pos.y,
+                        pos.z - entity.width * 0.5,
+                        entity.width.toDouble(),
+                        entity.height.toDouble(),
+                        SETTING_WIRE_COLOR,
+                        phase = minecraft.options.perspective.isFirstPerson,
+                        translate = true
+                    )
+                    Context.Immediate?.renderFilledBox(
+                        pos.x - entity.width * 0.5f,
+                        pos.y,
+                        pos.z - entity.width * 0.5,
+                        entity.width.toDouble(),
+                        entity.height.toDouble(),
+                        SETTING_WIRE_COLOR,
+                        phase = minecraft.options.perspective.isFirstPerson,
+                        translate = true
+                    )
+                }
+                return@on
+            }
+
             val blockPos = event.blockContext.blockPos()
             val camera = minecraft.gameRenderer.camera
             val cam = camera.pos
@@ -42,13 +74,20 @@ object BlockOverlay : Feature("blockOverlay") {
                     ShapeContext.of(camera.focusedEntity)
                 )
 
-            event.cancel()
             Context.Immediate?.renderBoxShape(
                 blockShape,
                 blockPos.x - cam.x,
                 blockPos.y - cam.y,
                 blockPos.z - cam.z,
-                color,
+                SETTING_WIRE_COLOR,
+                minecraft.options.perspective.isFirstPerson
+            )
+            Context.Immediate?.renderFilledShape(
+                blockShape,
+                blockPos.x - cam.x,
+                blockPos.y - cam.y,
+                blockPos.z - cam.z,
+                SETTING_FILL_COLOR,
                 minecraft.options.perspective.isFirstPerson
             )
         }
