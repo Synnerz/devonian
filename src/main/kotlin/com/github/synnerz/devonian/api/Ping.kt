73da42043ee41c5724d158ca93a689fce4d79117
Ch1ck3nNeedsRNG <chick_is_bored>
2025-11-13 01:47:18 -0500

    "api" -me

--- src/main/kotlin/com/github/synnerz/devonian/api/Ping.kt
+++ src/main/kotlin/com/github/synnerz/devonian/api/Ping.kt
@@ -1,4 +1,4 @@
-package com.github.synnerz.devonian.features.misc
+package com.github.synnerz.devonian.api
 
 import com.github.synnerz.devonian.Devonian
 import com.github.synnerz.devonian.api.events.*
