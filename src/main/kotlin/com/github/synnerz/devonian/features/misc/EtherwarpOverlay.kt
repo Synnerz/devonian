f744f736342e780277357d9772f459a809221a6d
Ch1ck3nNeedsRNG <chick_is_bored>
2025-11-12 21:50:03 -0500

    feat: change `EtherwarpOverlay` make better, show fail reason + position, and disable when appropriate

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/EtherwarpOverlay.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/EtherwarpOverlay.kt
@@ -2,177 +2,200 @@ package com.github.synnerz.devonian.features.misc
 
 import com.github.synnerz.barrl.Context
 import com.github.synnerz.devonian.api.ChatUtils
-import com.github.synnerz.devonian.commands.DevonianCommand
-import com.github.synnerz.devonian.api.events.RenderWorldEvent
-import com.github.synnerz.devonian.features.Feature
-import com.github.synnerz.devonian.utils.ColorEnum
 import com.github.synnerz.devonian.api.ItemUtils
-import com.github.synnerz.devonian.utils.JsonUtils
-import net.minecraft.block.AbstractRailBlock
-import net.minecraft.block.AbstractSkullBlock
-import net.minecraft.block.BlockState
-import net.minecraft.block.Blocks
-import net.minecraft.block.ButtonBlock
-import net.minecraft.block.DyedCarpetBlock
-import net.minecraft.block.SaplingBlock
+import com.github.synnerz.devonian.api.events.RenderOverlayEvent
+import com.github.synnerz.devonian.api.events.RenderWorldEvent
+import com.github.synnerz.devonian.commands.DevonianCommand
+import com.github.synnerz.devonian.features.Feature
+import com.github.synnerz.devonian.hud.HudManager
+import com.github.synnerz.devonian.mixin.accessor.ClientPlayerEntityAccessor
+import com.github.synnerz.devonian.utils.*
 import net.minecraft.block.ShapeContext
 import net.minecraft.item.Items
 import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.hit.HitResult
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Vec3d
 import net.minecraft.world.EmptyBlockView
-import net.minecraft.world.RaycastContext
 import java.awt.Color
+import kotlin.math.hypot
 
 object EtherwarpOverlay : Feature("etherwarpOverlay") {
+    private var SETTING_ETHER_WIRE_COLOR = ColorEnum.WHITE.color
+    private var SETTING_ETHER_FILL_COLOR =
+        Color(SETTING_ETHER_WIRE_COLOR.red, SETTING_ETHER_WIRE_COLOR.green, SETTING_ETHER_WIRE_COLOR.blue, 80)
+    private val SETTING_ETHER_FAIL_WIRE_COLOR = ColorEnum.RED.color
+    private val SETTING_ETHER_FAIL_FILL_COLOR =
+        Color(
+            SETTING_ETHER_FAIL_WIRE_COLOR.red,
+            SETTING_ETHER_FAIL_WIRE_COLOR.green,
+            SETTING_ETHER_FAIL_WIRE_COLOR.blue,
+            80
+        )
+    private const val SETTING_ETHER_SHOW_FAIL_REASON = true
+    private const val SETTING_ETHER_USING_CANCEL_INTERACT = false
+    private const val SETTING_USE_SMOOTH_POSITION = true
+
     private val validWeapons = mutableListOf("ASPECT_OF_THE_END", "ASPECT_OF_THE_VOID", "ETHERWARP_CONDUIT")
-    private var color = ColorEnum.WHITE.color
-    private var secondary = Color(color.red, color.green, color.blue, 80)
+    private var failReason = ""
+
+    private val failReasonHud = HudManager.createHud("EtherwarpFailReasonDisplay", "&4Can't TP: No air above!")
 
     override fun initialize() {
         JsonUtils.set("etherwarpOverlayColor", -1)
 
         DevonianCommand.command.subcommand("etherwarpoverlay") { _, args ->
             if (args.isEmpty()) return@subcommand 0
-            color = ColorEnum.valueOf(args.first() as String).color
-            secondary = Color(color.red, color.green, color.blue, 80)
-            JsonUtils.set("etherwarpOverlayColor", color.rgb)
+            SETTING_ETHER_WIRE_COLOR = ColorEnum.valueOf(args.first() as String).color
+            SETTING_ETHER_FILL_COLOR =
+                Color(SETTING_ETHER_WIRE_COLOR.red, SETTING_ETHER_WIRE_COLOR.green, SETTING_ETHER_WIRE_COLOR.blue, 80)
+            JsonUtils.set("etherwarpOverlayColor", SETTING_ETHER_WIRE_COLOR.rgb)
             ChatUtils.sendMessage("&aSuccessfully set etherwarp overlay color to &6${args.first()}", true)
             1
         }.string("color").suggest("color", *ColorEnum.entries.map { it.name }.toTypedArray())
 
         JsonUtils.afterLoad {
-            color = Color(JsonUtils.get<Int>("etherwarpOverlayColor") ?: -1, true)
-            secondary = Color(color.red, color.green, color.blue, 80)
+            SETTING_ETHER_WIRE_COLOR = Color(JsonUtils.get<Int>("etherwarpOverlayColor") ?: -1, true)
+            SETTING_ETHER_FILL_COLOR =
+                Color(SETTING_ETHER_WIRE_COLOR.red, SETTING_ETHER_WIRE_COLOR.green, SETTING_ETHER_WIRE_COLOR.blue, 80)
         }
 
         on<RenderWorldEvent> { event ->
             val player = minecraft.player
             if (minecraft.world == null || player == null) return@on
+            val world = minecraft.world!!
+
+            failReason = ""
 
             val heldItem = player.getStackInHand(Hand.MAIN_HAND)
-            if (!(
-                        heldItem.item == Items.DIAMOND_SHOVEL ||
-                        heldItem.item == Items.DIAMOND_SWORD ||
-                        heldItem.item == Items.PLAYER_HEAD
-                    )) return@on
+            if (
+                heldItem.item != Items.DIAMOND_SHOVEL &&
+                heldItem.item != Items.DIAMOND_SWORD &&
+                heldItem.item != Items.PLAYER_HEAD
+            ) return@on
 
             val itemId = ItemUtils.skyblockId(heldItem) ?: return@on
             val requireSneak = heldItem.item == Items.DIAMOND_SHOVEL || heldItem.item == Items.DIAMOND_SWORD
 
             if (requireSneak && !player.isSneaking) return@on
-            if (!validWeapons.any { it == itemId }) return@on
+            if (!validWeapons.contains(itemId)) return@on
 
             val extraAttributes = ItemUtils.extraAttributes(heldItem) ?: return@on
             if (requireSneak && !extraAttributes.contains("ethermerge")) return@on
 
+            if (!SETTING_ETHER_USING_CANCEL_INTERACT) {
+                val target = minecraft.crosshairTarget
+                if (target != null && target.type == HitResult.Type.BLOCK) {
+                    val blockTarget = target as BlockHitResult
+                    if (BlockTypes.Interactable.contains(world.getBlockState(blockTarget.blockPos).block)) return@on
+                }
+            }
+
             val tunedTransmission = extraAttributes.get("tuned_transmission")
             val tunedInt = tunedTransmission?.asInt()
             val tuners = if (tunedInt == null || tunedInt.isEmpty) 0 else tunedInt.get()
+            val dist = 57 + tuners
 
-            val hitResult = raycast(57 + tuners) ?: return@on
-            val camera = minecraft.gameRenderer.camera ?: return@on
-            val cam = camera.pos
-            val blockPos = hitResult.blockPos
-            val posFoot = blockPos.up()
-            val posHead = blockPos.up(2)
+            val px: Double
+            val py: Double
+            val pz: Double
+            val lookVec: Vec3d
+            if (SETTING_USE_SMOOTH_POSITION) {
+                val posVec = player.getLerpedPos(event.ctx.tickCounter().getTickProgress(false))
+                px = posVec.x
+                py = posVec.y
+                pz = posVec.z
+                lookVec = player.getRotationVec(event.ctx.tickCounter().getTickProgress(false))
+            } else {
+                val playerAccessor = player as ClientPlayerEntityAccessor
+                px = playerAccessor.lastXClient
+                py = playerAccessor.lastYClient
+                pz = playerAccessor.lastZClient
+                lookVec = player.getRotationVector(playerAccessor.lastPitchClient, playerAccessor.lastYawClient)
+            }
 
-            val blockFoot = minecraft.world!!.getBlockState(posFoot)
-            val blockHead = minecraft.world!!.getBlockState(posHead)
-            if (!blockFoot.isValid() || !blockHead.isValid()) return@on
+            var hitResult = raycast(
+                px, py, pz,
+                lookVec.x * dist,
+                lookVec.y * dist,
+                lookVec.z * dist
+            )
 
-            val blockState = minecraft.world!!.getBlockState(blockPos)
-            if (blockState.isValid(true)) return@on
+            if (hitResult == null) {
+                failReason = "&4Can't TP: Too far!"
+                val maxDist = hypot(256.0, 16.0 * minecraft.options.clampedViewDistance)
+                hitResult = raycast(
+                    px + lookVec.x * dist,
+                    py + lookVec.y * dist,
+                    pz + lookVec.z * dist,
+                    px + lookVec.x * maxDist,
+                    py + lookVec.y * maxDist,
+                    pz + lookVec.z * maxDist,
+                )
+                if (hitResult == null) return@on
+            } else {
+                val bpFoot = hitResult.up(1)
+                val bpHead = hitResult.up(2)
 
-            val outlineShape = blockState.getOutlineShape(
+                val bsFoot = world.getBlockState(bpFoot)
+                val bsHead = world.getBlockState(bpHead)
+                if (
+                    !BlockTypes.AirLike.contains(bsFoot.block) ||
+                    !BlockTypes.AirLike.contains(bsHead.block)
+                ) failReason = "&4Can't TP: No air above!"
+            }
+
+            val camera = event.ctx.camera()
+            val cameraPos = camera.pos
+
+            val outlineShape = world.getBlockState(hitResult).getOutlineShape(
                 EmptyBlockView.INSTANCE,
-                blockPos,
+                hitResult,
                 ShapeContext.of(camera.focusedEntity)
             )
 
             Context.Immediate?.renderBoxShape(
                 outlineShape,
-                blockPos.x - cam.x,
-                blockPos.y - cam.y,
-                blockPos.z - cam.z,
-                color,
+                hitResult.x - cameraPos.x,
+                hitResult.y - cameraPos.y,
+                hitResult.z - cameraPos.z,
+                if (failReason.isEmpty()) SETTING_ETHER_WIRE_COLOR else SETTING_ETHER_FAIL_WIRE_COLOR,
                 true
             )
 
             Context.Immediate?.renderFilledShape(
                 outlineShape,
-                blockPos.x - cam.x,
-                blockPos.y - cam.y,
-                blockPos.z - cam.z,
-                secondary,
-                true
+                hitResult.x - cameraPos.x,
+                hitResult.y - cameraPos.y,
+                hitResult.z - cameraPos.z,
+                if (failReason.isEmpty()) SETTING_ETHER_FILL_COLOR else SETTING_ETHER_FAIL_FILL_COLOR,
+                false
+            )
+        }
+
+        on<RenderOverlayEvent> { event ->
+            if (!SETTING_ETHER_SHOW_FAIL_REASON) return@on
+            if (failReason.isEmpty()) return@on
+            Render2D.drawString(
+                event.ctx,
+                failReason,
+                failReasonHud.x, failReasonHud.y, failReasonHud.scale
             )
         }
     }
 
-    private fun raycast(dist: Int): BlockHitResult? {
-        val player = minecraft.player ?: return null
-        val sum = if (player.isSneaking) 1.54 else 1.7
-        val playerPos = player.pos.add(0.0, sum, 0.0)
-        val entityPos = minecraft.cameraEntity?.getRotationVec(
-                minecraft.renderTickCounter.getTickProgress(true)
-            )!!
-        val end = playerPos.add(entityPos.x * dist, entityPos.y * dist, entityPos.z * dist)
+    private fun raycast(
+        x: Double, y: Double, z: Double,
+        dx: Double, dy: Double, dz: Double
+    ): BlockPos? {
+        val w = minecraft.world ?: return null
 
-        val hitResult = minecraft.world!!.raycast(
-            RaycastContext(
-                playerPos,
-                end,
-                RaycastContext.ShapeType.OUTLINE,
-                RaycastContext.FluidHandling.ANY,
-                minecraft.cameraEntity!!
-            )
-        )
-        if (hitResult.type !== HitResult.Type.BLOCK) return null
-
-        return hitResult
-    }
-
-    private fun BlockState.isValid(isTarget: Boolean = false): Boolean {
-        return when (block) {
-            // TODO: missing snow_layer, double_plant, piston_extension
-            Blocks.AIR,
-            Blocks.FIRE,
-            Blocks.LEVER,
-            Blocks.TORCH,
-            Blocks.TRIPWIRE,
-            Blocks.TRIPWIRE_HOOK,
-            Blocks.CARROTS,
-            Blocks.WHEAT,
-            Blocks.POTATOES,
-            Blocks.NETHER_WART,
-            Blocks.PUMPKIN_STEM,
-            Blocks.MELON_STEM,
-            Blocks.REDSTONE_TORCH,
-            Blocks.REDSTONE_WIRE,
-            Blocks.FLOWER_POT,
-            Blocks.DEAD_BUSH,
-            Blocks.TALL_GRASS,
-            Blocks.LADDER,
-            Blocks.REPEATER,
-            Blocks.COMPARATOR,
-            Blocks.COBWEB,
-            Blocks.WATER,
-            Blocks.LILY_PAD,
-            Blocks.LAVA,
-            Blocks.VINE,
-            Blocks.BROWN_MUSHROOM,
-            Blocks.RED_MUSHROOM,
-            Blocks.DANDELION,
-            Blocks.POPPY,
-            Blocks.SHORT_GRASS,
-            Blocks.FERN -> true
-            is SaplingBlock -> true
-            is AbstractRailBlock -> true
-            is ButtonBlock -> true
-            is AbstractSkullBlock -> true
-            is DyedCarpetBlock -> !isTarget
-            else -> false
+        for (bp in DDA(x, y, z, dx, dy, dz)) {
+            val bs = w.getBlockState(bp)
+            if (!BlockTypes.AirLike.contains(bs.block)) return bp
         }
+
+        return null
     }
 }
