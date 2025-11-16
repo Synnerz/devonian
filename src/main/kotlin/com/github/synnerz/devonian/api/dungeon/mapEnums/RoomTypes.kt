976339e07ea0598360ed6bd342ad280801fab3c7
Ch1ck3nNeedsRNG <chick_is_bored>
2025-11-15 22:41:09 -0500

    add room priorities

--- src/main/kotlin/com/github/synnerz/devonian/api/dungeon/mapEnums/RoomTypes.kt
+++ src/main/kotlin/com/github/synnerz/devonian/api/dungeon/mapEnums/RoomTypes.kt
@@ -1,15 +1,15 @@
 package com.github.synnerz.devonian.api.dungeon.mapEnums
 
-enum class RoomTypes {
-    NORMAL,
-    PUZZLE,
-    TRAP,
-    YELLOW,
-    BLOOD,
-    FAIRY,
-    RARE,
-    ENTRANCE,
-    UNKNOWN;
+enum class RoomTypes(val prio: Int) {
+    BLOOD(0),
+    ENTRANCE(10),
+    PUZZLE(20),
+    RARE(30),
+    YELLOW(40),
+    TRAP(50),
+    UNKNOWN(60),
+    FAIRY(70),
+    NORMAL(80);
 
     companion object {
         fun byName(name: String) =
