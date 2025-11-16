0ae19f31482553abd8b357dbcc06422a1391d3f7
Ch1ck3nNeedsRNG <chick_is_bored>
2025-11-16 02:03:56 -0500

    fix rendering issues

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/map/DungeonMap.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/map/DungeonMap.kt
@@ -2,6 +2,7 @@ package com.github.synnerz.devonian.features.dungeons.map
 
 import com.github.synnerz.devonian.api.dungeon.DungeonDoor
 import com.github.synnerz.devonian.api.dungeon.DungeonRoom
+import com.github.synnerz.devonian.api.events.RenderOverlayEvent
 import com.github.synnerz.devonian.hud.HudFeature
 import com.github.synnerz.devonian.utils.BoundingBox
 import net.minecraft.client.gui.DrawContext
@@ -20,12 +21,12 @@ object DungeonMap : HudFeature(
         100.0 * scale, 100.0 * scale
     )
 
-    // TODO: hey doc remember to clone the list before passing it in so you dont get a conc mod :(
     fun redrawMap(rooms: List<DungeonRoom?>, doors: List<DungeonDoor?>) {
         val bounds = getBounds()
+        val window = minecraft.window
         mapRenderer.update(
-            (bounds.w + 0.5).toInt(),
-            (bounds.h + 0.5).toInt(),
+            (bounds.w * window.scaleFactor + 0.5).toInt(),
+            (bounds.h * window.scaleFactor + 0.5).toInt(),
             DungeonMapRenderData(
                 rooms, doors,
                 DungeonMapRenderOptions(
@@ -56,8 +57,14 @@ object DungeonMap : HudFeature(
     }
 
     override fun drawImpl(ctx: DrawContext) {
-        mapRenderer.draw(ctx, x.toFloat(), y.toFloat(), scale)
+        mapRenderer.draw(ctx, x.toFloat(), y.toFloat(), (1.0 / minecraft.window.scaleFactor).toFloat())
 
         // TODO: draw players
+    }
+
+    override fun initialize() {
+        on<RenderOverlayEvent> { event ->
+            draw(event.ctx)
+        }
     }
 }
