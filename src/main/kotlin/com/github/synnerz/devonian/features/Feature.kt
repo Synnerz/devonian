f3e5a9817371e002edee074949056f186f6493d3
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-17 02:07:15 -0700

    internal: port `Feature` to new `ConfigData`

--- src/main/kotlin/com/github/synnerz/devonian/features/Feature.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/Feature.kt
@@ -4,9 +4,8 @@ import com.github.synnerz.devonian.Devonian
 import com.github.synnerz.devonian.api.ChatUtils
 import com.github.synnerz.devonian.api.events.*
 import com.github.synnerz.devonian.config.ui.Category
+import com.github.synnerz.devonian.config.ui.ConfigData
 import com.github.synnerz.devonian.config.ui.ConfigGui
-import com.github.synnerz.devonian.config.ui.ConfigType
-import com.github.synnerz.devonian.utils.JsonUtils
 import com.github.synnerz.devonian.utils.Location
 import com.github.synnerz.talium.components.UISwitch
 import net.minecraft.text.ClickEvent
@@ -17,44 +16,112 @@ open class Feature @JvmOverloads constructor(
     description: String = "",
     category: String = "Misc",
     val area: String? = null,
-    val subarea: String? = null
+    val subarea: String? = null,
+    // To avoid conflict, maybe change the position later ?
+    displayName: String = configName
 ) {
     val minecraft = Devonian.minecraft
     val id = 256652 + Devonian.features.size
     private val style = Style.EMPTY.withClickEvent(ClickEvent.RunCommand("devonian config $id"))
     private var displayed = false
     protected var isRegistered = false
-    private lateinit var _category: Category
+    protected var _category: Category
     private var configComp: UISwitch? = null
+    private val configData = ConfigData.Switch(configName, false)
     val events = mutableListOf<EventBus.EventListener>()
 
     init {
         Devonian.features.add(this)
-        if (configName != "hudManagerInstructions") {
-            _category = ConfigGui.category(category).addConfig(configName, description, ConfigType.SWITCH)
-        }
-        JsonUtils.setConfig(configName, false)
+        _category = ConfigGui.category(category)
+
+        if (configName != "hudManagerInstructions")
+            _category.addSwitch(displayName, description, configData)
     }
 
     open fun initialize() {}
 
     fun isEnabled(): Boolean {
-        return JsonUtils.getConfig(configName) ?: false
+        return configData.get()
     }
 
     fun setEnabled() {
-        JsonUtils.setConfig(configName, true)
+        configData.set(true)
         onToggle(true)
     }
 
     fun setDisabled() {
-        JsonUtils.setConfig(configName, false)
+        configData.set(false)
         onToggle(false)
     }
+
+    fun config(): ConfigData.Switch<Boolean> = configData
 
     fun toggle() {
         if (!isEnabled()) return setEnabled()
         setDisabled()
+    }
+
+    @JvmOverloads
+    fun addSwitch(
+        configName: String,
+        description: String,
+        displayName: String = configName
+    ): ConfigData.Switch<Boolean> {
+        return _category.addSwitch(
+            displayName,
+            description,
+            ConfigData.Switch("${this.configName}$${configName}", false)
+        )
+    }
+
+    @JvmOverloads
+    fun addSlider(
+        configName: String,
+        description: String,
+        min: Double = 0.0, max: Double = 100.0,
+        displayName: String = configName
+    ): ConfigData.Slider<Double> {
+        return _category.addSlider(
+            displayName,
+            description,
+            ConfigData.Slider("${this.configName}$${configName}", 0.0, min, max)
+        )
+    }
+
+    @JvmOverloads
+    fun addButton(
+        displayName: String,
+        description: String,
+        buttonTitle: String = "Click!",
+        onClick: () -> Unit
+    ) {
+        _category.addButton(displayName, description, buttonTitle, onClick)
+    }
+
+    @JvmOverloads
+    fun addTextInput(
+        configName: String,
+        description: String,
+        displayName: String = configName
+    ): ConfigData.TextInput {
+        return _category.addTextInput(
+            displayName,
+            description,
+            ConfigData.TextInput("${this.configName}$${configName}", "")
+        )
+    }
+
+    fun addSelection(
+        configName: String,
+        description: String,
+        options: List<String>,
+        displayName: String = configName
+    ): ConfigData.Selection {
+        return _category.addSelection(
+            displayName,
+            description,
+            ConfigData.Selection("${this.configName}$${configName}", 0, options)
+        )
     }
 
     fun displayChat() {
@@ -85,7 +152,7 @@ open class Feature @JvmOverloads constructor(
     }
 
     open fun onToggle(state: Boolean) {
-        if (configComp == null && ::_category.isInitialized)
+        if (configComp == null)
             configComp = _category.elements[configName] as UISwitch?
 
         configComp?.state = state
