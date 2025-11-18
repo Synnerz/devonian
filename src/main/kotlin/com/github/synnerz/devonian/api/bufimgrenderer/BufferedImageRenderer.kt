6941c5c925242a43d7dbe5c8c9660692a31f8e65
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-18 10:24:53 -0700

    internal: change `BufferedImageRenderer` mappings

--- src/main/kotlin/com/github/synnerz/devonian/api/bufimgrenderer/BufferedImageRenderer.kt
+++ src/main/kotlin/com/github/synnerz/devonian/api/bufimgrenderer/BufferedImageRenderer.kt
@@ -1,19 +1,19 @@
 package com.github.synnerz.devonian.api.bufimgrenderer
 
-import com.github.synnerz.devonian.mixin.accessor.DrawContextAccessor
+import com.github.synnerz.devonian.mixin.accessor.GuiGraphicsAccessor
 import com.mojang.blaze3d.pipeline.BlendFunction
 import com.mojang.blaze3d.pipeline.RenderPipeline
 import com.mojang.blaze3d.platform.DepthTestFunction
 import com.mojang.blaze3d.platform.DestFactor
 import com.mojang.blaze3d.platform.SourceFactor
-import com.mojang.blaze3d.vertex.VertexFormat.DrawMode
+import com.mojang.blaze3d.vertex.DefaultVertexFormat
+import com.mojang.blaze3d.vertex.VertexFormat.Mode
 import kotlinx.atomicfu.atomic
-import net.minecraft.client.gl.RenderPipelines
-import net.minecraft.client.gui.DrawContext
-import net.minecraft.client.render.RenderLayer
-import net.minecraft.client.render.RenderPhase
-import net.minecraft.client.render.VertexFormats
-import net.minecraft.util.Identifier
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.client.renderer.RenderPipelines
+import net.minecraft.client.renderer.RenderStateShard
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.util.TriState
 import java.awt.image.BufferedImage
 import java.util.concurrent.Future
@@ -26,17 +26,17 @@ abstract class BufferedImageRenderer<T>(val name: String, bilinear: TriState) {
     val dirtyImage = atomic<BufferedImage?>(null)
     val bimgProvider: BufferedImageFactory = BufferedImageFactoryImpl()
     var lastFuture: Future<*>? = null
-    val mcid = Identifier.of("devonian", "buffered_image/${name.lowercase()}")
-    val layer = RenderLayer.of(
+    val mcid = ResourceLocation.fromNamespaceAndPath("devonian", "buffered_image/${name.lowercase()}")
+    val layer = RenderType.create(
         "devonian/buffered_image_layer/${name.lowercase()}",
         1536,
         false,
         true,
         pipeline,
-        RenderLayer.MultiPhaseParameters
+        RenderType.CompositeState
             .builder()
-            .texture(RenderPhase.Texture(mcid, bilinear, false))
-            .build(false)
+            .setTextureState(RenderStateShard.TextureStateShard(mcid, bilinear, false))
+            .createCompositeState(false)
     )
 
     init {
@@ -63,34 +63,34 @@ abstract class BufferedImageRenderer<T>(val name: String, bilinear: TriState) {
         if (bimg != null) uploader.upload(bimg)
     }
 
-    fun draw(ctx: DrawContext, x: Float, y: Float, scale: Float = 1f) {
+    fun draw(ctx: GuiGraphics, x: Float, y: Float, scale: Float = 1f) {
         draw(ctx, x, y, uploader.w * scale, uploader.h * scale)
         uploadImage()
     }
 
-    fun drawStretched(ctx: DrawContext, x: Float, y: Float, w: Float, h: Float) {
+    fun drawStretched(ctx: GuiGraphics, x: Float, y: Float, w: Float, h: Float) {
         draw(ctx, x, y, w, h)
         uploadImage()
     }
 
-    private fun draw(ctx: DrawContext, x: Float, y: Float, w: Float, h: Float) {
+    private fun draw(ctx: GuiGraphics, x: Float, y: Float, w: Float, h: Float) {
         if (uploader.texId == -1) return
 
-        val mat = ctx.matrices.peek().positionMatrix
-        val consumer = (ctx as DrawContextAccessor).vertexConsumers
+        val mat = ctx.pose().last().pose()
+        val consumer = (ctx as GuiGraphicsAccessor).vertexConsumers
         val buf = consumer.getBuffer(layer)
-        buf.vertex(mat, x, y, 0f).texture(0f, 0f).color(-1)
-        buf.vertex(mat, x, y + h, 0f).texture(0f, 1f).color(-1)
-        buf.vertex(mat, x + w, y, 0f).texture(1f, 0f).color(-1)
-        buf.vertex(mat, x + w, y + h, 0f).texture(1f, 1f).color(-1)
+        buf.addVertex(mat, x, y, 0f).setUv(0f, 0f).setColor(-1)
+        buf.addVertex(mat, x, y + h, 0f).setUv(0f, 1f).setColor(-1)
+        buf.addVertex(mat, x + w, y, 0f).setUv(1f, 0f).setColor(-1)
+        buf.addVertex(mat, x + w, y + h, 0f).setUv(1f, 1f).setColor(-1)
     }
 
     companion object {
         val pool = ThreadPoolExecutor(1, 2, 60, TimeUnit.SECONDS, LinkedBlockingQueue())
-        val pipeline = RenderPipeline.builder(RenderPipelines.POSITION_TEX_COLOR_SNIPPET)
+        val pipeline = RenderPipeline.builder(RenderPipelines.GUI_TEXTURED_SNIPPET)
             .withLocation("devonian/buffered_image_textured_triangle_strip")
             .withCull(false)
-            .withVertexFormat(VertexFormats.POSITION_TEXTURE_COLOR, DrawMode.TRIANGLE_STRIP)
+            .withVertexFormat(DefaultVertexFormat.POSITION_TEX_COLOR, Mode.TRIANGLE_STRIP)
             .withDepthWrite(false)
             .withDepthTestFunction(DepthTestFunction.NO_DEPTH_TEST)
             .withBlend(
