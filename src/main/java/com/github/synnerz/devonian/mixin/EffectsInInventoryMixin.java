0724fa08b13fc2c30e45d6e639a5903cb32803f7
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-18 13:27:25 -0700

    internal: change `EffectsInInventoryMixin` mappings

--- src/main/java/com/github/synnerz/devonian/mixin/EffectsInInventoryMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/EffectsInInventoryMixin.java
@@ -1,21 +1,21 @@
 package com.github.synnerz.devonian.mixin;
 
 import com.github.synnerz.devonian.features.misc.HideInventoryEffects;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.ingame.StatusEffectsDisplay;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.EffectsInInventory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(StatusEffectsDisplay.class)
-public class StatusEffectsDisplayMixin {
+@Mixin(EffectsInInventory.class)
+public class EffectsInInventoryMixin {
     @Inject(
-            method = "drawStatusEffects(Lnet/minecraft/client/gui/DrawContext;II)V",
+            method = "renderEffects",
             at = @At("HEAD"),
             cancellable = true
     )
-    private void devonian$onInventoryEffects(DrawContext context, int mouseX, int mouseY, CallbackInfo ci) {
+    private void devonian$onInventoryEffects(GuiGraphics guiGraphics, int i, int j, CallbackInfo ci) {
         if (!HideInventoryEffects.INSTANCE.isEnabled()) return;
         ci.cancel();
     }
