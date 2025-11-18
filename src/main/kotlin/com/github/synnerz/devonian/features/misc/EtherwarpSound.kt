db98b28b7b6c0f413b95767f9dcf324aa2fd1119
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-18 12:24:23 -0700

    internal: change `EtherwarpSound` mappings

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/EtherwarpSound.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/EtherwarpSound.kt
@@ -5,16 +5,16 @@ import com.github.synnerz.devonian.api.events.SoundPlayEvent
 import com.github.synnerz.devonian.commands.DevonianCommand
 import com.github.synnerz.devonian.features.Feature
 import com.github.synnerz.devonian.utils.JsonUtils
-import net.minecraft.registry.Registries
-import net.minecraft.sound.SoundEvents
-import net.minecraft.util.Identifier
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.sounds.SoundEvents
 
 object EtherwarpSound : Feature(
     "etherwarpSound",
     "Changes the sound the etherwarp makes whenever you have etherwarped successfully, customize it via /devonian etherwarpsound"
 ) {
     private const val KEY = "etherwarpSound"
-    private var soundEvent = SoundEvents.ENTITY_ENDER_DRAGON_HURT
+    private var soundEvent = SoundEvents.ENDER_DRAGON_HURT
 
     override fun initialize() {
         JsonUtils.set(KEY, "minecraft:entity.ender_dragon.hurt")
@@ -24,7 +24,7 @@ object EtherwarpSound : Feature(
             if (args.isEmpty()) return@subcommand 0
             val soundRegistry = args.first() as String
 
-            soundEvent = Registries.SOUND_EVENT.get(Identifier.of(soundRegistry))
+            soundEvent = BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse(soundRegistry))
 
             JsonUtils.set(KEY, soundRegistry)
             ChatUtils.sendMessage("&aSuccessfully set etherwarp sound to &6$soundRegistry", true)
@@ -33,12 +33,12 @@ object EtherwarpSound : Feature(
             .greedyString("sound")
             .suggest(
                 "sound",
-                *Registries.SOUND_EVENT.entrySet.map { "${it.value.id.namespace}:${it.value.id.path}" }.toTypedArray()
+                *BuiltInRegistries.SOUND_EVENT.entrySet().map { "${it.value.location.namespace}:${it.value.location.path}" }.toTypedArray()
             )
 
         JsonUtils.afterLoad {
             val savedRegistry = JsonUtils.get<String>(KEY) ?: "minecraft:entity.ender_dragon.hurt"
-            soundEvent = Registries.SOUND_EVENT.get(Identifier.of(savedRegistry))
+            soundEvent = BuiltInRegistries.SOUND_EVENT.getValue(ResourceLocation.parse(savedRegistry))
         }
 
         on<SoundPlayEvent> { event ->
@@ -46,11 +46,11 @@ object EtherwarpSound : Feature(
                 event.sound != "minecraft:entity.ender_dragon.hurt" ||
                 event.volume != 1f ||
                 event.pitch != 0.53968257f ||
-                soundEvent == SoundEvents.ENTITY_ENDER_DRAGON_HURT
+                soundEvent == SoundEvents.ENDER_DRAGON_HURT
             ) return@on
 
             event.cancel()
-            minecraft.world?.playSoundClient(
+            minecraft.level?.playLocalSound(
                 event.x, event.y, event.z,
                 soundEvent, event.category,
                 1f, 1f,
