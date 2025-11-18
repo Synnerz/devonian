02b38dc188068001b7c15d69da269f57d7aab9df
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-18 13:23:16 -0700

    internal: change `InventoryMenuMixin` mappings

--- src/main/java/com/github/synnerz/devonian/mixin/InventoryMenuMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/InventoryMenuMixin.java
@@ -2,35 +2,35 @@ package com.github.synnerz.devonian.mixin;
 
 import com.github.synnerz.devonian.api.events.DropItemEvent;
 import com.github.synnerz.devonian.api.events.EventBus;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.RecipeInputInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.screen.PlayerScreenHandler;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.CraftingContainer;
+import net.minecraft.world.inventory.InventoryMenu;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerScreenHandler.class)
-public abstract class PlayerScreenHandlerMixin {
+@Mixin(InventoryMenu.class)
+public abstract class InventoryMenuMixin {
     @Shadow
-    public abstract RecipeInputInventory getCraftingInput();
+    public abstract CraftingContainer getCraftSlots();
 
     @Inject(
-            method = "onClosed",
+            method = "removed",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/inventory/CraftingResultInventory;clear()V",
+                    target = "Lnet/minecraft/world/inventory/ResultContainer;clearContent()V",
                     shift = At.Shift.AFTER
             ),
             cancellable = true
     )
-    private void devonian$onClosedClear(PlayerEntity player, CallbackInfo ci) {
-        if (player.getWorld().isClient) {
-            RecipeInputInventory craftingInventory = getCraftingInput();
-            for (int idx = 0; idx < craftingInventory.size(); idx++) {
-                ItemStack stack = craftingInventory.getStack(idx);
+    private void devonian$onClosedClear(Player player, CallbackInfo ci) {
+        if (player.level().isClientSide) {
+            CraftingContainer craftingInventory = getCraftSlots();
+            for (int idx = 0; idx < craftingInventory.getContainerSize(); idx++) {
+                ItemStack stack = craftingInventory.getItem(idx);
                 DropItemEvent event = new DropItemEvent(stack, true);
                 if (!stack.isEmpty()) EventBus.INSTANCE.post(event);
                 if (event.isCancelled()) ci.cancel();
