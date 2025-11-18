c92ae7ad11ee77be567926c0c9276a8b301e7c7b
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-18 13:04:32 -0700

    internal: change `ScreenEffectRendererMixin` mappings

--- src/main/java/com/github/synnerz/devonian/mixin/ScreenEffectRendererMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/ScreenEffectRendererMixin.java
@@ -1,22 +1,22 @@
 package com.github.synnerz.devonian.mixin;
 
 import com.github.synnerz.devonian.features.misc.RemoveFireOverlay;
-import net.minecraft.client.gui.hud.InGameOverlayRenderer;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.util.math.MatrixStack;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.ScreenEffectRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(InGameOverlayRenderer.class)
-public class InGameOverlayRendererMixin {
+@Mixin(ScreenEffectRenderer.class)
+public class ScreenEffectRendererMixin {
     @Inject(
-            method = "renderFireOverlay",
+            method = "renderFire",
             at = @At("HEAD"),
             cancellable = true
     )
-    private static void devonian$renderFireOverlay(MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
+    private static void devonian$renderFireOverlay(PoseStack poseStack, MultiBufferSource multiBufferSource, CallbackInfo ci) {
         if (!RemoveFireOverlay.INSTANCE.isEnabled()) return;
         ci.cancel();
     }
