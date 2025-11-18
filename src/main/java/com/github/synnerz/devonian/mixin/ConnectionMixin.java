0039fea5c337c88bbdccf7f0da37bcaffd1e85bd
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-18 12:44:03 -0700

    internal: change `ConnectionMixin` mappings

--- src/main/java/com/github/synnerz/devonian/mixin/ConnectionMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/ConnectionMixin.java
@@ -4,42 +4,42 @@ import com.github.synnerz.devonian.api.events.EventBus;
 import com.github.synnerz.devonian.api.events.PacketReceivedEvent;
 import com.github.synnerz.devonian.api.events.PacketSentEvent;
 import io.netty.channel.ChannelHandlerContext;
-import net.minecraft.network.ClientConnection;
-import net.minecraft.network.NetworkSide;
-import net.minecraft.network.PacketCallbacks;
-import net.minecraft.network.packet.Packet;
+import net.minecraft.network.Connection;
+import net.minecraft.network.PacketSendListener;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.PacketFlow;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientConnection.class)
-public abstract class ClientConnectionMixin {
+@Mixin(Connection.class)
+public abstract class ConnectionMixin {
     @Shadow
-    public abstract NetworkSide getSide();
+    public abstract PacketFlow getReceiving();
 
     @Inject(
-            method = "channelRead0(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/packet/Packet;)V",
+            method = "channelRead0(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/protocol/Packet;)V",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/network/ClientConnection;handlePacket(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/listener/PacketListener;)V",
+                    target = "Lnet/minecraft/network/Connection;genericsFtw(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;)V",
                     shift = At.Shift.BEFORE
             ),
             cancellable = true
     )
     private void devonian$handlePacket(ChannelHandlerContext channelHandlerContext, Packet<?> packet, CallbackInfo ci) {
-        if (getSide() == NetworkSide.CLIENTBOUND) {
+        if (getReceiving() == PacketFlow.CLIENTBOUND) {
             EventBus.INSTANCE.post(new PacketReceivedEvent(packet, ci));
         }
     }
 
     @Inject(
-            method = "send(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/PacketCallbacks;Z)V",
+            method = "sendPacket",
             at = @At("HEAD"),
             cancellable = true
     )
-    private void devonian$sendPacket(Packet<?> packet, PacketCallbacks callbacks, boolean flush, CallbackInfo ci) {
+    private void devonian$sendPacket(Packet<?> packet, PacketSendListener packetSendListener, boolean bl, CallbackInfo ci) {
         EventBus.INSTANCE.post(new PacketSentEvent(packet, ci));
     }
 }
