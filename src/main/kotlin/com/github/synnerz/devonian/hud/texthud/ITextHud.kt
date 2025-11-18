cd3e3c58ca03a15230250f7bfb9e3c7455150cf9
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-18 03:51:06 -0700

    internal: change `ITextHud` mappings

--- src/main/kotlin/com/github/synnerz/devonian/hud/texthud/ITextHud.kt
+++ src/main/kotlin/com/github/synnerz/devonian/hud/texthud/ITextHud.kt
@@ -1,14 +1,14 @@
 package com.github.synnerz.devonian.hud.texthud
 
 import com.github.synnerz.devonian.utils.BoundingBox
-import net.minecraft.client.gui.DrawContext
+import net.minecraft.client.gui.GuiGraphics
 
 interface ITextHud {
     fun getWidth(): Double
     fun getLineHeight(): Double
     fun getHeight(): Double
     fun getBounds(): BoundingBox
-    fun draw(ctx: DrawContext)
+    fun draw(ctx: GuiGraphics)
     fun clearLines(): ITextHud
     fun addLine(s: String): ITextHud
     fun addLines(s: List<String>): ITextHud
