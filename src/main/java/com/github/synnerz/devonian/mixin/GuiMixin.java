076ad812ef836d3fd33036811d86cada8b8cf76f
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-18 13:03:27 -0700

    internal: change `GuiMixin` mappings

--- src/main/java/com/github/synnerz/devonian/mixin/GuiMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/GuiMixin.java
@@ -2,22 +2,22 @@ package com.github.synnerz.devonian.mixin;
 
 import com.github.synnerz.devonian.api.events.RenderOverlayEvent;
 import com.github.synnerz.devonian.features.misc.HidePotionEffectOverlay;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(InGameHud.class)
-public class InGameHudMixin {
+@Mixin(Gui.class)
+public class GuiMixin {
     @Inject(
-            method = "renderStatusEffectOverlay",
+            method = "renderEffects",
             at = @At("HEAD"),
             cancellable = true
     )
-    private void devonian$renderStatusOverlay(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+    private void devonian$renderStatusOverlay(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci) {
         if (!HidePotionEffectOverlay.INSTANCE.isEnabled()) return;
         ci.cancel();
     }
@@ -26,7 +26,7 @@ public class InGameHudMixin {
             method = "render",
             at = @At("TAIL")
     )
-    private void devonian$onRenderOverlay(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
-        new RenderOverlayEvent(context, tickCounter).post();
+    private void devonian$onRenderOverlay(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci) {
+        new RenderOverlayEvent(guiGraphics, deltaTracker).post();
     }
 }
