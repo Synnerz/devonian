87281bcf34e39617a9c2cfc081f3e1a816156797
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-18 02:19:32 -0700

    internal: change `ItemUtils` mappings

--- src/main/kotlin/com/github/synnerz/devonian/api/ItemUtils.kt
+++ src/main/kotlin/com/github/synnerz/devonian/api/ItemUtils.kt
@@ -1,21 +1,21 @@
 package com.github.synnerz.devonian.api
 
-import net.minecraft.component.DataComponentTypes
-import net.minecraft.item.ItemStack
-import net.minecraft.nbt.NbtCompound
+import net.minecraft.core.component.DataComponents
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.world.item.ItemStack
 
 object ItemUtils {
     @SuppressWarnings("deprecation")
     fun skyblockId(itemStack: ItemStack): String? {
-        val nbt = itemStack.get(DataComponentTypes.CUSTOM_DATA)?.nbt ?: return null
+        val nbt = itemStack.get(DataComponents.CUSTOM_DATA)?.unsafe ?: return null
         val itemId = nbt.getString("id")
         if (itemId.isEmpty) return null
 
         return itemId.get()
     }
 
-    fun extraAttributes(itemStack: ItemStack): NbtCompound? {
-        return itemStack.get(DataComponentTypes.CUSTOM_DATA)?.nbt
+    fun extraAttributes(itemStack: ItemStack): CompoundTag? {
+        return itemStack.get(DataComponents.CUSTOM_DATA)?.unsafe
     }
 
     fun uuid(itemStack: ItemStack): String? {
@@ -26,7 +26,7 @@ object ItemUtils {
     }
 
     fun lore(itemStack: ItemStack): List<String>? {
-        val lore = itemStack.get(DataComponentTypes.LORE)?.lines ?: return null
+        val lore = itemStack.get(DataComponents.LORE)?.lines ?: return null
 
         return lore.map { it.string }
     }
