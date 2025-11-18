befe260822099b14e10b9b97995d3af7ce9febce
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-18 13:19:41 -0700

    internal: change `PlayerTabOverlayMixin` mappings

--- src/main/java/com/github/synnerz/devonian/mixin/PlayerTabOverlayMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/PlayerTabOverlayMixin.java
@@ -1,22 +1,22 @@
 package com.github.synnerz.devonian.mixin;
 
 import com.github.synnerz.devonian.features.misc.RemoveTabPing;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.hud.PlayerListHud;
-import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.PlayerTabOverlay;
+import net.minecraft.client.multiplayer.PlayerInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerListHud.class)
-public class PlayerListHudMixin {
+@Mixin(PlayerTabOverlay.class)
+public class PlayerTabOverlayMixin {
     @Inject(
-            method = "renderLatencyIcon",
+            method = "renderPingIcon",
             at = @At("HEAD"),
             cancellable = true
     )
-    private void devonian$renderPingIcon(DrawContext context, int width, int x, int y, PlayerListEntry entry, CallbackInfo ci) {
+    private void devonian$renderPingIcon(GuiGraphics guiGraphics, int i, int j, int k, PlayerInfo playerInfo, CallbackInfo ci) {
         if (!RemoveTabPing.INSTANCE.isEnabled()) return;
         ci.cancel();
     }
