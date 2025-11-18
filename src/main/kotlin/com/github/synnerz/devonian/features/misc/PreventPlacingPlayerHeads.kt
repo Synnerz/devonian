2a483073f717060c06092d4ea09688a3fd5af3d7
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-18 03:53:20 -0700

    internal: change `PreventPlacingPlayerHeads` mappings

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/PreventPlacingPlayerHeads.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/PreventPlacingPlayerHeads.kt
@@ -3,7 +3,7 @@ package com.github.synnerz.devonian.features.misc
 import com.github.synnerz.devonian.api.ItemUtils
 import com.github.synnerz.devonian.api.events.BlockInteractEvent
 import com.github.synnerz.devonian.features.Feature
-import net.minecraft.item.Items
+import net.minecraft.world.item.Items
 
 object PreventPlacingPlayerHeads : Feature(
     "preventPlacingPlayerHeads",
@@ -11,7 +11,7 @@ object PreventPlacingPlayerHeads : Feature(
 ) {
     override fun initialize() {
         on<BlockInteractEvent> { event ->
-            if (minecraft.world?.getBlockState(event.pos) == null) return@on
+            if (minecraft.level?.getBlockState(event.pos) == null) return@on
             if (ItemUtils.skyblockId(event.itemStack) == null || event.itemStack.item != Items.PLAYER_HEAD) return@on
 
             event.cancel()
