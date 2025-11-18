852df0698db0877e4a6c7431e5431e0f9a6582e0
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-18 12:47:42 -0700

    internal: change `EntityRenderDispatcherMixin` mappings

--- src/main/java/com/github/synnerz/devonian/mixin/EntityRenderDispatcherMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/EntityRenderDispatcherMixin.java
@@ -2,10 +2,10 @@ package com.github.synnerz.devonian.mixin;
 
 import com.github.synnerz.devonian.api.events.EventBus;
 import com.github.synnerz.devonian.api.events.RenderEntityEvent;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.EntityRenderDispatcher;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,11 +14,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EntityRenderDispatcher.class)
 public class EntityRenderDispatcherMixin {
     @Inject(
-            method = "render(Lnet/minecraft/entity/Entity;DDDFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
+            method = "render(Lnet/minecraft/world/entity/Entity;DDDFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
             at = @At("HEAD"),
             cancellable = true
     )
-    private <E extends Entity> void devonian$preEntityRender(E entity, double x, double y, double z, float tickProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
-        EventBus.INSTANCE.post(new RenderEntityEvent(entity, matrices, vertexConsumers, light, ci));
+    private <E extends Entity> void devonian$preEntityRender(E entity, double d, double e, double f, float g, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, CallbackInfo ci) {
+        EventBus.INSTANCE.post(new RenderEntityEvent(entity, poseStack, multiBufferSource, i, ci));
     }
 }
