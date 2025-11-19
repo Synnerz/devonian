bfada572c51948a504f32f3672ce6c0cece2b4af
Ch1ck3nNeedsRNG <chick_is_bored>
2025-11-19 01:43:24 -0500

    change to namechant event

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/RemoveDamageTag.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/RemoveDamageTag.kt
@@ -1,9 +1,8 @@
 package com.github.synnerz.devonian.features.dungeons
 
 import com.github.synnerz.devonian.api.Scheduler
-import com.github.synnerz.devonian.api.events.PacketReceivedEvent
+import com.github.synnerz.devonian.api.events.PacketNameChangeEvent
 import com.github.synnerz.devonian.features.Feature
-import net.minecraft.network.protocol.game.ClientboundAddEntityPacket
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
 
@@ -13,23 +12,17 @@ object RemoveDamageTag : Feature(
     "Dungeons",
     "catacombs"
 ) {
-    private var damageTagRegex = "^.?\\d[\\d,.]+.*?\$".toRegex()
+    private var damageTagRegex = "^.?\\d[\\d,.]+.*?$".toRegex()
 
     override fun initialize() {
-        on<PacketReceivedEvent> { event ->
-            val packet = event.packet
-            if (packet !is ClientboundAddEntityPacket) return@on
-            if (packet.type !== EntityType.ARMOR_STAND) return@on
+        on<PacketNameChangeEvent> { event ->
+            if (event.type !== EntityType.ARMOR_STAND) return@on
 
-            Scheduler.scheduleServerTask {
-                val world = minecraft.level ?: return@scheduleServerTask
-                val entityId = packet.id
-                val entity = world.getEntity(entityId) ?: return@scheduleServerTask
-                val name = entity.customName?.string ?: return@scheduleServerTask
-                if (!name.matches(damageTagRegex)) return@scheduleServerTask
+            val world = minecraft.level ?: return@on
 
-                world.removeEntity(entityId, Entity.RemovalReason.DISCARDED)
-            }
+            if (!event.name.string.matches(damageTagRegex)) return@on
+
+            Scheduler.scheduleTask { world.removeEntity(event.entityId, Entity.RemovalReason.DISCARDED) }
         }
     }
 }
