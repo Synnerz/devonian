6a9f80716f6115bd88ef3d65863efc6a58c1c1ea
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-19 12:17:03 -0700

    internal: add score calc helpers

--- src/main/kotlin/com/github/synnerz/devonian/api/dungeon/Dungeons.kt
+++ src/main/kotlin/com/github/synnerz/devonian/api/dungeon/Dungeons.kt
@@ -1,19 +1,41 @@
 package com.github.synnerz.devonian.api.dungeon
 
 import com.github.synnerz.devonian.Devonian
-import com.github.synnerz.devonian.api.events.AreaEvent
-import com.github.synnerz.devonian.api.events.EventBus
-import com.github.synnerz.devonian.api.events.SubAreaEvent
-import com.github.synnerz.devonian.api.events.TabUpdateEvent
-import com.github.synnerz.devonian.api.events.TickEvent
+import com.github.synnerz.devonian.api.events.*
 import com.github.synnerz.devonian.utils.Location
 import com.github.synnerz.devonian.utils.StringUtils
 
 object Dungeons {
     private val playerInfoRegex = "^\\[\\d+] (\\w+)(?:.+?)? \\((\\w+) ?([IVXLCDM]+)?\\)$".toRegex()
     private val dungeonFloorRegex = "^ * ⏣ The Catacombs \\((\\w+)\\)$".toRegex()
+    private val clearedPercentRegex = "^Cleared: (\\d+)% \\(\\d+\\)\$".toRegex()
+    private val timeElapsedRegex = "^Time Elapsed: (?:(\\d+)h)? ?(?:(\\d+)m)? ?(\\d+)s\$".toRegex()
+    private val teamDeathsRegex = "^Team Deaths: (\\d+)$".toRegex()
+    private val cryptsRegex = "^ Crypts: (\\d+)$".toRegex()
+    private val secretsFoundPercentRegex = "^ Secrets Found: ([\\d.]+)%\$".toRegex()
+    private val secretsFoundRegex = "^ Secrets Found: (\\d+)\$".toRegex()
+    private val completedRoomsRegex = "^ Completed Rooms: (\\d+)\$".toRegex()
+    private val openedRoomsRegex = "^ Opened Rooms: (\\d+)\$".toRegex()
+    private val discoveriesRegex = "^Discoveries: (\\d+)$".toRegex()
+    private val puzzleNameRegex = "^ ([\\w ]+): \\[(✦|✔|✖)\\] ?\\(?(\\w+)?\\)?\$".toRegex()
+    private val puzzlesCountRegex = "^Puzzles: \\((\\d+)\\)\$".toRegex()
     val players = linkedMapOf<String, DungeonPlayer>()
     var floor = FloorType.None
+    var clearedPercent = 0
+    var timeElapsed = 0
+    var deaths = 0
+    var crypts = 0
+    var secretsFoundPercent = 0.0
+    var secretsFound = 0
+    var completedRooms = 0
+    var openedRooms = 0
+    var discoveries = 0
+    var totalPuzzles = 0
+    var completedPuzzles = 0
+    val bloodDone = false
+    val bossEntered = false
+    val mimicKilled = false
+    var score = 0
 
     fun initialize() {
         DungeonScanner.init()
@@ -22,6 +44,53 @@ object Dungeons {
     init {
         EventBus.on<TabUpdateEvent> { event ->
             if (Location.area != "catacombs") return@on
+
+            event.matches(cryptsRegex)?.let {
+                crypts = it[0].toInt()
+                return@on
+            }
+
+            event.matches(teamDeathsRegex)?.let {
+                deaths = it[0].toInt()
+                return@on
+            }
+
+            event.matches(secretsFoundPercentRegex)?.let {
+                secretsFoundPercent = it[0].toDouble()
+                return@on
+            }
+
+            event.matches(completedRoomsRegex)?.let {
+                completedRooms = it[0].toInt()
+                return@on
+            }
+
+            event.matches(secretsFoundRegex)?.let {
+                secretsFound = it[0].toInt()
+                return@on
+            }
+
+            event.matches(openedRoomsRegex)?.let {
+                openedRooms = it[0].toInt()
+                return@on
+            }
+
+            event.matches(discoveriesRegex)?.let {
+                discoveries = it[0].toInt()
+                return@on
+            }
+
+            event.matches(puzzlesCountRegex)?.let {
+                totalPuzzles = it[0].toInt()
+                return@on
+            }
+
+            event.matches(puzzleNameRegex)?.let {
+                if (it[1].isEmpty() || it[1] != "✔") return@on
+                completedPuzzles++.coerceIn(0, totalPuzzles)
+                return@on
+            }
+
             val match = event.matches(playerInfoRegex) ?: return@on
             val (name, role) = match
 
@@ -65,6 +134,7 @@ object Dungeons {
                 players.clear()
                 DungeonScanner.reset()
                 DungeonMapScanner.reset()
+                reset()
             }
         }
 
@@ -75,5 +145,33 @@ object Dungeons {
 
             floor = FloorType.from(name)
         }
+
+        EventBus.on<ScoreboardEvent> { event ->
+            val matchesTime = event.matches(timeElapsedRegex)
+            if (matchesTime != null) {
+                val hours = matchesTime[0].ifEmpty { "0" }.toInt()
+                val minutes = matchesTime[1].ifEmpty { "0" }.toInt()
+                val seconds = matchesTime[2].ifEmpty { "0" }.toInt()
+                timeElapsed = hours * 3600 + minutes * 60 + seconds
+                return@on
+            }
+
+            val match = event.matches(clearedPercentRegex) ?: return@on
+            clearedPercent = match[0].toInt()
+        }
+    }
+
+    private fun reset() {
+        clearedPercent = 0
+        timeElapsed = 0
+        deaths = 0
+        crypts = 0
+        secretsFoundPercent = 0.0
+        completedRooms = 0
+        openedRooms = 0
+        discoveries = 0
+        totalPuzzles = 0
+        completedPuzzles = 0
+        score = 0
     }
 }
--- src/main/kotlin/com/github/synnerz/devonian/api/dungeon/FloorType.kt
+++ src/main/kotlin/com/github/synnerz/devonian/api/dungeon/FloorType.kt
@@ -9,26 +9,28 @@ enum class FloorType(
     val roomsW: Int,
     val roomsH: Int,
     val longName: String = shortName,
+    val requiredPercent: Double = 1.0,
+    val requiredSpeed: Int = 600
 ) {
     None(0, false, "", 0, 0, ""),
 
-    Entrance(0, false, "E", 4, 4, "Entrance"),
+    Entrance(0, false, "E", 4, 4, "Entrance", 0.3, 1200),
 
-    F1(1, false, "F1", 4, 5),
-    F2(2, false, "F2", 5, 5),
-    F3(3, false, "F3", 5, 5),
-    F4(4, false, "F4", 6, 5),
-    F5(5, false, "F5", 6, 6),
-    F6(6, false, "F6", 6, 6),
-    F7(7, false, "F7", 6, 6),
+    F1(1, false, "F1", 4, 5, requiredPercent = 0.3),
+    F2(2, false, "F2", 5, 5, requiredPercent = 0.4),
+    F3(3, false, "F3", 5, 5, requiredPercent = 0.5),
+    F4(4, false, "F4", 6, 5, requiredPercent = 0.6, requiredSpeed = 720),
+    F5(5, false, "F5", 6, 6, requiredPercent = 0.7),
+    F6(6, false, "F6", 6, 6, requiredPercent = 0.85, requiredSpeed = 720),
+    F7(7, false, "F7", 6, 6, requiredSpeed = 840),
 
-    M1(1, true, "M1", 4, 5),
-    M2(2, true, "M2", 5, 5),
-    M3(3, true, "M3", 5, 5),
-    M4(4, true, "M4", 6, 5),
-    M5(5, true, "M5", 6, 6),
-    M6(6, true, "M6", 6, 6),
-    M7(7, true, "M7", 6, 6);
+    M1(1, true, "M1", 4, 5, requiredSpeed = 480),
+    M2(2, true, "M2", 5, 5, requiredSpeed = 480),
+    M3(3, true, "M3", 5, 5, requiredSpeed = 480),
+    M4(4, true, "M4", 6, 5, requiredSpeed = 480),
+    M5(5, true, "M5", 6, 6, requiredSpeed = 480),
+    M6(6, true, "M6", 6, 6, requiredSpeed = 480),
+    M7(7, true, "M7", 6, 6, requiredSpeed = 900);
 
     val maxDim = max(roomsW, roomsH)
 
