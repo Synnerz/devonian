22263191b0d0f9667b8e5dbdc2c56424fbacc092
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-20 18:09:50 -0700

    feat: reimpl `CompactChat` to properly work now

--- src/main/java/com/github/synnerz/devonian/mixin/ChatComponentMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/ChatComponentMixin.java
@@ -1,10 +1,15 @@
 package com.github.synnerz.devonian.mixin;
 
+import com.github.synnerz.devonian.features.misc.CompactChat;
 import com.github.synnerz.devonian.features.misc.RemoveChatLimit;
 import net.minecraft.client.gui.components.ChatComponent;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
@@ -26,5 +31,19 @@ public class ChatComponentMixin {
     private int devonian$onMessageVisibleSize(List instance) {
         if (RemoveChatLimit.INSTANCE.isEnabled() && instance.size() > 100) return 100;
         return instance.size();
+    }
+
+    @ModifyVariable(
+            method = "addMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/MessageSignature;Lnet/minecraft/client/GuiMessageTag;)V",
+            at = @At("HEAD"),
+            argsOnly = true
+    )
+    private Component devonian$addMessage(Component text) {
+        return CompactChat.INSTANCE.compactText(text);
+    }
+
+    @Inject(method = "clearMessages", at = @At("HEAD"))
+    private void devonian$clearMessages(boolean bl, CallbackInfo ci) {
+        CompactChat.INSTANCE.clearHistory();
     }
 }
--- src/main/kotlin/com/github/synnerz/devonian/features/misc/CompactChat.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/CompactChat.kt
@@ -1,45 +1,81 @@
 package com.github.synnerz.devonian.features.misc
 
 import com.github.synnerz.devonian.api.ChatUtils
-import com.github.synnerz.devonian.api.Scheduler
-import com.github.synnerz.devonian.api.events.ChatEvent
 import com.github.synnerz.devonian.features.Feature
-import com.github.synnerz.devonian.utils.StringUtils.clearCodes
+import net.minecraft.ChatFormatting
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.FormattedText
+import net.minecraft.network.chat.MutableComponent
+import net.minecraft.network.chat.Style
+import net.minecraft.network.chat.contents.PlainTextContents
+import java.util.*
 
-// FIXME: whenever using `removeLines` it will always be worse in performance
-//  due to it being O(n), find some way to make this functionality work without it being too annoying to code
-//  in order for it to not be as performance intensive in the future.
+// Credits to <https://github.com/caoimhebyrne/compact-chat>
+// Licensed under the MIT license
 object CompactChat : Feature(
     "compactChat",
     "Stacks the messages if they are repeated and adds the amount of times it was repeated"
 ) {
-    private val chatList = mutableMapOf<String, Pair<Int, Long>>()
+    private val STYLE = Style.EMPTY.withColor(ChatFormatting.GRAY)
+    private val chatHistory = hashMapOf<String, MessageHistory>()
 
-    override fun initialize() {
-        on<ChatEvent> { event ->
-            val msg = event.message
-            if (msg.trim().isEmpty()) return@on
+    data class MessageHistory(var times: Int = 0, var lastTime: Long = 0L)
 
-            val data = chatList[msg]
-            val lastTime = data?.second
+    fun compactText(text: Component): Component {
+        if (!isEnabled()) return text
+        val string = text.string
+        if (string.isBlank()) return text
+        val cachedData = chatHistory.getOrPut(string) { MessageHistory(0, System.currentTimeMillis()) }
+        if (System.currentTimeMillis() - cachedData.lastTime > 60_000) cachedData.times = 0
 
-            if (lastTime != null && System.currentTimeMillis() - lastTime < 60_000) {
-                val count = data.first + 1
-                Scheduler.scheduleTask(0) {
-                    ChatUtils.removeLines {
-                        it.content?.string?.clearCodes() == msg || ChatUtils.chatLineIds[it] == msg.hashCode()
-                    }
-                    event.cancel()
-                    ChatUtils.sendMessageWithId(
-                        event.text.copy().append(ChatUtils.literal(" &7($count)")),
-                        msg.hashCode()
-                    )
-                    chatList[msg] = Pair(count, System.currentTimeMillis())
-                }
-                return@on
+        cachedData.times++
+        cachedData.lastTime = System.currentTimeMillis()
+        if (cachedData.times <= 1) return text
+
+        val textCopy = text.copy()
+        val iter = ChatUtils.chatComponentAccessor.messages.listIterator()
+
+        while (iter.hasNext()) {
+            val line = iter.next()
+            val contentCopy = line.content.copy()
+
+            contentCopy.siblings.removeIf { it.contents is CompactChatComponent }
+
+            if (contentCopy.string.equals(textCopy.string)) {
+                iter.remove()
+                ChatUtils.chatComponentAccessor.invokeRefresh()
+                break
             }
-
-            chatList[msg] = Pair(1, System.currentTimeMillis())
         }
+
+        return textCopy.append(CompactChatComponent.of(cachedData.times).withStyle(STYLE))
     }
+
+    fun clearHistory() {
+        chatHistory.clear()
+    }
+}
+
+// Credits to <https://github.com/caoimhebyrne/compact-chat>
+// Licensed under the MIT license
+class CompactChatComponent(val times: Int = 0) : PlainTextContents {
+    companion object {
+        @JvmStatic
+        fun of(times: Int): MutableComponent = MutableComponent.create(CompactChatComponent(times))
+    }
+
+    override fun text(): String = " ($times)"
+
+    override fun <T : Any?> visit(
+        styledContentConsumer: FormattedText.StyledContentConsumer<T>,
+        style: Style
+    ): Optional<T> {
+        return styledContentConsumer.accept(style, text())
+    }
+
+    override fun <T : Any?> visit(contentConsumer: FormattedText.ContentConsumer<T>): Optional<T> {
+        return contentConsumer.accept(text())
+    }
+
+    override fun toString(): String = "CompactChatComponent(x$times)"
 }
