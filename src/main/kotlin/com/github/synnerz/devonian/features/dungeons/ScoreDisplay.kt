a3c5b3ca42c643f67ba456683e4eb1f27c352310
Ch1ck3nNeedsRNG <chick_is_bored>
2025-11-24 22:45:44 -0500

    add illegalmap style score display

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/ScoreDisplay.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/ScoreDisplay.kt
@@ -11,6 +11,12 @@ object ScoreDisplay : TextHudFeature(
     "Dungeons",
     "catacombs"
 ) {
+    private val SETTING_ILLEGALMAP_FORMAT = addSwitch(
+        "illegapStyle",
+        "&4OVERRIDES OTHER SETTINGS",
+        "IllegalMap Score Style",
+        false
+    )
     private val SETTING_SHOW_SECRETS = addSwitch(
         "showSecrets",
         "",
@@ -28,18 +34,33 @@ object ScoreDisplay : TextHudFeature(
         score: Int,
         mimic: Boolean,
         prince: Boolean,
+        crypts: Int,
         secrets: Int,
         secretsRequired: Int,
         totalSecrets: Int,
         floor: FloorType
     ): List<String> {
+        val tierColor = when {
+            score < 100 -> "&c"
+            score < 160 -> "&9"
+            score < 230 -> "&a"
+            score < 270 -> "&5"
+            score < 300 -> "&e"
+            else -> "&6&l"
+        }
+        val m = if (mimic) "&a&l✔" else "&c&l✘"
+        val p = if (prince) "&a&l✔" else "&c&l✘"
+        if (SETTING_ILLEGALMAP_FORMAT.get()) return listOf(
+            "&7Secrets: &b$secrets&7-&e${secretsRequired - secrets}&7-&c$totalSecrets &8| &7Score: $tierColor$score",
+            "&7Crypts ${if (crypts >= 5) "&a" else "&c"}$crypts &8| &7M: $m &8| &7P: $p"
+        )
         val tier = when {
-            score < 100 -> "&cD"
-            score < 160 -> "&9C"
-            score < 230 -> "&aB"
-            score < 270 -> "&5A"
-            score < 300 -> "&eS"
-            else -> "&6&lS+"
+            score < 100 -> "D"
+            score < 160 -> "C"
+            score < 230 -> "B"
+            score < 270 -> "A"
+            score < 300 -> "S"
+            else -> "S+"
         }
         val arr = mutableListOf(
             "&eScore: ${
@@ -48,22 +69,22 @@ object ScoreDisplay : TextHudFeature(
                     score < 300 -> "&e"
                     else -> "&a"
                 }
-            }$score &7(${tier}&r&7)"
+            }$score &7($tierColor$tier&r&7)"
         )
         if (SETTING_SHOW_SECRETS.get()) arr.add(
-            "&eSecrets: ${if (secrets >= secretsRequired) "&a" else "&c"}${secrets}&7/&a${secretsRequired}" +
+            "&eSecrets: ${if (secrets >= secretsRequired) "&a" else "&c"}$secrets&7/&a$secretsRequired" +
             (if (floor.requiredPercent == 1.0) "" else " &7(&6Total: $totalSecrets&7)")
         )
         if (SETTING_SHOW_MIMIC_PRINCE.get()) {
-            if (floor.floorNum >= 6) arr.add("&eMimic: ${if (mimic) "&a&l✔" else "&c&l✘"}")
-            arr.add("&ePrince: ${if (prince) "&a&l✔" else "&c&l✘"}")
+            if (floor.floorNum >= 6) arr.add("&eMimic: $m")
+            arr.add("&ePrince: $p")
         }
         return arr
     }
 
     override fun getEditText(): List<String> = getLines(
         301,
-        false, true,
+        false, true, 4,
         54, 61, 61,
         FloorType.M7
     )
@@ -75,6 +96,7 @@ object ScoreDisplay : TextHudFeature(
                     Dungeons.score.value,
                     Dungeons.mimicKilled.value,
                     Dungeons.princeKilled.value,
+                    Dungeons.crypts.value,
                     Dungeons.secretsFound.value,
                     Dungeons.totalSecretsRequired.value,
                     Dungeons.totalSecrets.value,
