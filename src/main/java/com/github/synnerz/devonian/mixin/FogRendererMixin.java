7cbfe41d3957dabe38ebcb8ef89694e274443917
Ch1ck3nNeedsRNG <chick_is_bored>
2025-11-25 17:10:13 -0500

    fix world not rendering

--- src/main/java/com/github/synnerz/devonian/mixin/FogRendererMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/FogRendererMixin.java
@@ -1,24 +1,21 @@
 package com.github.synnerz.devonian.mixin;
 
 import com.github.synnerz.devonian.features.misc.DisableFog;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.client.renderer.fog.environment.FogEnvironment;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.material.FogType;
+import net.minecraft.client.renderer.fog.FogRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(net.minecraft.client.renderer.fog.FogRenderer.class)
 public class FogRendererMixin {
-    @WrapOperation(
-        method = "setupFog",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/fog/environment/FogEnvironment;isApplicable(Lnet/minecraft/world/level/material/FogType;Lnet/minecraft/world/entity/Entity;)Z")
+    @ModifyVariable(
+        method = "getBuffer",
+        at = @At("HEAD"),
+        ordinal = 0,
+        argsOnly = true
     )
-    private static boolean devonian$disableFog(FogEnvironment instance, FogType fogType, Entity entity, Operation<Boolean> original) {
-        if (!DisableFog.INSTANCE.isEnabled()) return false;
-        Boolean val = DisableFog.INSTANCE.setupFog(instance, fogType, entity);
-        if (val == null) return original.call(instance, fogType, entity);
-        return val;
+    private static FogRenderer.FogMode devonian$disableFog(FogRenderer.FogMode fogMode) {
+        if (!DisableFog.INSTANCE.isEnabled()) return fogMode;
+        return FogRenderer.FogMode.NONE;
     }
 }
