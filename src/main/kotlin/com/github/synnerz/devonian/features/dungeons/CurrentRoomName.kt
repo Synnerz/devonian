3d5a2c41206cef05101f3f596a26b111bddebf37
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-27 12:02:07 -0700

    feat: fix `CurrentRoomName` rendering in boss

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/CurrentRoomName.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/CurrentRoomName.kt
@@ -1,6 +1,7 @@
 package com.github.synnerz.devonian.features.dungeons
 
 import com.github.synnerz.devonian.api.dungeon.DungeonScanner
+import com.github.synnerz.devonian.api.dungeon.Dungeons
 import com.github.synnerz.devonian.api.events.RenderOverlayEvent
 import com.github.synnerz.devonian.hud.texthud.TextHudFeature
 
@@ -12,6 +13,7 @@ object CurrentRoomName : TextHudFeature(
 ) {
     override fun initialize() {
         on<RenderOverlayEvent> {
+            if (Dungeons.inBoss.value) return@on
             val currentRoom = DungeonScanner.currentRoom ?: return@on
             if (currentRoom.name != null)
                 setLine("&bRoom&f: &a${currentRoom.name}")
