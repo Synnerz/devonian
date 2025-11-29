8840b91a76d1ca83d3e7d73a9ddbf168796eba22
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-29 13:21:09 -0700

    feat: change `ThreeWeirdosSolver` to highlight wrong chest

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/solvers/ThreeWeirdosSolver.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/solvers/ThreeWeirdosSolver.kt
@@ -8,7 +8,6 @@ import com.github.synnerz.devonian.features.Feature
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.phys.Vec3
 import java.awt.Color
-import kotlin.math.round
 
 object ThreeWeirdosSolver : Feature(
     "threeWeirdosSolver",
@@ -27,16 +26,38 @@ object ThreeWeirdosSolver : Feature(
         "The reward isn't in any of our chests.?".toRegex(),
         "Both of them are telling the truth\\. Also, \\w+ has the reward in their chest.?".toRegex()
     )
+    private val wrongAnswers = listOf(
+        "One of us is telling the truth!".toRegex(),
+        "They are both telling the truth\\. The reward isn't in \\w+'s chest.".toRegex(),
+        "We are all telling the truth!".toRegex(),
+        "\\w+ is telling the truth and the reward is in his chest.".toRegex(),
+        "My chest doesn't have the reward. At least one of the others is telling the truth!".toRegex(),
+        "One of the others is lying.".toRegex(),
+        "They are both telling the truth, the reward is in \\w+'s chest.".toRegex(),
+        "They are both lying, the reward is in my chest!".toRegex(),
+        "The reward is in my chest.".toRegex(),
+        "The reward is not in my chest\\. They are both lying.".toRegex(),
+        "\\w+ is telling the truth.".toRegex(),
+        "My chest has the reward.".toRegex()
+    )
     private val dirs = listOf(
         1 to 0,
         -1 to 0,
         0 to 1,
         0 to -1
     )
-    private val FILLED_COLOR = Color(0, 255, 0, 80)
+    private val CORRECT_OUTLINE_COLOR = Color(0, 255, 0, 255)
+    private val CORRECT_FILLED_COLOR = Color(0, 255, 0, 80)
+    private val WRONG_OUTLINE_COLOR = Color(255, 0, 0, 255)
+    private val WRONG_FILLED_COLOR = Color(255, 0, 0, 80)
     private val entityList = mutableMapOf<String, Int>()
-    var currentChest: Pair<Double, Double>? = null
-    var currentEntity: Vec3? = null
+    val answers = mutableListOf<AnswerData>()
+
+    data class AnswerData(
+        var entityPos: Vec3,
+        var chestPos: Pair<Double, Double>,
+        var isCorrect: Boolean
+    )
 
     override fun initialize() {
         on<ChatEvent> { event ->
@@ -52,11 +73,21 @@ object ThreeWeirdosSolver : Feature(
 
             val match = event.matches(npcRegex) ?: return@on
             val ( name, message ) = match
+            if (wrongAnswers.any { it.matches(message) }) {
+                getChest(name, false)?.let {
+                    if (answers.contains(it)) return@on
+                    answers.add(it)
+                }
+                return@on
+            }
             if (!solutions.any { it.matches(message) }) return@on
 
             event.cancel()
             ChatUtils.sendMessage("&e[NPC] &b&l$name: &a&l$message")
-            getChest(name)
+            getChest(name, true)?.let {
+                if (answers.contains(it)) return@on
+                answers.add(it)
+            }
         }
 
         on<NameChangeEvent> { event ->
@@ -64,24 +95,36 @@ object ThreeWeirdosSolver : Feature(
         }
 
         on<RenderWorldEvent> {
-            if (currentChest == null) return@on
-            Context.Immediate?.renderBox(
-                currentChest!!.first, 69.0, currentChest!!.second,
-                color = Color.GREEN,
-                phase = true
-            )
-            Context.Immediate?.renderFilledBox(
-                currentChest!!.first, 69.0, currentChest!!.second,
-                color = FILLED_COLOR,
-                phase = true
-            )
-            if (currentEntity == null) return@on
-            Context.Immediate?.renderFilledBox(
-                currentEntity!!.x - 0.5, currentEntity!!.y, currentEntity!!.z - 0.5,
-                0.8, 2.0,
-                FILLED_COLOR,
-                true
-            )
+            if (answers.isEmpty()) return@on
+            answers.forEach {
+                val entityPos = it.entityPos
+                val chestPos = it.chestPos
+                val isCorrect = it.isCorrect
+
+                // Render chest highlight
+                Context.Immediate?.renderBox(
+                    chestPos.first, 69.0, chestPos.second,
+                    color = if (isCorrect) CORRECT_OUTLINE_COLOR else WRONG_OUTLINE_COLOR,
+                    true
+                )
+                Context.Immediate?.renderFilledBox(
+                    chestPos.first, 69.0, chestPos.second,
+                    color = if (isCorrect) CORRECT_FILLED_COLOR else WRONG_FILLED_COLOR
+                )
+
+                // Render entity highlight
+                Context.Immediate?.renderBox(
+                    entityPos.x - 0.5, entityPos.y, entityPos.z - 0.5,
+                    0.8, 2.0,
+                    if (isCorrect) CORRECT_OUTLINE_COLOR else WRONG_OUTLINE_COLOR,
+                    true
+                )
+                Context.Immediate?.renderFilledBox(
+                    entityPos.x - 0.5, entityPos.y, entityPos.z - 0.5,
+                    0.8, 2.0,
+                    if (isCorrect) CORRECT_FILLED_COLOR else WRONG_FILLED_COLOR
+                )
+            }
         }
     }
 
@@ -90,24 +133,26 @@ object ThreeWeirdosSolver : Feature(
     }
 
     private fun reset() {
-        currentChest = null
-        currentEntity = null
+        answers.clear()
         entityList.clear()
     }
 
-    private fun getChest(name: String) {
-        val entityId = entityList[name] ?: return
-        val entity = minecraft.level?.getEntity(entityId) ?: return
+    private fun getChest(name: String, isCorrect: Boolean): AnswerData? {
+        val entityId = entityList[name] ?: return null
+        val entity = minecraft.level?.getEntity(entityId) ?: return null
         val pos = entity.position()
         val x0 = pos.x - 0.5
         val z0 = pos.z - 0.5
-        currentEntity = pos
+        var chestPos: Pair<Double, Double>? = null
 
         for (dir in dirs) {
             val ( dx, dz ) = dir
             val blockAt = WorldUtils.getBlockState(x0 + dx, 69.0, z0 + dz) ?: continue
             if (blockAt.block != Blocks.CHEST) continue
-            currentChest = x0 + dx to z0 + dz
+            chestPos = x0 + dx to z0 + dz
         }
+        if (chestPos == null) return null
+
+        return AnswerData(pos, chestPos, isCorrect)
     }
 }
