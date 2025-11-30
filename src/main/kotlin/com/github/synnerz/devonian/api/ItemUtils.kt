f70704be67a3f7a33832000f66631b295acce50c
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-30 13:47:22 -0700

    internal: add color codes opt to `ItemUtils#lore`

--- src/main/kotlin/com/github/synnerz/devonian/api/ItemUtils.kt
+++ src/main/kotlin/com/github/synnerz/devonian/api/ItemUtils.kt
@@ -1,5 +1,6 @@
 package com.github.synnerz.devonian.api
 
+import com.github.synnerz.devonian.utils.StringUtils.colorCodes
 import net.minecraft.core.component.DataComponents
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.world.item.ItemStack
@@ -25,8 +26,10 @@ object ItemUtils {
         return uuid.get()
     }
 
-    fun lore(itemStack: ItemStack): List<String>? {
+    fun lore(itemStack: ItemStack, colorCodes: Boolean = false): List<String>? {
         val lore = itemStack.get(DataComponents.LORE)?.lines ?: return null
+
+        if (colorCodes) return lore.map { it.colorCodes() }
 
         return lore.map { it.string }
     }
