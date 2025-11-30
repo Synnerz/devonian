c7480a9c51b20a61668b2cd4a3765a5904afc831
Doc <65479309+DocilElm@users.noreply.github.com>
2025-11-30 13:46:48 -0700

    internal: add `Component#colorCodes` to StringUtil

--- src/main/kotlin/com/github/synnerz/devonian/features/inventory/InventoryHistoryLog.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/inventory/InventoryHistoryLog.kt
@@ -6,11 +6,7 @@ import com.github.synnerz.devonian.api.events.TickEvent
 import com.github.synnerz.devonian.api.events.WorldChangeEvent
 import com.github.synnerz.devonian.hud.texthud.TextHudFeature
 import com.github.synnerz.devonian.utils.Location
-import net.minecraft.ChatFormatting
-import net.minecraft.network.chat.Component
-import net.minecraft.network.chat.Style
-import net.minecraft.network.chat.TextColor
-import java.util.*
+import com.github.synnerz.devonian.utils.StringUtils.colorCodes
 import kotlin.math.abs
 
 object InventoryHistoryLog : TextHudFeature(
@@ -55,7 +51,7 @@ object InventoryHistoryLog : TextHudFeature(
                 if (i == 8) return@forEachIndexed
                 if (v.isEmpty) return@forEachIndexed
 
-                val name = v.customName?.format() ?: v.itemName.string
+                val name = v.customName?.colorCodes() ?: v.itemName.string
                 val count = v.count
                 newInv.merge(name.clearName(), count, Int::plus)
                 inventory?.merge(name.clearName(), -count, Int::plus)
@@ -85,42 +81,4 @@ object InventoryHistoryLog : TextHudFeature(
     private fun String.clearName(): String = this.replace(" ?§r§8 ?x\\d+".toRegex(), "").trim()
 
     override fun getEditText(): List<String> = listOf("&c-100&r &eSocial Credit")
-
-    private val colorToFormat = ChatFormatting.entries.mapNotNull { format ->
-        TextColor.fromLegacyFormat(format)?.let { it to format }
-    }.toMap()
-
-    private fun parseStyle(style: Style): String = buildString {
-        append("§r")
-
-        style.color?.let(colorToFormat::get)?.run(::append)
-
-        when {
-            style.isBold -> append("§l")
-            style.isItalic -> append("§o")
-            style.isUnderlined -> append("§n")
-            style.isStrikethrough -> append("§m")
-            style.isObfuscated -> append("§k")
-        }
-    }
-
-    private fun parseFormat(_text: Component): String {
-        var str = ""
-
-        _text.contents.visit({ style, text ->
-            val styleFormat = parseStyle(style)
-            str += "${styleFormat}$text"
-            Optional.empty<Any>()
-        }, _text.style)
-
-        return str
-    }
-
-    private fun Component.format(): String {
-        var str = parseFormat(this)
-
-        str += this.siblings.joinToString("", transform = ::parseFormat)
-
-        return str
-    }
 }
