ec2ad5c762bccd8ed0f04cf8a71fc82611f09613
Doc <65479309+DocilElm@users.noreply.github.com>
2025-12-02 02:52:28 -0700

    feat: change `ScoreDisplay` to use `minSecrets`

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/ScoreDisplay.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/ScoreDisplay.kt
@@ -4,7 +4,6 @@ import com.github.synnerz.devonian.api.dungeon.Dungeons
 import com.github.synnerz.devonian.api.dungeon.FloorType
 import com.github.synnerz.devonian.api.events.RenderOverlayEvent
 import com.github.synnerz.devonian.hud.texthud.TextHudFeature
-import kotlin.math.max
 
 object ScoreDisplay : TextHudFeature(
     "dungeonScoreDisplay",
@@ -39,6 +38,7 @@ object ScoreDisplay : TextHudFeature(
         secrets: Int,
         secretsRequired: Int,
         totalSecrets: Int,
+        remainingSecrets: Int,
         floor: FloorType
     ): List<String> {
         val tierColor = when {
@@ -52,7 +52,7 @@ object ScoreDisplay : TextHudFeature(
         val m = if (mimic) "&a&l✔" else "&c&l✘"
         val p = if (prince) "&a&l✔" else "&c&l✘"
         if (SETTING_ILLEGALMAP_FORMAT.get()) return listOf(
-            "&7Secrets: &b$secrets&7-&e${max(secretsRequired - secrets, 0)}&7-&c$totalSecrets &8| &7Score: $tierColor$score",
+            "&7Secrets: &b$secrets&7-&e${remainingSecrets}&7-&c$totalSecrets &8| &7Score: $tierColor$score",
             "&7Crypts ${if (crypts >= 5) "&a" else "&c"}$crypts &8| &7M: $m &8| &7P: $p"
         )
         val tier = when {
@@ -87,6 +87,7 @@ object ScoreDisplay : TextHudFeature(
         301,
         false, true, 4,
         54, 61, 61,
+        0,
         FloorType.M7
     )
 
@@ -103,6 +104,7 @@ object ScoreDisplay : TextHudFeature(
                     Dungeons.secretsFound.value,
                     Dungeons.totalSecretsRequired.value,
                     Dungeons.totalSecrets.value,
+                    Dungeons.remainingMinSecrets.value,
                     Dungeons.floor
                 )
             )
