0a1e3197cdd678064617dbd480571c5015a007d3
Doc <65479309+DocilElm@users.noreply.github.com>
2025-12-02 23:44:02 -0700

    internal: add helper methods to `ScreenUtils`

--- src/main/kotlin/com/github/synnerz/devonian/api/ScreenUtils.kt
+++ src/main/kotlin/com/github/synnerz/devonian/api/ScreenUtils.kt
@@ -1,8 +1,15 @@
 package com.github.synnerz.devonian.api
 
 import com.github.synnerz.devonian.Devonian
+import com.github.synnerz.devonian.features.inventory.ProtectItem.minecraft
+import com.github.synnerz.devonian.mixin.accessor.AbstractContainerScreenAccessor
+import net.minecraft.client.gui.screens.Screen
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
+import net.minecraft.client.gui.screens.inventory.ContainerScreen
+import net.minecraft.client.gui.screens.inventory.InventoryScreen
 import net.minecraft.world.inventory.ClickType
+import net.minecraft.world.inventory.Slot
+import net.minecraft.world.item.ItemStack
 
 object ScreenUtils {
     @JvmOverloads
@@ -28,5 +35,25 @@ object ScreenUtils {
                 slot, clickButton, clickMode, it
             )
         }
+    }
+
+    fun cursorStack(screen: Screen): ItemStack? = cursorSlot(screen)?.item
+
+    fun cursorSlot(screen: Screen): Slot? {
+        if (!(screen is InventoryScreen || screen is ContainerScreen)) return null
+        val accessor = screen as AbstractContainerScreenAccessor
+        val window = minecraft.window ?: return null
+
+        return accessor.getSlotAtPos(
+            minecraft.mouseHandler.getScaledXPos(window),
+            minecraft.mouseHandler.getScaledYPos(window)
+        )
+    }
+
+    fun swap(from: Int, to: Int) {
+        minecraft.player ?: return
+        val screenId = ((minecraft.screen ?: return) as InventoryScreen).menu?.containerId ?: return
+
+        minecraft.gameMode?.handleInventoryMouseClick(screenId, from, to, ClickType.SWAP, minecraft.player!!)
     }
 }
