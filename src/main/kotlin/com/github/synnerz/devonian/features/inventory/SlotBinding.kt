9ee4a2984981338d39bb1891977dda80f66c7df4
Doc <65479309+DocilElm@users.noreply.github.com>
2025-12-06 03:58:12 -0700

    its actually left doc

--- src/main/kotlin/com/github/synnerz/devonian/features/inventory/SlotBinding.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/inventory/SlotBinding.kt
@@ -18,7 +18,7 @@ import org.lwjgl.glfw.GLFW
 
 object SlotBinding : Feature(
     "slotBinding",
-    "Bind a slot to another slot (with keybind in controls) so you can shift + right click on it to swap each others' items around"
+    "Bind a slot to another slot (with keybind in controls) so you can shift + left click on it to swap each others' items around"
 ) {
     private const val KEY_NAME = "slotsBound"
     private val boundSlots = mutableListOf<SlotBindingData>()
