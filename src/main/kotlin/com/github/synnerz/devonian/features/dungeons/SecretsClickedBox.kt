659a351bf3c051b7f99b41ee3a9c0682bd624bf9
Doc <65479309+DocilElm@users.noreply.github.com>
2025-12-06 04:45:58 -0700

    internal: change features to new events

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/SecretsClickedBox.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/SecretsClickedBox.kt
@@ -2,16 +2,12 @@ package com.github.synnerz.devonian.features.dungeons
 
 import com.github.synnerz.barrl.Context
 import com.github.synnerz.devonian.api.Scheduler
+import com.github.synnerz.devonian.api.dungeon.DungeonEvent
 import com.github.synnerz.devonian.api.events.ChatEvent
-import com.github.synnerz.devonian.api.events.PacketSentEvent
 import com.github.synnerz.devonian.api.events.RenderWorldEvent
 import com.github.synnerz.devonian.api.events.WorldChangeEvent
 import com.github.synnerz.devonian.features.Feature
 import net.minecraft.core.BlockPos
-import net.minecraft.core.registries.BuiltInRegistries
-import net.minecraft.network.protocol.game.ServerboundUseItemOnPacket
-import net.minecraft.world.level.block.entity.BlockEntityType
-import net.minecraft.world.level.block.entity.SkullBlockEntity
 import java.awt.Color
 
 object SecretsClickedBox : Feature(
@@ -20,8 +16,6 @@ object SecretsClickedBox : Feature(
     "Dungeons",
     "catacombs"
 ) {
-    private val skullIds = listOf("e0f3e929-869e-3dca-9504-54c666ee6f23", "fed95410-aba1-39df-9b95-1d4f361eb66e")
-    private val allowedBlocks = listOf("minecraft:chest", "minecraft:lever", "minecraft:trapped_chest")
     private val lockedChestRegex = "^That chest is locked!$".toRegex()
     private val SETTING_BLOCK_COLOR = addColorPicker(
         "blockColor",
@@ -39,36 +33,11 @@ object SecretsClickedBox : Feature(
     var wasLocked = false
 
     override fun initialize() {
-        on<PacketSentEvent> { event ->
-            val packet = event.packet
-            if (packet !is ServerboundUseItemOnPacket) return@on
-            val result = packet.hitResult
-            val blockState = minecraft.level?.getBlockState(result.blockPos) ?: return@on
-            val registryName = BuiltInRegistries.BLOCK.getKey(blockState.block)
-
-            if (registryName.path == "player_head" && blockState.hasBlockEntity()) {
-                val entityBlock = minecraft.level?.getBlockEntity(result.blockPos) ?: return@on
-                if (entityBlock.type != BlockEntityType.SKULL) return@on
-                val skullBlock = entityBlock as SkullBlockEntity
-                val owner = skullBlock.ownerProfile ?: return@on
-                val id = owner.partialProfile().id ?: return@on
-
-                if (!skullIds.contains("$id")) return@on
-
-                clickedBlock = entityBlock.blockPos
-                val thisClickedBlock = clickedBlock
-                Scheduler.scheduleTask(20) {
-                    if (clickedBlock == thisClickedBlock) clickedBlock = null
-                }
-                return@on
-            }
-
-            if (!allowedBlocks.contains("${registryName.namespace}:${registryName.path}")) return@on
-
-            clickedBlock = result.blockPos
-            val thisClickedBlock = clickedBlock
+        on<DungeonEvent.SecretClicked> {
+            clickedBlock = BlockPos(it.x.toInt(), it.y.toInt(), it.z.toInt())
+            val prevBlock = clickedBlock
             Scheduler.scheduleTask(20) {
-                if (clickedBlock != thisClickedBlock) return@scheduleTask
+                if (clickedBlock != prevBlock) return@scheduleTask
                 clickedBlock = null
                 wasLocked = false
             }
--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/SecretsSound.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/SecretsSound.kt
@@ -1,19 +1,10 @@
 package com.github.synnerz.devonian.features.dungeons
 
 import com.github.synnerz.devonian.api.Scheduler
+import com.github.synnerz.devonian.api.dungeon.DungeonEvent
 import com.github.synnerz.devonian.api.events.ChatEvent
-import com.github.synnerz.devonian.api.events.PacketReceivedEvent
-import com.github.synnerz.devonian.api.events.PacketSentEvent
-import com.github.synnerz.devonian.api.events.SoundPlayEvent
 import com.github.synnerz.devonian.features.Feature
-import net.minecraft.core.component.DataComponents
-import net.minecraft.core.registries.BuiltInRegistries
-import net.minecraft.network.protocol.game.ClientboundTakeItemEntityPacket
-import net.minecraft.network.protocol.game.ServerboundUseItemOnPacket
 import net.minecraft.sounds.SoundEvents
-import net.minecraft.world.entity.item.ItemEntity
-import net.minecraft.world.level.block.entity.BlockEntityType
-import net.minecraft.world.level.block.entity.SkullBlockEntity
 
 object SecretsSound : Feature(
     "secretsSound",
@@ -21,70 +12,21 @@ object SecretsSound : Feature(
     "Dungeons",
     "catacombs"
 ) {
-    private val skullIds = listOf("e0f3e929-869e-3dca-9504-54c666ee6f23", "fed95410-aba1-39df-9b95-1d4f361eb66e")
-    private val allowedBlocks = listOf("minecraft:chest", "minecraft:lever", "minecraft:trapped_chest")
-    private val secretItems = setOf(
-        "Healing VIII Splash Potion", "Healing Potion 8 Splash Potion", "Decoy",
-        "Inflatable Jerry", "Spirit Leap", "Trap",
-        "Training Weights", "Defuse Kit", "Dungeon Chest Key",
-        "Treasure Talisman", "Revive Stone", "Architect's First Draft",
-        "Candycomb"
-    )
     private val lockedChestRegex = "^That chest is locked!$".toRegex()
     private val successSound = SoundEvents.BLAZE_HURT
     private val declineSound = SoundEvents.ANVIL_PLACE
 
     override fun initialize() {
-        on<PacketReceivedEvent> { event ->
-            val packet = event.packet
-            if (packet !is ClientboundTakeItemEntityPacket) return@on
-
-            val id = packet.itemId
-            val entityIn = minecraft.level?.getEntity(id) ?: return@on
-            if (entityIn !is ItemEntity) return@on
-            val itemStack = entityIn.item
-            val customName = itemStack.get(DataComponents.CUSTOM_NAME)?.string
-            if (!secretItems.contains(customName)) return@on
-
-            playSound()
-        }
-
-        on<PacketSentEvent> { event ->
-            val packet = event.packet
-            if (packet !is ServerboundUseItemOnPacket) return@on
-            val result = packet.hitResult
-            val blockState = minecraft.level?.getBlockState(result.blockPos) ?: return@on
-            val registryName = BuiltInRegistries.BLOCK.getKey(blockState.block)
-
-            if (registryName.path == "player_head" && blockState.hasBlockEntity()) {
-                val entityBlock = minecraft.level?.getBlockEntity(result.blockPos) ?: return@on
-                if (entityBlock.type != BlockEntityType.SKULL) return@on
-                val skullBlock = entityBlock as SkullBlockEntity
-                val owner = skullBlock.ownerProfile ?: return@on
-                val id = owner.partialProfile().id ?: return@on
-
-                if (!skullIds.contains("$id")) return@on
-
-                playSound()
-                return@on
-            }
-
-            if (!allowedBlocks.contains("${registryName.namespace}:${registryName.path}")) return@on
-
+        on<DungeonEvent.SecretPickup> { playSound() }
+        on<DungeonEvent.SecretClicked> { playSound() }
+        on<DungeonEvent.SecretBat> {
+            it.cancel()
             playSound()
         }
 
         on<ChatEvent> { event ->
             event.matches(lockedChestRegex) ?: return@on
             playSound(true)
-        }
-
-        on<SoundPlayEvent> { event ->
-            if (event.volume != 0.1f) return@on
-            if (event.sound == "minecraft:entity.bat.death" || event.sound == "minecraft:entity.bat.hurt") {
-                event.cancel()
-                playSound()
-            }
         }
     }
 
