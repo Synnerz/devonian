7260ed3a18ae6cccf6c3243fc9c87a717ed3e5b2
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2025-12-12 01:55:04 -0500

    `DisableWorldLoadingScreen` fix softlock on joining single player worlds

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/DisableWorldLoadingScreen.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/DisableWorldLoadingScreen.kt
@@ -2,17 +2,21 @@ package com.github.synnerz.devonian.features.misc
 
 import com.github.synnerz.devonian.api.events.GuiOpenEvent
 import com.github.synnerz.devonian.features.Feature
+import com.github.synnerz.devonian.mixin.accessor.LevelLoadingScreenAccessor
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.screens.LevelLoadingScreen
 import net.minecraft.client.gui.screens.Overlay
 
 object DisableWorldLoadingScreen : Feature("disableWorldLoadingScreen") {
+    private var levelLoadingScreen: LevelLoadingScreenAccessor? = null
+
     override fun initialize() {
         on<GuiOpenEvent> { event ->
             if (event.screen !is LevelLoadingScreen) {
                 if (minecraft.overlay is PausingOverlay) minecraft.overlay = null
                 return@on
             }
+            levelLoadingScreen = event.screen as? LevelLoadingScreenAccessor
             event.cancel()
             minecraft.setScreen(null)
             minecraft.overlay = PausingOverlay
@@ -27,5 +31,10 @@ object DisableWorldLoadingScreen : Feature("disableWorldLoadingScreen") {
 
     object PausingOverlay : Overlay() {
         override fun render(guiGraphics: GuiGraphics, i: Int, j: Int, f: Float) {}
+
+        override fun tick() {
+            val screen = levelLoadingScreen ?: return
+            if (screen.loadTracker.isLevelReady || (minecraft.singleplayerServer?.isReady ?: false)) minecraft.overlay = null
+        }
     }
 }
