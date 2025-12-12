d1fd1736c6a5b2a87becbca936644f909a673e15
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2025-12-12 01:23:57 -0500

    add hint for /dv font

--- src/main/kotlin/com/github/synnerz/devonian/features/HudManagerHider.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/HudManagerHider.kt
@@ -3,5 +3,8 @@ package com.github.synnerz.devonian.features
 import com.github.synnerz.devonian.hud.texthud.TextHudFeature
 
 object HudManagerHider : TextHudFeature("hudManagerHider", isInternal = true) {
-    override fun getEditText(): List<String> = listOf("Hide &cDisabled&r HUDs")
+    override fun getEditText(): List<String> = listOf("Hide &cDisabled&r HUDs").let {
+        if (isEnabled()) listOf(it[0], "Hint: /dv font <name>")
+        else it
+    }
 }
