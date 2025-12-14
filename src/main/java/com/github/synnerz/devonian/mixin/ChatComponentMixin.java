624ee83e9efd5d24f51854e8fc97c8d93d9fab96
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2025-12-14 01:39:25 -0500

    change `allMessages` to LinkedList

--- src/main/java/com/github/synnerz/devonian/mixin/ChatComponentMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/ChatComponentMixin.java
@@ -10,12 +10,10 @@ import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Mixin(ChatComponent.class)
@@ -80,4 +78,7 @@ public class ChatComponentMixin {
     private void devonian$refreshTrimmedMessages(CallbackInfo ci) {
         ChatUtils.INSTANCE.getLineCache().clear();
     }
+
+    @Shadow
+    private List<GuiMessage> allMessages = new LinkedList<>();
 }
