3feb569d7a4769ccc154f4494d7780faffe45303
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2025-12-14 13:21:59 -0500

    rewrite `ProtectItem`

--- src/main/kotlin/com/github/synnerz/devonian/features/inventory/ProtectItem.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/inventory/ProtectItem.kt
@@ -3,25 +3,18 @@ package com.github.synnerz.devonian.features.inventory
 import com.github.synnerz.devonian.Devonian
 import com.github.synnerz.devonian.api.ChatUtils
 import com.github.synnerz.devonian.api.ItemUtils
-import com.github.synnerz.devonian.api.Scheduler
-import com.github.synnerz.devonian.api.ScreenUtils
-import com.github.synnerz.devonian.api.dungeon.Dungeons
-import com.github.synnerz.devonian.api.events.CancellableEvent
-import com.github.synnerz.devonian.api.events.DropItemEvent
+import com.github.synnerz.devonian.api.events.GuiCloseEvent
 import com.github.synnerz.devonian.api.events.GuiKeyDownEvent
-import com.github.synnerz.devonian.api.events.GuiSlotClickEvent
+import com.github.synnerz.devonian.api.events.GuiKeyUpEvent
 import com.github.synnerz.devonian.config.Config
 import com.github.synnerz.devonian.features.Feature
-import com.github.synnerz.devonian.utils.Location
+import com.github.synnerz.devonian.mixin.accessor.AbstractContainerScreenAccessor
+import com.github.synnerz.devonian.utils.StringUtils.colorCodes
 import com.google.gson.JsonArray
 import com.google.gson.JsonPrimitive
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.minecraft.client.KeyMapping
-import net.minecraft.client.gui.screens.inventory.ContainerScreen
-import net.minecraft.resources.ResourceLocation
-import net.minecraft.sounds.SoundEvent
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.level.block.Blocks
 import org.lwjgl.glfw.GLFW
 
 object ProtectItem : Feature(
@@ -30,8 +23,7 @@ object ProtectItem : Feature(
     subcategory = "Inventory",
 ) {
     private const val KEY_NAME = "protectedItems"
-    private var lockedList = mutableListOf<String>()
-    private val hopperItem = Blocks.HOPPER.asItem()
+    private var lockedList = mutableSetOf<String>()
     private val keybind = KeyBindingHelper.registerKeyBinding(
         KeyMapping(
             "key.devonian.protectItem",
@@ -39,81 +31,63 @@ object ProtectItem : Feature(
             Devonian.keybindCategory
         )
     )
+    private var once = false
 
     override fun initialize() {
         Config.set(KEY_NAME, JsonArray())
 
         Config.onAfterLoad {
-            lockedList = Config.get<List<JsonPrimitive>>(KEY_NAME)?.map { it.asString } ?.toMutableList() ?: mutableListOf()
+            lockedList =
+                Config.get<List<JsonPrimitive>>(KEY_NAME)?.map { it.asString }?.toMutableSet() ?: mutableSetOf()
         }
 
-        on<DropItemEvent> { event ->
-            if (Location.area == "catacombs" && Dungeons.timeElapsed.value != 0 && minecraft.screen == null) return@on
-            val stack = event.itemStack
-            val uuid = ItemUtils.uuid(stack) ?: return@on
-            if (!lockedList.contains(uuid)) return@on
-            cancelEvent(event, stack)
-        }
+        Config.onPreSave {
+            val array = JsonArray()
 
-        on<GuiSlotClickEvent> { event ->
-            if (event.slot == null) return@on
-            if (event.slot.container !== minecraft.player?.inventory) return@on
-            val screen = minecraft.screen ?: return@on
-            if (screen !is ContainerScreen) return@on
-            val possibleHopper = event.handler.getSlot(49)?.item
-            if (possibleHopper != null && event.slotId != 49 && isLocked(event.slot.item)) {
-                val lore = ItemUtils.lore(possibleHopper)
-                if (possibleHopper.item == hopperItem && possibleHopper.customName?.string == "Sell Item")
-                    cancelEvent(event, event.slot.item)
-                else if (lore != null && lore.any { it.contains("buyback") })
-                    cancelEvent(event, event.slot.item)
-            }
-            if (!screen.title.string.contains("Auction") || !isLocked(event.slot.item)) return@on
+            lockedList.forEach { array.add(it) }
 
-            cancelEvent(event, event.slot.item)
+            Config.set(KEY_NAME, array)
         }
 
         on<GuiKeyDownEvent> { event ->
             if (!keybind.matches(event.event)) return@on
-            val stack = ScreenUtils.cursorStack(event.screen) ?: return@on
+            if (once) return@on
+            once = true
+
+            val screen = event.screen as? AbstractContainerScreenAccessor ?: return@on
+            val stack = screen.hoveredSlot?.item ?: return@on
+
             val uuid = ItemUtils.uuid(stack) ?: return@on
             val msg = if (lockedList.contains(uuid)) "&cRemoved" else "&aAdded"
 
             if (lockedList.contains(uuid)) lockedList.remove(uuid)
             else lockedList.add(uuid)
-            updateCache()
 
-            ChatUtils.sendMessage("&bProtect item $msg &b${stack.customName?.string ?: stack.itemName.string}", true)
+            ChatUtils.sendMessage("&bProtect item $msg &b${stack.customName?.colorCodes() ?: stack.itemName.string}", true)
+        }
+
+        on<GuiKeyUpEvent> { event ->
+            if (!keybind.matches(event.event)) return@on
+            once = false
+        }
+
+        on<GuiCloseEvent> {
+            once = false
+        }
+
+        on<PreventItem.SlotEvent> { event ->
+            if (!event.losesItem) return@on
+
+            if (!isLocked(event.item)) return@on
+
+            event.cancel("ProtectItem")
         }
     }
 
     private fun isLocked(itemStack: ItemStack?): Boolean {
         if (itemStack == null) return false
+        if (itemStack.isEmpty) return false
         val uuid = ItemUtils.uuid(itemStack) ?: return false
         return lockedList.contains(uuid)
-    }
-
-    private fun updateCache() {
-        val array = JsonArray()
-
-        lockedList.forEach { array.add(it) }
-
-        Config.set(KEY_NAME, array)
-    }
-
-    private fun cancelEvent(event: CancellableEvent, stack: ItemStack) {
-        Scheduler.scheduleTask(1) {
-            minecraft.player?.playSound(
-                SoundEvent.createVariableRangeEvent(ResourceLocation.withDefaultNamespace("block.note_block.bass")),
-                1f,
-                1f
-            )
-        }
-        event.cancel()
-
-        val customName = stack.customName ?: ChatUtils.literal("&cName not found")
-        val text = ChatUtils.literal("${ChatUtils.prefix} &cCancelled attempt at dropping ")
-            .append(customName)
-        ChatUtils.sendMessage(text)
     }
 }
