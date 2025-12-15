427ff9e9c8259b2a24534a006622908cc9c45e55
Doc <65479309+DocilElm@users.noreply.github.com>
2025-12-15 11:58:21 -0700

    wasn't me

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/RefillGFSCommands.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/RefillGFSCommands.kt
@@ -9,22 +9,22 @@ object RefillGFSCommands {
 
     fun initialize() {
         DevonianCommand.command.subcommand("pearls") { _, args ->
-            inventory ?: return@subcommand 0
-            val amount = inventory!!.sumOf { if ((it.customName?.string ?: it.displayName.string).contains("Ender Pearl")) it.count else 0 }
+            val inv = inventory ?: return@subcommand 0
+            val amount = inv.sumOf { if ((it.customName?.string ?: it.displayName.string).contains("Ender Pearl")) it.count else 0 }
             ChatUtils.command("gfs ender pearl ${16 - amount}")
             1
         }
 
         DevonianCommand.command.subcommand("leaps") { _, args ->
-            inventory ?: return@subcommand 0
-            val amount = inventory!!.sumOf { if (it.customName?.string?.contains("Spirit Leap") == true) it.count else 0 }
+            val inv = inventory ?: return@subcommand 0
+            val amount = inv.sumOf { if (it.customName?.string?.contains("Spirit Leap") == true) it.count else 0 }
             ChatUtils.command("gfs spirit leap ${16 - amount}")
             1
         }
 
         DevonianCommand.command.subcommand("superbooms") { _, args ->
-            inventory ?: return@subcommand 0
-            val amount = inventory!!.sumOf { if (it.customName?.string?.contains("Superboom TNT") == true) it.count else 0 }
+            val inv = inventory ?: return@subcommand 0
+            val amount = inv.sumOf { if (it.customName?.string?.contains("Superboom TNT") == true) it.count else 0 }
             ChatUtils.command("gfs superboom tnt ${64 - amount}")
             1
         }
