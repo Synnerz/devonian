a90ebd9df1821e6e53c518f3a2a009ba099344ac
Doc <65479309+DocilElm@users.noreply.github.com>
2025-12-15 12:04:25 -0700

    internal: use skyblockIds instead

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/RefillGFSCommands.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/RefillGFSCommands.kt
@@ -2,6 +2,7 @@ package com.github.synnerz.devonian.features.dungeons
 
 import com.github.synnerz.devonian.Devonian
 import com.github.synnerz.devonian.api.ChatUtils
+import com.github.synnerz.devonian.api.ItemUtils
 import com.github.synnerz.devonian.commands.DevonianCommand
 
 object RefillGFSCommands {
@@ -10,21 +11,21 @@ object RefillGFSCommands {
     fun initialize() {
         DevonianCommand.command.subcommand("pearls") { _, args ->
             val inv = inventory ?: return@subcommand 0
-            val amount = inv.sumOf { if ((it.customName?.string ?: it.displayName.string).contains("Ender Pearl")) it.count else 0 }
+            val amount = inv.sumOf { if (ItemUtils.skyblockId(it) == "ENDER_PEARL") it.count else 0 }
             ChatUtils.command("gfs ender pearl ${16 - amount}")
             1
         }
 
         DevonianCommand.command.subcommand("leaps") { _, args ->
             val inv = inventory ?: return@subcommand 0
-            val amount = inv.sumOf { if (it.customName?.string?.contains("Spirit Leap") == true) it.count else 0 }
+            val amount = inv.sumOf { if (ItemUtils.skyblockId(it) == "SPIRIT_LEAP") it.count else 0 }
             ChatUtils.command("gfs spirit leap ${16 - amount}")
             1
         }
 
         DevonianCommand.command.subcommand("superbooms") { _, args ->
             val inv = inventory ?: return@subcommand 0
-            val amount = inv.sumOf { if (it.customName?.string?.contains("Superboom TNT") == true) it.count else 0 }
+            val amount = inv.sumOf { if (ItemUtils.skyblockId(it) == "SUPERBOOM_TNT") it.count else 0 }
             ChatUtils.command("gfs superboom tnt ${64 - amount}")
             1
         }
