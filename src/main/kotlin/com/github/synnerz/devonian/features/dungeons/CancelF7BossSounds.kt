2816b7bdd1e7ddd67826349bfa0ee768f149aecc
Doc <65479309+DocilElm@users.noreply.github.com>
2025-12-16 03:15:07 -0700

    last try i think idk

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/CancelF7BossSounds.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/CancelF7BossSounds.kt
@@ -15,42 +15,18 @@ object CancelF7BossSounds : Feature(
     override fun initialize() {
         on<SoundPlayEvent> { event ->
             if (!Dungeons.inBoss.value || Dungeons.floor.floorNum != 7) return@on
-            if (
-                event.sound == "minecraft:entity.wither.ambient" &&
-                (event.pitch == 0.4920635f && event.volume == 0.1f ||
-                event.pitch == 1.1904762f && event.volume == 5.0f)
-            ) event.cancel()
+            if (event.volume >= 2f) {
+                event.cancel()
+                return@on
+            }
 
-            // Louad wither ambient
-            if (
-                event.sound == "minecraft:entity.wither.ambient" &&
-                event.volume == 30.0f /*&&
-                event.pitch == 0.6984127f*/
-            ) event.cancel()
-
-            if (
-                event.sound == "minecraft:entity.lightning_bolt.thunder" &&
-                event.volume == 10000.0f /*&&
-                event.pitch == 0.8888889f*/
-            ) event.cancel()
-
-            // Loud wither hurt
             if (
                 event.sound == "minecraft:entity.wither.hurt" &&
-                event.volume in 15f..40f /*&&
-                (event.pitch == 1f || event.pitch == 0.4920635f)*/
-            ) event.cancel()
-
-            // Lower wither hurt
-            if (
-                event.sound == "minecraft:entity.wither.hurt" &&
-                (event.volume == 0.5f || event.volume == 5.0f) &&
+                event.volume == 0.5f &&
                 (event.pitch == 1.0f || event.pitch == 0.93650794f || event.pitch == 1.0158731f || event.pitch == 1.1111112f)
-            ) event.cancel()
-
-            if (event.sound == "minecraft:entity.generic.explode") {
-                if (event.volume in 2f..40f/* && event.pitch == 0.4920635f*/) event.cancel()
-                if (event.volume in 2f..4f/* && event.pitch in 0.52380955f..0.8095238f*/) event.cancel()
+            ) {
+                event.cancel()
+                return@on
             }
 
             if (
@@ -58,23 +34,6 @@ object CancelF7BossSounds : Feature(
                 event.volume == 0.5f &&
                 (event.pitch in 0.8730159f..1.1587301f)
             ) event.cancel()
-
-            if (
-                event.sound == "minecraft:entity.arrow.shoot" &&
-                event.volume == 8.0f
-            ) event.cancel()
-
-            if (
-                event.sound == "minecraft:entity.ender_dragon.flap" &&
-                event.volume == 8.0f
-            ) event.cancel()
-
-            if (
-                event.sound == "minecraft:entity.lightning_bolt.thunder" &&
-                event.volume == 50f
-            ) event.cancel()
-
-            if (event.sound == "minecraft:entity.wither.shoot") event.cancel()
         }
     }
 }
