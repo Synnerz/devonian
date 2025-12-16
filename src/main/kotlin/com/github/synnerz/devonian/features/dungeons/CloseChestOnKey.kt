6230b875370cc766892d6709fd810dd389627e0c
Doc <65479309+DocilElm@users.noreply.github.com>
2025-12-15 17:39:28 -0700

    feat: change `CloseChestOnKey` to better impl

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/CloseChestOnKey.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/CloseChestOnKey.kt
@@ -1,12 +1,10 @@
 package com.github.synnerz.devonian.features.dungeons
 
-import com.github.synnerz.devonian.api.ChatUtils
-import com.github.synnerz.devonian.api.Scheduler
+import com.github.synnerz.devonian.api.dungeon.Dungeons
 import com.github.synnerz.devonian.api.events.GuiKeyDownEvent
-import com.github.synnerz.devonian.api.events.PacketReceivedEvent
 import com.github.synnerz.devonian.config.Categories
 import com.github.synnerz.devonian.features.Feature
-import net.minecraft.network.protocol.game.ClientboundOpenScreenPacket
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
 import net.minecraft.network.protocol.game.ServerboundContainerClosePacket
 
 object CloseChestOnKey : Feature(
@@ -23,21 +21,16 @@ object CloseChestOnKey : Feature(
         minecraft.options.keyDown, // s
         minecraft.options.keyShift, // shift
     )
-    private var containerId = -1
 
     override fun initialize() {
-        on<PacketReceivedEvent> { event ->
-            val packet = event.packet
-            if (packet !is ClientboundOpenScreenPacket) return@on
-            val containerName = packet.title.string ?: return@on
-            if (containerName != "Chest") return@on
-
-            Scheduler.scheduleTask {
-                containerId = packet.containerId
-            }
-        }
-
         on<GuiKeyDownEvent> { event ->
+            if (Dungeons.inBoss.value) return@on
+            if (event.screen !is AbstractContainerScreen<*>) return@on
+
+            val container = event.screen
+            if (container.title.string != "Chest") return@on
+            val containerId = container.menu.containerId
+
             keybinds.forEach {
                 if (containerId == -1) return@on
                 if (!it.matches(event.event)) return@forEach
@@ -45,7 +38,6 @@ object CloseChestOnKey : Feature(
                 event.cancel()
                 minecraft.connection?.send(ServerboundContainerClosePacket(containerId))
                 minecraft.setScreen(null)
-                containerId = -1
                 return@on
             }
         }
