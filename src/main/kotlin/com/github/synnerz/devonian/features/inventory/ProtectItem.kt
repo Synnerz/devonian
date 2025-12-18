6e539fceb5672df26bf172a13e1b7ed26219499b
Doc <65479309+DocilElm@users.noreply.github.com>
2025-12-18 03:14:57 -0700

    feat: add peek keybind to `ProtectItem`

--- src/main/kotlin/com/github/synnerz/devonian/features/inventory/ProtectItem.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/inventory/ProtectItem.kt
@@ -3,9 +3,7 @@ package com.github.synnerz.devonian.features.inventory
 import com.github.synnerz.devonian.Devonian
 import com.github.synnerz.devonian.api.ChatUtils
 import com.github.synnerz.devonian.api.ItemUtils
-import com.github.synnerz.devonian.api.events.GuiCloseEvent
-import com.github.synnerz.devonian.api.events.GuiKeyDownEvent
-import com.github.synnerz.devonian.api.events.GuiKeyUpEvent
+import com.github.synnerz.devonian.api.events.*
 import com.github.synnerz.devonian.config.Config
 import com.github.synnerz.devonian.features.Feature
 import com.github.synnerz.devonian.mixin.accessor.AbstractContainerScreenAccessor
@@ -16,6 +14,7 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.minecraft.client.KeyMapping
 import net.minecraft.world.item.ItemStack
 import org.lwjgl.glfw.GLFW
+import java.awt.Color
 
 object ProtectItem : Feature(
     "protectItem",
@@ -31,7 +30,16 @@ object ProtectItem : Feature(
             Devonian.keybindCategory
         )
     )
+    private val peekKeybind = KeyBindingHelper.registerKeyBinding(
+        KeyMapping(
+            "key.devonian.protectItem.peek",
+            GLFW.GLFW_KEY_UNKNOWN,
+            Devonian.keybindCategory
+        )
+    )
+    private val PROTECTED_COLOR = Color.CYAN.rgb
     private var once = false
+    private var highlightSlots = false
 
     override fun initialize() {
         Config.set(KEY_NAME, JsonArray())
@@ -50,6 +58,11 @@ object ProtectItem : Feature(
         }
 
         on<GuiKeyDownEvent> { event ->
+            if (peekKeybind.matches(event.event)) {
+                highlightSlots = true
+                return@on
+            }
+
             if (!keybind.matches(event.event)) return@on
             if (once) return@on
             once = true
@@ -67,6 +80,11 @@ object ProtectItem : Feature(
         }
 
         on<GuiKeyUpEvent> { event ->
+            if (peekKeybind.matches(event.event)) {
+                highlightSlots = false
+                return@on
+            }
+
             if (!keybind.matches(event.event)) return@on
             once = false
         }
@@ -82,6 +100,16 @@ object ProtectItem : Feature(
 
             event.cancel("ProtectItem")
         }
+
+        on<RenderSlotEvent> { event ->
+            if (!highlightSlots) return@on
+            val slot = event.slot
+            val item = slot.item ?: return@on
+
+            if (!isLocked(item)) return@on
+
+            event.ctx.fill(slot.x, slot.y, slot.x + 16, slot.y + 16, PROTECTED_COLOR)
+        }.prio = 30
     }
 
     private fun isLocked(itemStack: ItemStack?): Boolean {
