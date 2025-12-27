857d277b63ac42b9dae3dd4201bf634e6c3b4236
Doc <65479309+DocilElm@users.noreply.github.com>
2025-12-27 14:51:56 -0700

    feat: fix `InventoryScale` not resetting properly

--- src/main/kotlin/com/github/synnerz/devonian/features/inventory/InventoryScale.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/inventory/InventoryScale.kt
@@ -1,9 +1,9 @@
 package com.github.synnerz.devonian.features.inventory
 
-import com.github.synnerz.devonian.api.events.GuiCloseEvent
-import com.github.synnerz.devonian.api.events.GuiOpenEvent
+import com.github.synnerz.devonian.api.events.*
 import com.github.synnerz.devonian.features.Feature
 import com.github.synnerz.devonian.features.dungeons.CustomTerminalScale
+import net.minecraft.client.gui.screens.Screen
 import net.minecraft.client.gui.screens.inventory.ContainerScreen
 import net.minecraft.client.gui.screens.inventory.InventoryScreen
 import kotlin.math.roundToInt
@@ -20,28 +20,49 @@ object InventoryScale : Feature(
         "0 = auto",
         "Inventory Scale Amount"
     )
+    private var lastScreen: Screen? = null
     private var oldScale = -1
 
     override fun initialize() {
-        on<GuiOpenEvent> { event ->
-            val screen = event.screen
-            if (screen is ContainerScreen || screen is InventoryScreen) {
-                val title = screen.title.string
-                if (CustomTerminalScale.shouldScale(title)) return@on
-                val guiScale = minecraft.options.guiScale()
+        on<TickEvent> {
+            val screen = minecraft.screen
+            if (lastScreen == null && screen != null) {
+                if (screen is ContainerScreen || screen is InventoryScreen) {
+                    val title = screen.title.string
+                    if (!CustomTerminalScale.shouldScale(title)) {
+                        val guiScale = minecraft.options.guiScale()
 
-                oldScale = guiScale.get()
-                guiScale.set(SETTING_SCALE.get().roundToInt())
+                        oldScale = guiScale.get()
+                        guiScale.set(SETTING_SCALE.get().roundToInt())
+                    }
+                }
+                lastScreen = screen
+                return@on
             }
-        }
 
-        on<GuiCloseEvent> { event ->
-            val screen = event.screen
-            if (screen is ContainerScreen || screen is InventoryScreen) {
-                if (oldScale == -1) return@on
+            if (lastScreen != null && screen != null && screen != lastScreen) {
+                if (screen is ContainerScreen || screen is InventoryScreen) {
+                    val title = screen.title.string
+                    if (!CustomTerminalScale.shouldScale(title) && oldScale == -1) {
+                        val guiScale = minecraft.options.guiScale()
 
-                minecraft.options.guiScale().set(oldScale)
-                oldScale = -1
+                        oldScale = guiScale.get()
+                        guiScale.set(SETTING_SCALE.get().roundToInt())
+                    }
+                }
+                lastScreen = screen
+                return@on
+            }
+
+            if (lastScreen != null && screen == null) {
+                if (lastScreen is ContainerScreen || lastScreen is InventoryScreen) {
+                    if (oldScale == -1) return@on
+
+                    minecraft.options.guiScale().set(oldScale)
+                    oldScale = -1
+                }
+                lastScreen = null
+                return@on
             }
         }
     }
