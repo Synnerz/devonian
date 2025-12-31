cf68ee8d34f20afe572e51e652020e124a4e8336
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2025-12-31 00:49:48 -0500

    change `remainingMinSecrets` to use current bonus score instead of max

--- src/main/kotlin/com/github/synnerz/devonian/api/dungeon/Dungeons.kt
+++ src/main/kotlin/com/github/synnerz/devonian/api/dungeon/Dungeons.kt
@@ -146,7 +146,7 @@ object Dungeons {
         .zip(speedScore) { a, b -> a + b }
         .zip(bonusScore) { a, b -> a + b }
 
-    val _targetScore = floorState.zip(isPaul) { floor, paul -> 40 - (1 + (if (floor.floorNum >= 6) 7 else 5) + (if (paul) 10 else 0)) }
+    val _targetScore = bonusScore_.map { 40 - it }
     val minSecrets = _targetScore.zip(totalSecretsRequired) { score, secrets -> ceil(score * secrets / 40.0).toInt() }
     val remainingMinSecrets = minSecrets.zip(secretsFound) { total, found -> max(0, total - found) }
 
