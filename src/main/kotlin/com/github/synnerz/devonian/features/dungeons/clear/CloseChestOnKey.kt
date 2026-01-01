7de2d6b4c56e17fde7268658916b4228b8f38933
Gaeritag <125013771+Gaeritag@users.noreply.github.com>
2026-01-01 13:33:06 +0100

    Added cheat version of close chest on key -> dont need key anymore.

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/clear/CloseChestOnKey.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/clear/CloseChestOnKey.kt
@@ -1,7 +1,7 @@
 package com.github.synnerz.devonian.features.dungeons.clear
 
 import com.github.synnerz.devonian.api.dungeon.Stages
-import com.github.synnerz.devonian.api.events.GuiKeyDownEvent
+import com.github.synnerz.devonian.api.events.GuiOpenEvent
 import com.github.synnerz.devonian.config.Categories
 import com.github.synnerz.devonian.features.Feature
 import com.github.synnerz.devonian.utils.BasicState
@@ -19,31 +19,20 @@ object CloseChestOnKey : Feature(
         return super.createRequirements() + listOf(Stages.Clear.isActiveState)
     }
 
-    private val keybinds get() = listOf(
-        minecraft.options.keyUp, // w
-        minecraft.options.keyLeft, // a
-        minecraft.options.keyRight, // d
-        minecraft.options.keyDown, // s
-        minecraft.options.keyShift, // shift
-    )
-
     override fun initialize() {
-        on<GuiKeyDownEvent> { event ->
+        on<GuiOpenEvent> { event ->
             if (event.screen !is AbstractContainerScreen<*>) return@on
 
             val container = event.screen
             if (container.title.string != "Chest") return@on
             val containerId = container.menu.containerId
 
-            keybinds.forEach {
                 if (containerId == -1) return@on
-                if (!it.matches(event.event)) return@forEach
 
                 event.cancel()
                 minecraft.connection?.send(ServerboundContainerClosePacket(containerId))
                 minecraft.setScreen(null)
                 return@on
-            }
         }
     }
 }
