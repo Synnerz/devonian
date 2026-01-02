9e5c745c1c3ec1989a0d676bf877b61ade2af7ae
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2026-01-02 18:47:03 -0500

    fix `AutoRequeueDungeons` (dt)

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/AutoRequeueDungeons.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/AutoRequeueDungeons.kt
@@ -1,11 +1,13 @@
 package com.github.synnerz.devonian.features.dungeons
 
 import com.github.synnerz.devonian.api.ChatUtils
-import com.github.synnerz.devonian.api.Location
+import com.github.synnerz.devonian.api.events.ChatChannelEvent
 import com.github.synnerz.devonian.api.events.ChatEvent
 import com.github.synnerz.devonian.api.events.WorldChangeEvent
 import com.github.synnerz.devonian.config.Categories
 import com.github.synnerz.devonian.features.Feature
+import java.util.Collections
+import java.util.concurrent.ConcurrentHashMap
 
 object AutoRequeueDungeons : Feature(
     "autoRequeueDungeons",
@@ -14,37 +16,38 @@ object AutoRequeueDungeons : Feature(
     subcategory = "QOL",
 ) {
     private val extraStatsRegex = "^ *> EXTRA STATS <\$".toRegex()
-    private val partyChatRegex = "^Party > (?:\\[\\d+] .? ?)?(?:\\[[^]]+] )?(\\w{1,16}): !(\\w{1,2})(?: [\\w ]+)?$".toRegex()
-    private var needsDowntime: String? = null
+    private var needsDowntime = Collections.newSetFromMap<String>(ConcurrentHashMap())
+
+    private fun requeue() {
+        ChatUtils.command("instancerequeue")
+    }
 
     override fun initialize() {
-        on<ChatEvent> { event ->
-            val partyMatch = event.matches(partyChatRegex)
-            if (partyMatch != null) {
-                val (name, msg) = partyMatch
-                when (msg.lowercase()) {
-                    "r" -> {
-                        ChatUtils.sendMessage("&a$needsDowntime is ready", true)
-                        needsDowntime = null
-                        return@on
-                    }
-                    "dt" -> needsDowntime = name
+        on<ChatChannelEvent.PartyChatEvent> { event ->
+            val msg = event.userMessage.lowercase()
+            if (msg == "r") {
+                if (!needsDowntime.remove(event.name)) return@on
+
+                ChatUtils.sendMessage("&a${event.name} is ready", true)
+                if (needsDowntime.isEmpty()) requeue()
+            } else if (msg.startsWith("dt") || msg.startsWith("!dt")) {
+                if (needsDowntime.add(event.name)) {
+                    ChatUtils.sendMessage("&bUser &6${event.name} &bneeds downtime", true)
                 }
-                ChatUtils.sendMessage("&bUser &6$needsDowntime &bneeds downtime", true)
-                return@on
             }
-            if (Location.area != "catacombs" || event.matches(extraStatsRegex) == null) return@on
-            if (!needsDowntime.isNullOrBlank()) {
-                ChatUtils.command("pc $needsDowntime needs downtime")
-                return@on
-            }
-            ChatUtils.command("instancerequeue")
+        }
+
+        on<ChatEvent> { event ->
+            if (!extraStatsRegex.matches(event.message)) return@on
+
+            if (needsDowntime.isEmpty()) requeue()
+            else ChatUtils.command("pc ${needsDowntime.joinToString(", ")} needs downtime")
         }
     }
 
     override fun onWorldChange(event: WorldChangeEvent) {
-        if (needsDowntime == null) return
+        if (needsDowntime.isEmpty()) return
         ChatUtils.sendMessage("&aDowntime has been reset.", true)
-        needsDowntime = null
+        needsDowntime.clear()
     }
 }
