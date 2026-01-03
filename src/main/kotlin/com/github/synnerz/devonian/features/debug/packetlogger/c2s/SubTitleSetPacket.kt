2a952204666e930d50c6256523559cc5e7862eba
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2026-01-02 22:39:38 -0500

    good try

--- src/main/kotlin/com/github/synnerz/devonian/features/debug/packetlogger/c2s/SubTitleSetPacket.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/debug/packetlogger/c2s/SubTitleSetPacket.kt
@@ -2,6 +2,7 @@ package com.github.synnerz.devonian.features.debug.packetlogger.c2s
 
 import com.github.synnerz.devonian.config.json.JsonDataObject
 import com.github.synnerz.devonian.features.debug.packetlogger.ISerializer
+import com.github.synnerz.devonian.utils.StringUtils.colorCodes
 import net.minecraft.network.protocol.PacketFlow
 import net.minecraft.network.protocol.PacketType
 import net.minecraft.network.protocol.game.ClientboundSetSubtitleTextPacket
@@ -9,11 +10,10 @@ import net.minecraft.network.protocol.game.GamePacketTypes
 
 object SubTitleSetPacket : ISerializer<ClientboundSetSubtitleTextPacket> {
     override val type: PacketType<ClientboundSetSubtitleTextPacket> = GamePacketTypes.CLIENTBOUND_SET_SUBTITLE_TEXT
-    override val flow: PacketFlow = PacketFlow.SERVERBOUND
+    override val flow: PacketFlow = PacketFlow.CLIENTBOUND
 
     override fun serialize(packet: ClientboundSetSubtitleTextPacket, obj: JsonDataObject) {
-        obj.set("component", packet.text)
-        obj.set("isTerminal", packet.isTerminal)
-        obj.set("isSkippable", packet.isSkippable)
+        obj.set("text", packet.text.colorCodes())
+        obj.set("text_", packet.text.string)
     }
 }
--- src/main/kotlin/com/github/synnerz/devonian/features/debug/packetlogger/c2s/TitleSetPacket.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/debug/packetlogger/c2s/TitleSetPacket.kt
@@ -2,6 +2,7 @@ package com.github.synnerz.devonian.features.debug.packetlogger.c2s
 
 import com.github.synnerz.devonian.config.json.JsonDataObject
 import com.github.synnerz.devonian.features.debug.packetlogger.ISerializer
+import com.github.synnerz.devonian.utils.StringUtils.colorCodes
 import net.minecraft.network.protocol.PacketFlow
 import net.minecraft.network.protocol.PacketType
 import net.minecraft.network.protocol.game.ClientboundSetTitleTextPacket
@@ -9,11 +10,10 @@ import net.minecraft.network.protocol.game.GamePacketTypes
 
 object TitleSetPacket : ISerializer<ClientboundSetTitleTextPacket> {
     override val type: PacketType<ClientboundSetTitleTextPacket> = GamePacketTypes.CLIENTBOUND_SET_TITLE_TEXT
-    override val flow: PacketFlow = PacketFlow.SERVERBOUND
+    override val flow: PacketFlow = PacketFlow.CLIENTBOUND
 
     override fun serialize(packet: ClientboundSetTitleTextPacket, obj: JsonDataObject) {
-        obj.set("component", packet.text)
-        obj.set("isTerminal", packet.isTerminal)
-        obj.set("isSkippable", packet.isSkippable)
+        obj.set("text", packet.text.colorCodes())
+        obj.set("text_", packet.text.string)
     }
 }
