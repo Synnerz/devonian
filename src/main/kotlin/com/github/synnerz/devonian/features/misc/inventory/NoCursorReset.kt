fc319ad85f3392e9de7566064c348b0c449dddc1
Doc <65479309+DocilElm@users.noreply.github.com>
2026-01-04 14:31:47 -0700

    feat: rewrite `NoCursorReset` part 3

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/inventory/NoCursorReset.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/inventory/NoCursorReset.kt
@@ -1,41 +1,37 @@
 package com.github.synnerz.devonian.features.misc.inventory
 
-import com.github.synnerz.devonian.api.events.TickEvent
+import com.github.synnerz.devonian.api.events.ClientContainerCloseEvent
+import com.github.synnerz.devonian.api.events.EventBus
+import com.github.synnerz.devonian.api.events.ServerContainerCloseEvent
+import com.github.synnerz.devonian.api.events.ServerContainerOpenEvent
 import com.github.synnerz.devonian.features.Feature
 import kotlinx.atomicfu.atomic
-import net.minecraft.client.gui.screens.Screen
 
 object NoCursorReset : Feature(
     "noCursorReset",
     "Avoids resetting your cursor whenever navigating guis",
     subcategory = "Inventory",
 ) {
-    var lastScreen: Screen? = null
-    var resetCursor = atomic(true)
+    private var lastContainer = atomic(-1)
+    private var lastClose = atomic(-1)
 
     override fun initialize() {
-        on<TickEvent> {
-            val screen = minecraft.screen
-            if (lastScreen == null && screen != null) {
-                resetCursor.value = true
-                lastScreen = screen
-                return@on
-            }
-            if (lastScreen != null && screen != null && lastScreen != screen) {
-                resetCursor.value = false
-                lastScreen = screen
-                return@on
-            }
-            if (lastScreen != null && screen == null) {
-                resetCursor.value = true
-                lastScreen = null
-                return@on
-            }
+        on<ServerContainerOpenEvent> { event ->
+            if (EventBus.serverTicks() - lastClose.value < 2) lastContainer.value = EventBus.serverTicks()
+        }
+
+        on<ServerContainerCloseEvent> {
+            lastContainer.value = -1
+            lastClose.value = EventBus.serverTicks()
+        }
+
+        on<ClientContainerCloseEvent> {
+            lastContainer.value = -1
         }
     }
 
     fun shouldReset(): Boolean {
         if (!isEnabled()) return true
-        return resetCursor.value
+        return lastContainer.value == -1 || EventBus.serverTicks() - lastContainer.value > 3
     }
 }
