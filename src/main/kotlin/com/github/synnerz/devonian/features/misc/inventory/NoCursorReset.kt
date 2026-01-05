ac484fc5f37799255cecaa37e2c5798aec4f5259
Doc <65479309+DocilElm@users.noreply.github.com>
2026-01-04 18:31:48 -0700

    oversights are the end of me

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/inventory/NoCursorReset.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/inventory/NoCursorReset.kt
@@ -1,9 +1,6 @@
 package com.github.synnerz.devonian.features.misc.inventory
 
-import com.github.synnerz.devonian.api.events.ClientContainerCloseEvent
-import com.github.synnerz.devonian.api.events.EventBus
-import com.github.synnerz.devonian.api.events.ServerContainerCloseEvent
-import com.github.synnerz.devonian.api.events.ServerContainerOpenEvent
+import com.github.synnerz.devonian.api.events.*
 import com.github.synnerz.devonian.features.Feature
 import kotlinx.atomicfu.atomic
 
@@ -30,8 +27,13 @@ object NoCursorReset : Feature(
         }
     }
 
+    override fun onWorldChange(event: WorldChangeEvent) {
+        lastContainer.value = -1
+        lastClose.value = -1
+    }
+
     fun shouldReset(): Boolean {
         if (!isEnabled()) return true
-        return lastContainer.value == -1 || EventBus.serverTicks() - lastContainer.value > 3
+        return lastContainer.value == -1 || lastClose.value == -1 || EventBus.serverTicks() - lastClose.value < 0 || EventBus.serverTicks() - lastContainer.value > 3
     }
 }
