6cc83fb3e99be230373b174c847f4ec5057d127b
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2026-01-05 23:18:53 -0500

    rewrite tp maze

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/solvers/TeleportMazeSolver.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/solvers/TeleportMazeSolver.kt
@@ -2,7 +2,7 @@ package com.github.synnerz.devonian.features.dungeons.solvers
 
 import com.github.synnerz.barrl.Context
 import com.github.synnerz.devonian.api.ChatUtils
-import com.github.synnerz.devonian.api.WorldUtils
+import com.github.synnerz.devonian.api.Scheduler
 import com.github.synnerz.devonian.api.dungeon.DungeonEvent
 import com.github.synnerz.devonian.api.dungeon.DungeonScanner
 import com.github.synnerz.devonian.api.dungeon.Stages
@@ -11,14 +11,9 @@ import com.github.synnerz.devonian.config.Categories
 import com.github.synnerz.devonian.features.Feature
 import com.github.synnerz.devonian.utils.BasicState
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket
-import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.phys.HitResult
-import org.joml.Vector3f
 import java.awt.Color
-import kotlin.math.abs
-import kotlin.math.cos
-import kotlin.math.roundToInt
-import kotlin.math.sin
+import kotlin.math.*
 
 object TeleportMazeSolver : Feature(
     "teleportMazeSolver",
@@ -33,110 +28,92 @@ object TeleportMazeSolver : Feature(
 
     private val endFramePositions = listOf(
         // Y is always 69
-        4 to 6,
-        4 to 12,
-        4 to 14,
-        4 to 20,
-        4 to 22,
-        4 to 28,
-        10 to 6,
-        10 to 12,
-        10 to 14,
-        10 to 20,
-        10 to 22,
-        10 to 28,
-        12 to 22,
-        12 to 28,
-        15 to 12,
-        15 to 14,
-        20 to 6,
-        20 to 12,
-        20 to 14,
-        20 to 20,
-        20 to 22,
-        20 to 28,
-        26 to 6,
-        26 to 12,
-        26 to 14,
-        26 to 20,
-        26 to 22,
-        26 to 28
-    )
-    private val toRender = mutableListOf<Pair<Int, Int>>()
-    var inMaze = false
-    var cells: MutableList<Cell>? = null
-    var orderedPads = mutableListOf<Pad>()
-    var minX = 0
-    var minZ = 0
-    var enteredAt = -1
+        CompPad(4, 6, 5, 7),
+        CompPad(4, 12, 5, 11),
+        CompPad(4, 14, 5, 15),
+        CompPad(4, 20, 5, 19),
+        CompPad(4, 22, 5, 23),
+        CompPad(4, 28, 5, 27),
+        CompPad(10, 6, 9, 7),
+        CompPad(10, 12, 9, 11),
+        CompPad(10, 14, 9, 15),
+        CompPad(10, 20, 9, 19),
+        CompPad(10, 22, 9, 23),
+        CompPad(10, 28, 9, 27),
+        CompPad(12, 22, 13, 23),
+        CompPad(12, 28, 13, 27),
+        CompPad(18, 22, 17, 23),
+        CompPad(18, 28, 17, 27),
+        CompPad(20, 6, 21, 7),
+        CompPad(20, 12, 21, 11),
+        CompPad(20, 14, 21, 15),
+        CompPad(20, 20, 21, 19),
+        CompPad(20, 22, 21, 23),
+        CompPad(20, 28, 21, 27),
+        CompPad(26, 6, 25, 7),
+        CompPad(26, 12, 25, 11),
+        CompPad(26, 14, 25, 15),
+        CompPad(26, 20, 25, 19),
+        CompPad(26, 22, 25, 23),
+        CompPad(26, 28, 25, 27),
 
-    data class Pad(
+        CompPad(15, 12, 14, 11, special = true),
+        CompPad(15, 14, 16, 15, special = true, isEnd = true),
+    )
+    private var inMaze = false
+    private var enteredAt = -1
+    private val pads = mutableListOf<Pad>()
+
+    private data class CompPad(
+        val cx: Int,
+        val cz: Int,
+        val tx: Int,
+        val tz: Int,
+        val special: Boolean = false,
+        val isEnd: Boolean = false,
+    )
+    private data class Pad(
         val x: Int,
         val z: Int,
-        var cx: Int = 0,
-        var cz: Int = 0,
-        var angle: Double = 0.0,
-        var blacklisted: Boolean = false
-    )
-
-    data class Cell(
-        val gx: Int,
-        val gz: Int,
-        val pads: MutableList<Pad> = mutableListOf()
+        val tx: Int,
+        val tz: Int,
+        val special: Boolean,
+        val isEnd: Boolean,
     ) {
-        fun addPad(pad: Pad) = pads.add(pad)
+        var visited = false
+        var correct = false
+        var possible = false
     }
 
     override fun initialize() {
         on<DungeonEvent.RoomEnter> {
             val room = it.room
             if (room.name != "Teleport Maze") return@on
+
             inMaze = true
             enteredAt = EventBus.serverTicks()
+            if (!room.hasRotation()) return@on
 
-            val pads = mutableListOf<Pad>()
-            for (pos in endFramePositions) {
-                val x1 = pos.first
-                val y = 69
-                val z1 = pos.second
-                val coords = room.fromComp(x1, z1) ?: continue
-                val ( x, z ) = coords
-                val blockState = WorldUtils.getBlockState(x, y, z) ?: continue
-                if (blockState.block !== Blocks.END_PORTAL_FRAME) continue
+            pads.clear()
+            for (comp in endFramePositions) {
+                val (x, z) = room.fromComp(comp.cx, comp.cz) ?: continue
+                val (tx, tz) = room.fromComp(comp.tx, comp.tz) ?: continue
 
-                pads.add(Pad(x, z))
-            }
-
-            minX = (pads.minOfOrNull { pad -> pad.x } ?: 0)
-            minZ = (pads.minOfOrNull { pad -> pad.z } ?: 0)
-            cells = MutableList(9) { Cell(it / 3, it % 3) }
-
-            for (pad in pads) {
-                pad.cx = (pad.x - minX) / 8
-                pad.cz = (pad.z - minZ) / 8
-                val idx = pad.cx * 3 + pad.cz
-                if (idx >= cells!!.size) continue
-                cells!![idx].addPad(pad)
+                pads.add(Pad(x, z, tx, tz, comp.special, comp.isEnd))
             }
         }
 
         on<DungeonEvent.RoomLeave> {
-            if (!inMaze) return@on
-            toRender.clear()
-            inMaze = false
-            minX = 0
-            minZ = 0
-            cells = null
-            enteredAt = -1
-            orderedPads.clear()
+            reset()
         }
 
         on<PacketReceivedEvent> { event ->
             if (!inMaze) return@on
 
-            val player = minecraft.player ?: return@on
             val packet = event.packet
             if (packet !is ClientboundPlayerPositionPacket) return@on
+
+            if (packet.relatives.isNotEmpty()) return@on
 
             val change = packet.change
             val pos = change.position
@@ -144,60 +121,61 @@ object TeleportMazeSolver : Feature(
 
             if (pos.x % 0.5 != 0.0 || pos.y != 69.5 || pos.z % 0.5 != 0.0) return@on
 
-            val newPad = padNear(pos.x.roundToInt(), pos.z.roundToInt()) ?: return@on
-            val oldPad = padNear(player.x.roundToInt(), player.z.roundToInt()) ?: return@on
+            Scheduler.scheduleBeforePacket {
+                val player = minecraft.player ?: return@scheduleBeforePacket
+                val oldPad = pads.minByOrNull { abs(it.x - player.x) + abs(it.z - player.z) } ?: return@scheduleBeforePacket
+                val newPad = pads.minByOrNull { abs(it.x - pos.x) + abs(it.z - pos.z) } ?: return@scheduleBeforePacket
 
-            newPad.blacklisted = true
-            oldPad.blacklisted = true
+                if (newPad.special) {
+                    pads.forEach {
+                        it.visited = false
+                        it.correct = false
+                        it.possible = false
+                    }
+                    if (newPad.isEnd) return@scheduleBeforePacket
+                }
+                else {
+                    oldPad.visited = true
+                    newPad.visited = true
+                }
 
-            padAngle(pos.x.roundToInt(), pos.z.roundToInt(), yaw)
-            padAngle(pos.x.toInt(), pos.z.toInt(), yaw)
+                val dir = Vec2(cos((yaw + 90.0) / 180.0 * PI), sin((yaw + 90.0) / 180.0 * PI))
+                pads.forEach {
+                    if (it === newPad) return@forEach
+                    if (it.special) return@forEach
+
+                    val offset = Vec2(
+                        (it.tx - newPad.tx).toDouble(),
+                        (it.tz - newPad.tz).toDouble(),
+                    )
+                    val matches = dir.parallel(offset)
+                    it.correct = matches
+                    it.possible = it.possible || matches
+                }
+            }
         }
 
         on<RenderWorldEvent> {
             if (!inMaze) return@on
-            if (orderedPads.isEmpty() || orderedPads.size < 2) return@on
-            if (orderedPads.getOrNull(0)?.angle == orderedPads.getOrNull(1)?.angle) return@on
 
-            val pos1 = orderedPads.getOrNull(0) ?: return@on
-            Context.Immediate?.renderBox(
-                pos1.x.toDouble(), 69.0, pos1.z.toDouble(),
-                Color.GREEN,
-                false
-            )
-            Context.Immediate?.renderFilledBox(
-                pos1.x.toDouble(), 69.0, pos1.z.toDouble(),
-                Color(0, 255, 0, 80),
-                false
-            )
-            // less likely
-            val pos2 = orderedPads.getOrNull(1) ?: return@on
-            Context.Immediate?.renderBox(
-                pos2.x.toDouble(), 69.0, pos2.z.toDouble(),
-                Color.ORANGE,
-                false
-            )
-            Context.Immediate?.renderFilledBox(
-                pos2.x.toDouble(), 69.0, pos2.z.toDouble(),
-                Color(255, 165, 0, 80),
-                false
-            )
+            pads.forEach {
+                val color = if (it.correct) Color.GREEN
+                    else if (it.visited) Color.RED
+                    else if (it.possible) Color.ORANGE
+                    else return@forEach
+                val colorFill = Color(color.red, color.green, color.blue, 80)
 
-            if (cells == null) return@on
-            for (cell in cells!!) {
-                for (pad in cell.pads) {
-                    if (!pad.blacklisted) continue
-                    Context.Immediate?.renderBox(
-                        pad.x.toDouble(), 69.0, pad.z.toDouble(),
-                        Color.RED,
-                        false
-                    )
-                    Context.Immediate?.renderFilledBox(
-                        pad.x.toDouble(), 69.0, pad.z.toDouble(),
-                        Color(255, 0, 0, 80),
-                        false
-                    )
-                }
+                Context.Immediate?.renderBox(
+                    it.x.toDouble(), 69.0, it.z.toDouble(),
+                    color,
+                    phase = false,
+                    lineWidth = 2.0,
+                )
+                Context.Immediate?.renderFilledBox(
+                    it.x.toDouble(), 69.0, it.z.toDouble(),
+                    colorFill,
+                    phase = false,
+                )
             }
         }
 
@@ -222,84 +200,25 @@ object TeleportMazeSolver : Feature(
         }
     }
 
-    override fun onWorldChange(event: WorldChangeEvent) {
-        toRender.clear()
+    private fun reset() {
         inMaze = false
-        minX = 0
-        minZ = 0
-        cells = null
         enteredAt = -1
-        orderedPads.clear()
+        pads.clear()
     }
 
-    private fun cellAt(x: Int, z: Int): Cell? {
-        if (x < minX || x > minX + 23 || z < minZ || z > minZ + 23) return null
-
-        val cx = (x - minX) / 8
-        val cz = (z - minZ) / 8
-
-        return cells?.find { it.gx == cx && it.gz == cz }
+    override fun onWorldChange(event: WorldChangeEvent) {
+        reset()
     }
 
-    private fun padNear(x: Int, z: Int): Pad? {
-        val cell = cellAt(x, z) ?: return null
-        var result: Pad? = null
-
-        for (pad in cell.pads) {
-            val dist = abs(x - pad.x) + abs(z - pad.z)
-            if (dist <= 3) {
-                result = pad
-                break
-            }
+    private data class Vec2(val u: Double, val v: Double)  {
+        fun parallel(o: Vec2): Boolean {
+            if (u.eq(0.0)) return o.u.eq(0.0) && v.sign == o.v.sign
+            if (v.eq(0.0)) return o.v.eq(0.0) && u.sign == o.u.sign
+            return (u * o.v).eq(v * o.u) && u.sign == o.u.sign
         }
 
-        return result
-    }
-
-    // Whether the current pad is in the start or the end cell
-    private fun inSpecial(pad: Pad): Boolean {
-        if (cells == null) return false
-        val endCell = cells!!.getOrNull(4)
-
-        if (endCell != null && endCell.pads.contains(pad)) return true
-
-        var result = false
-
-        for (cell in cells!!) {
-            if (cell.pads.size != 1 || cell == endCell || !cell.pads.contains(pad)) continue
-            result = true
-            break
+        companion object {
+            private fun Double.eq(o: Double) = abs(this - o) < 1e-2
         }
-
-        return result
-    }
-
-    private fun fromPitchYaw(pitch: Float, yaw: Float): Vector3f {
-        val radPitch = pitch * 0.017453292f
-        val radYaw = yaw * 0.017453292f
-
-        val f = cos(-radYaw - Math.PI.toFloat())
-        val f1 = sin(-radYaw - Math.PI.toFloat())
-        val f2 = -cos(-radPitch)
-        val f3 = sin(-radPitch)
-
-        return Vector3f(f1 * f2, f3, f * f2).normalize()
-    }
-
-    private fun padAngle(x: Int, z: Int, yaw: Float) {
-        val vec = fromPitchYaw(0f, yaw)
-
-        orderedPads.clear()
-        for (cell in cells!!) {
-            for (pad in cell.pads) {
-                if (inSpecial(pad) || pad.blacklisted) continue
-                val vec2 = Vector3f(pad.x + 0.5f - x, 0f, pad.z + 0.5f - z)
-                val angle = Math.toDegrees(vec.angle(vec2).toDouble())
-                pad.angle = angle
-                orderedPads.add(pad)
-            }
-        }
-
-        orderedPads.sortBy { it.angle }
     }
 }
