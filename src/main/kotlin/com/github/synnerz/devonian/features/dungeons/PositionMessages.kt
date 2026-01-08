b57ab0be9745032270bcd848c8aaa17be2803ae5
Doc <65479309+DocilElm@users.noreply.github.com>
2026-01-08 02:51:12 -0700

    feat: fix `PositionMessages`

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/PositionMessages.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/PositionMessages.kt
@@ -53,35 +53,36 @@ object PositionMessages : Feature(
                 PosMsg(60.0, 132.0, 140.0, 1.5, "at ee2 high!"),
                 PosMsg(69.0, 109.0, 122.0, 1.0, "at ee2 safe spot!"),
                 PosMsg(48.0, 109.0, 122.0, 1.0, "at ee2 safe spot!"),
-                PosMsg(54.0, 65.0, 76.0, 7.5, "at mid!"),
-                PosMsg(54.5, 63.7, 114.5, 2.0, null),
+                PosMsg(54.0, 65.0, 76.0, 33.0, "at mid!"),
             ),
             DungeonClass.Berserk to listOf(
-                PosMsg(63.0, 127.0, 35.0, 1.5, "at i4!"),
-                PosMsg(54.0, 65.0, 76.0, 7.5, "at mid!"),
-                PosMsg(54.5, 63.7, 114.5, 2.0, null),
+                PosMsg(63.0, 127.0, 35.0, 3.0, "at i4!"),
+                PosMsg(54.0, 65.0, 76.0, 33.0, "at mid!"),
             ),
             DungeonClass.Mage to listOf(
                 PosMsg(58.0, 123.0, 122.0, 0.3, "entering core!"),
                 PosMsg(54.0, 115.0, 51.0, 1.5, "at core!"),
-                PosMsg(54.0, 65.0, 76.0, 7.5, "at mid!"),
-                PosMsg(54.5, 63.7, 114.5, 2.0, null),
+                PosMsg(54.0, 65.0, 76.0, 33.0, "at mid!"),
             ),
             DungeonClass.Tank to listOf(
-                PosMsg(54.0, 65.0, 76.0, 7.5, "at mid!"),
-                PosMsg(54.5, 63.7, 114.5, 2.0, null),
+                PosMsg(54.0, 65.0, 76.0, 33.0, "at mid!"),
             ),
             DungeonClass.Healer to listOf(
                 PosMsg(108.0, 120.0, 94.0, 1.5, "at ss!"),
                 PosMsg(2.0, 109.0, 104.0, 1.5, "at ee3!"),
                 PosMsg(18.0, 121.0, 99.0, 3.0, "at ee3 safe spot!"),
-                PosMsg(-1.0, 120.0, 77.0, 1.5, "at arrows dev!"),
+                PosMsg(1.0, 120.0, 77.0, 3.0, "at arrows dev!"),
                 PosMsg(60.0, 132.0, 140.0, 1.5, "at levers dev!"),
-                PosMsg(54.0, 65.0, 76.0, 7.5, "at mid!"),
-                PosMsg(54.5, 5.0, 74.0, 4.0, "at p5!"),
-                PosMsg(54.5, 63.7, 114.5, 2.0, null),
+                PosMsg(54.0, 65.0, 76.0, 33.0, "at mid!"),
+                PosMsg(54.0, 5.0, 76.0, 8.0, "at p5!"),
             ),
         )
+    )
+    private val specialPos = listOf(
+        listOf(53.0, 63.0, 113.0),
+        listOf(55.0, 63.0, 113.0),
+        listOf(55.0, 63.0, 115.0),
+        listOf(53.0, 63.0, 115.0),
     )
     private var currentPos: List<PosMsg>? = null
 
@@ -113,7 +114,7 @@ object PositionMessages : Feature(
 
             val pos = minecraft.player ?: return@on
             val msg = currentPos?.find {
-                pos.distanceToSqr(it.x, it.y, it.z) <= (it.dist * it.dist) / 2
+                pos.distanceToSqr(it.x + 0.5, it.y, it.z + 0.5) <= it.dist
             } ?: return@on
             if (msg.sent) return@on
 
@@ -134,6 +135,15 @@ object PositionMessages : Feature(
                 Context.Immediate?.renderBox(
                     it.x, it.y, it.z,
                     1.0, 0.5,
+                    SETTING_RENDER_COLOR.getColor(),
+                )
+            }
+            specialPos.forEach {
+                val ( x, y, z ) = it
+
+                Context.Immediate?.renderBox(
+                    x, y, z,
+                    1.0, 1.05,
                     SETTING_RENDER_COLOR.getColor(),
                 )
             }
