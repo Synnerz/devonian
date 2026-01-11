005db86789a3a9793fdf6ae0e2f9a9bae844db98
legendaryjg25 <legendaryjg12@gmail.com>
2026-01-10 21:25:13 -0800

    Remove NoCursorReset.kt
    Toggling it on makes it break in a different way than it breaks with it off.

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/inventory/NoCursorReset.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/inventory/NoCursorReset.kt
@@ -1,39 +1,20 @@
 package com.github.synnerz.devonian.features.misc.inventory
 
-import com.github.synnerz.devonian.api.events.*
+import com.github.synnerz.devonian.api.events.WorldChangeEvent
 import com.github.synnerz.devonian.features.Feature
-import kotlinx.atomicfu.atomic
 
 object NoCursorReset : Feature(
     "noCursorReset",
     "Avoids resetting your cursor whenever navigating guis",
     subcategory = "Inventory",
 ) {
-    private var lastContainer = atomic(-1)
-    private var lastClose = atomic(-1)
-
     override fun initialize() {
-        on<ServerContainerOpenEvent> { event ->
-            if (EventBus.serverTicks() - lastClose.value < 2) lastContainer.value = EventBus.serverTicks()
-        }
-
-        on<ServerContainerCloseEvent> {
-            lastContainer.value = -1
-            lastClose.value = EventBus.serverTicks()
-        }
-
-        on<ClientContainerCloseEvent> {
-            lastContainer.value = -1
-        }
     }
 
     override fun onWorldChange(event: WorldChangeEvent) {
-        lastContainer.value = -1
-        lastClose.value = -1
     }
 
     fun shouldReset(): Boolean {
-        if (!isEnabled()) return true
-        return lastContainer.value == -1 || lastClose.value == -1 || EventBus.serverTicks() - lastClose.value < 0 || EventBus.serverTicks() - lastContainer.value > 3
+        return true
     }
 }
