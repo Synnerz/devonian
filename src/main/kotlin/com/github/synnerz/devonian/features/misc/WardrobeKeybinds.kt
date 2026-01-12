bddb2a24990e14bd8e13a45e3b87e397ccfe90d5
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2026-01-12 01:15:30 -0500

    change `WardrobeKeybinds` to use hotbar keys

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/WardrobeKeybinds.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/WardrobeKeybinds.kt
@@ -3,35 +3,20 @@ package com.github.synnerz.devonian.features.misc
 import com.github.synnerz.devonian.api.ScreenUtils
 import com.github.synnerz.devonian.api.events.GuiKeyDownEvent
 import com.github.synnerz.devonian.features.Feature
-import org.lwjgl.glfw.GLFW
 
 object WardrobeKeybinds : Feature(
     "wardrobeKeybinds",
-    "Whenever inside of wardrobe gui it will allow you to press 1-9 keys to switch through your wardrobes",
-    subcategory = "General"
+    "Whenever inside of wardrobe gui it will allow you to press the hotbar 1-9 keys to switch through your wardrobes",
+    subcategory = "General",
 ) {
-    // TODO: make these customizable
-    private val keybindList = listOf(
-        GLFW.GLFW_KEY_1,
-        GLFW.GLFW_KEY_2,
-        GLFW.GLFW_KEY_3,
-        GLFW.GLFW_KEY_4,
-        GLFW.GLFW_KEY_5,
-        GLFW.GLFW_KEY_6,
-        GLFW.GLFW_KEY_7,
-        GLFW.GLFW_KEY_8,
-        GLFW.GLFW_KEY_9,
-    )
-
     override fun initialize() {
         on<GuiKeyDownEvent> { event ->
             val screen = event.screen
             if (!screen.title.string.startsWith("Wardrobe (")) return@on
 
-            keybindList.forEach {
-                if (event.key != it) return@forEach
-                val slotIdx = it - 48
-                val wardrobeSlot = 35 + slotIdx
+            minecraft.options.keyHotbarSlots.forEachIndexed { i, v ->
+                if (!v.matches(event.event)) return@forEachIndexed
+                val wardrobeSlot = 36 + i
 
                 event.cancel()
                 ScreenUtils.click(wardrobeSlot.coerceIn(36, 44))
