48114020877744166371f3e3db84eaddbe7051f0
Doc <65479309+DocilElm@users.noreply.github.com>
2026-01-13 23:05:51 -0700

    feat: change `ShowSelectedPet` to be faster

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/ShowSelectedPet.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/ShowSelectedPet.kt
@@ -1,16 +1,8 @@
 package com.github.synnerz.devonian.features.misc
 
 import com.github.synnerz.devonian.api.ItemUtils
-import com.github.synnerz.devonian.api.events.PacketReceivedEvent
-import com.github.synnerz.devonian.api.events.PacketSentEvent
-import com.github.synnerz.devonian.api.events.RenderSlotEvent
-import com.github.synnerz.devonian.api.events.WorldChangeEvent
+import com.github.synnerz.devonian.api.events.*
 import com.github.synnerz.devonian.features.Feature
-import net.minecraft.network.protocol.game.ClientboundContainerClosePacket
-import net.minecraft.network.protocol.game.ClientboundContainerSetContentPacket
-import net.minecraft.network.protocol.game.ClientboundOpenScreenPacket
-import net.minecraft.network.protocol.game.ServerboundContainerClosePacket
-import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
 import java.awt.Color
 
@@ -30,58 +22,48 @@ object ShowSelectedPet : Feature(
     private var currentPetSlot = -1
 
     override fun initialize() {
-        on<PacketReceivedEvent> { event ->
-            val packet = event.packet
-            if (packet is ClientboundOpenScreenPacket) {
-                val title = packet.title.string
-                inPets = petsMenuRegex.matches(title)
-                currentPetSlot = -1
-                return@on
-            }
-
-            if (packet is ClientboundContainerClosePacket) {
-                inPets = false
-                currentPetSlot = -1
-                return@on
-            }
-
-            if (packet !is ClientboundContainerSetContentPacket) return@on
-
-            val items = packet.items
-            val possiblyForge = items.getOrNull(10)
-            if (possiblyForge != null && ItemUtils.skyblockId(possiblyForge) == "BEJEWELED_COLLAR") {
-                inPets = false
-                currentPetSlot = -1
-                return@on
-            }
-
-            for (idx in 0..<45) {
-                val itemStack = items.getOrNull(idx) ?: continue
-                if (itemStack.item != Items.PLAYER_HEAD || itemStack == ItemStack.EMPTY) continue
-
-                val lore = ItemUtils.lore(itemStack) ?: return@on
-                if (!lore.any { it == "Click to despawn!" }) continue
-
-                currentPetSlot = idx
-            }
+        on<ServerContainerOpenEvent> { event ->
+            inPets = petsMenuRegex.matches(event.titleStr)
+            currentPetSlot = -1
         }
 
-        on<PacketSentEvent> { event ->
-            val packet = event.packet
-            if (packet !is ServerboundContainerClosePacket) return@on
-
+        on<ServerContainerCloseEvent> {
             inPets = false
             currentPetSlot = -1
         }
 
+        on<ClientContainerCloseEvent> {
+            inPets = false
+            currentPetSlot = -1
+        }
+
+        on<ServerContainerSetSlotEvent> { event ->
+            if (!inPets || event.slot > 54) return@on
+
+            if (event.slot == 10 && ItemUtils.skyblockId(event.itemStack) == "BEJEWELED_COLLAR") {
+                inPets = false
+                currentPetSlot = -1
+                return@on
+            }
+
+            if (event.itemStack.item != Items.PLAYER_HEAD) return@on
+            ItemUtils.lore(event.itemStack)?.let {
+                for (line in it) {
+                    if (line == "Click to despawn!") {
+                        currentPetSlot = event.slot
+                        break
+                    }
+                }
+            }
+        }
+
         on<RenderSlotEvent> { event ->
-            if (!inPets) return@on
+            if (!inPets || event.isInventory()) return@on
             val slot = event.slot
-            if (slot.container == minecraft.player?.inventory) return@on
-            if (currentPetSlot == -1 || slot.containerSlot != currentPetSlot) return@on
+            if (slot.containerSlot != currentPetSlot) return@on
 
             event.ctx.fill(slot.x, slot.y, slot.x + 16, slot.y + 16, SETTING_HIGHLIGHT_COLOR.get())
-        }
+        }.prio = 30
     }
 
     override fun onWorldChange(event: WorldChangeEvent) {
