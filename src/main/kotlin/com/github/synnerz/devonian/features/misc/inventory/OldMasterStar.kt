be0ca3ef029af7f964a42927fe7cff348e2a5cf8
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2026-01-21 03:12:36 -0500

    IHATESKINSIHATESKINSIHATESKINS

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/inventory/OldMasterStar.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/inventory/OldMasterStar.kt
@@ -18,22 +18,38 @@ object OldMasterStar : Feature(
 
     fun transformName(name: Component): Component {
         return cache.getOrPut(name) {
-            if (name.siblings.size < 2) return@getOrPut name
-            val normalStarsComp = name.siblings[name.siblings.size - 2]
-            val starsComp = name.siblings.last()
-            val starsText = (starsComp.contents as? PlainTextContents.LiteralContents)?.text ?: return@getOrPut name
+            val l = name.siblings.size
+            if (l < 2) return@getOrPut name
+
+            var i = 0
+            var normalStarsComp = name.siblings[l - 2]
+            var starsComp = name.siblings.last()
+            var starsText = (starsComp.contents as? PlainTextContents.LiteralContents)?.text ?: return@getOrPut name
+
+            if (starsText == "✦") {
+                if (l < 3) return@getOrPut name
+                i = 1
+                normalStarsComp = name.siblings[l - 3]
+                starsComp = name.siblings[l - 2]
+                starsText = (starsComp.contents as? PlainTextContents.LiteralContents)?.text?.trim() ?: return@getOrPut name
+            }
+
             val stars = masterStars.indexOf(starsText) + 1
             if (stars <= 0) return@getOrPut name
 
             val comp = MutableComponent.create(name.contents).withStyle(name.style)
-            comp.siblings.addAll(name.siblings.subList(0, name.siblings.size - 2))
+            comp.siblings.addAll(name.siblings.subList(0, l - 2 - i))
             comp.siblings.add(Component.literal("✪".repeat(stars)).withStyle(starsComp.style))
             if (stars != 5) {
                 comp.siblings.add(Component.literal("✪".repeat(5 - stars)).withStyle(normalStarsComp.style))
             }
+            if (i > 0) {
+                comp.siblings.add(Component.literal(" "))
+                comp.siblings.addAll(name.siblings.subList(l - i, l))
+            }
 
             return@getOrPut comp
-        }
+        } ?: name
     }
 
     override fun onWorldChange(event: WorldChangeEvent) {
