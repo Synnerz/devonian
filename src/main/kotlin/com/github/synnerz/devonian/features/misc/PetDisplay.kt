c464b830a8595cf5e9a7d839ab285b2f385f8faa
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2026-01-24 17:59:42 -0500

    fix `PetDisplay`

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/PetDisplay.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/PetDisplay.kt
@@ -1,9 +1,12 @@
 package com.github.synnerz.devonian.features.misc
 
+import com.github.synnerz.devonian.api.Location
+import com.github.synnerz.devonian.api.Scheduler
 import com.github.synnerz.devonian.api.events.ChatEvent
 import com.github.synnerz.devonian.api.events.RenderOverlayEvent
 import com.github.synnerz.devonian.config.Config
 import com.github.synnerz.devonian.hud.texthud.TextHudFeature
+import com.github.synnerz.devonian.utils.BasicState
 import com.github.synnerz.devonian.utils.StringUtils.colorCodes
 
 object PetDisplay : TextHudFeature(
@@ -12,44 +15,58 @@ object PetDisplay : TextHudFeature(
     subcategory = "General",
 ) {
     private const val KEY_NAME = "petDisplay"
-    private val equippedPetRegex = "^You summoned your ([\\w ]+)(?: ✦)?!\$".toRegex()
-    private val formattedEquppedRegex = "^§r§aYou summoned your §r([§\\w ]+)(?: [§✦]+)?§r§a!$".toRegex()
-    private val autoPetRuleRegex = "^Autopet equipped your \\[Lvl \\d+](?: \\[\\d+.])? ([\\w ]+)(?: .)?! VIEW RULE$".toRegex()
-    private val formattedAutoRuleRegex = "^§r§cAutopet §eequipped your §7\\[Lvl \\d+](?: [§\\w \\[\\]✦]+)? ([§\\w ]+)(?:§\\w .)?(?: . )?§e! §a§lVIEW RULE$".toRegex()
-    private var currentPet: String? = ""
+
+    // https://regex101.com/r/cnWa1e/1
+    private val equippedPetRegex = "^You summoned your ([\\w\\s]+)( .+?)?!$".toRegex()
+    private val formattedEquippedPetRegex = "^§r§aYou summoned your §r((?:§.)*[\\w\\s]+)((?:§.)* .+?)?§r§a!$".toRegex()
+    // no intellij {0,1}? cannot be simplified to ?
+    private val formattedAutoPetRuleRegex = "^§cAutopet §eequipped your §7\\[Lvl \\d+] (?:.+? ){0,1}?((?:§.)*[\\w\\s]+)((?:§.)* .+?)?§e! §a§lVIEW RULE$".toRegex()
+    private val despawnedPetRegex = "^You despawned your ([\\w\\s]+)( .+?)?!$".toRegex()
+    private val formattedDespawnedPetRegex = "^§r§aYou despawned your §r((?:§.)*[\\w\\s]+)((?:§.)* .+?)?§r§a!$".toRegex()
+
+    private var currentPet = BasicState("").also {
+        it.listen { pet ->
+            Scheduler.scheduleTask {
+                setLine(pet)
+            }
+        }
+    }
 
     override fun initialize() {
         Config.set(KEY_NAME, "")
 
         Config.onAfterLoad {
-            val cached = Config.get<String>(KEY_NAME) ?: ""
-            currentPet = cached
+            currentPet.value = Config.get<String>(KEY_NAME) ?: ""
         }
 
         Config.onPreSave {
-            currentPet ?: return@onPreSave
-            Config.set(KEY_NAME, currentPet)
+            Config.set(KEY_NAME, currentPet.value)
         }
 
         on<ChatEvent> { event ->
-            event.matches(equippedPetRegex)?.let {
-                val match = formattedEquppedRegex.matchEntire(event.text.colorCodes())?.groupValues?.drop(1) ?: return@on
-                currentPet = match[0]
+            if (equippedPetRegex.matches(event.message)) {
+                val match = formattedEquippedPetRegex.matchEntire(event.text.colorCodes()) ?: return@on
+                match.groupValues.getOrNull(1)?.let { currentPet.value = it }
                 return@on
             }
-
-            event.matches(autoPetRuleRegex)?.let {
-                val match = formattedAutoRuleRegex.matchEntire(event.text.colorCodes())?.groupValues?.drop(1) ?: return@on
-                currentPet = match[0]
+            formattedAutoPetRuleRegex.matchEntire(event.text.string)?.let { match ->
+                match.groupValues.getOrNull(1)?.let { currentPet.value = it }
+            }
+            if (despawnedPetRegex.matches(event.message)) {
+                val match = formattedDespawnedPetRegex.matchEntire(event.text.colorCodes()) ?: return@on
+                match.groupValues.getOrNull(1)?.let { currentPet.value = "" }
                 return@on
             }
         }
 
         on<RenderOverlayEvent> {
-            if (currentPet == null) return@on
-            setLine(currentPet!!)
             draw(it.ctx)
-        }
+        }.setEnabled(
+            Location.stateInSkyblock.zip(
+                Location.stateInArea("the rift").map(Boolean::not),
+                Boolean::and
+            )
+        )
     }
 
     override fun getEditText(): List<String> = listOf("&6Rat")
