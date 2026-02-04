6d873bd53a045eecc2c0be2f119e07476cc51ccd
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2026-02-04 01:37:05 -0500

    change dragon health to be based on entity hp

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/m7/DragonHealth.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/m7/DragonHealth.kt
@@ -1,19 +1,20 @@
 package com.github.synnerz.devonian.features.dungeons.m7
 
 import com.github.synnerz.devonian.api.dungeon.Stages
-import com.github.synnerz.devonian.api.events.PacketReceivedEvent
+import com.github.synnerz.devonian.api.events.EntityDataEvent
 import com.github.synnerz.devonian.api.events.PreExtractRenderEntityEvent
 import com.github.synnerz.devonian.api.events.RenderWorldEvent
-import com.github.synnerz.devonian.api.events.ScoreboardEvent
+import com.github.synnerz.devonian.api.events.WorldChangeEvent
 import com.github.synnerz.devonian.config.Categories
 import com.github.synnerz.devonian.features.Feature
 import com.github.synnerz.devonian.utils.BasicState
+import com.github.synnerz.devonian.utils.StringUtils
 import com.github.synnerz.devonian.utils.render.Render3DImmediate
-import net.minecraft.network.protocol.game.ClientboundAddEntityPacket
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.boss.enderdragon.EnderDragon
-import net.minecraft.world.entity.decoration.ArmorStand
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.math.max
+import kotlin.math.roundToInt
 
 object DragonHealth : Feature(
     "dragonHealth",
@@ -28,49 +29,25 @@ object DragonHealth : Feature(
 
     private data class Health(val x: Double, val y: Double, val z: Double, val str: String)
 
-    private val nametagReg = "^﴾ [^\\sA-Za-z]* Withered Dragon ([\\d.,]+[KMB]?)/([\\d.,]+[KMB]?)❤ ﴿$".toRegex()
-    private val scoreboardReg = "^- (\\w+) Dragon (.+)$".toRegex()
-
     private val healths = mutableListOf<Health>()
-    private var dragons = mutableMapOf<M7Dragon, Int>()
     private var dragonHealths = ConcurrentHashMap<Int, String>()
+    private var maxHp = 1_000_000_000
 
     override fun initialize() {
-        on<PacketReceivedEvent> { event ->
-            val packet = event.packet as? ClientboundAddEntityPacket ?: return@on
-            if (packet.type != EntityType.ENDER_DRAGON) return@on
+        on<EntityDataEvent> { event ->
+            if (event.type != EntityType.ENDER_DRAGON) return@on
 
-            val type = M7Dragon.entries.minBy {
-                (it.path[0].x - packet.x) * (it.path[0].x - packet.x) +
-                (it.path[0].y - packet.y) * (it.path[0].y - packet.y) +
-                (it.path[0].z - packet.z) * (it.path[0].z - packet.z)
-            }
-            dragons[type] = packet.id
-        }
+            val hp = event.data.find { it.id == 9 }?.value as? Float ?: return@on
+            val hpI = hp.roundToInt()
+            maxHp = max(maxHp, hpI)
 
-        on<ScoreboardEvent> { event ->
-            val match = event.matches(scoreboardReg) ?: return@on
-            val type = match.getOrNull(0) ?: return@on
-            var health = match.getOrNull(1) ?: return@on
-            val dragon = M7Dragon.entries.find { it.displayName == type } ?: return@on
-            val id = dragons[dragon] ?: return@on
-            if (health.endsWith("❤")) health = health.dropLast(1)
-            dragonHealths[id] = health
+            dragonHealths[event.entityId] = StringUtils.colorForNumber(hpI, maxHp) + StringUtils.shortenNumber(hpI)
         }
 
         on<PreExtractRenderEntityEvent> { event ->
             val entity = event.entity as? EnderDragon ?: return@on
-
             if (entity.dragonDeathTime > 0) return@on
-
-            val str = (minecraft.level?.getEntity(entity.id + 8) as? ArmorStand?)?.let { tag ->
-                val name = tag.name.string
-                val match = nametagReg.matchEntire(name) ?: return@let null
-                val hp = match.groupValues.getOrNull(1) ?: return@let null
-                // val hp = match.groupValues.getOrNull(1)?.let { StringUtils.parseShortenedNumber(it) } ?: return@on
-                // val maxHp = match.groupValues.getOrNull(2)?.let { StringUtils.parseShortenedNumber(it) } ?: return@on
-                return@let hp
-            } ?: dragonHealths[entity.id] ?: return@on
+            val hp = dragonHealths[entity.id] ?: return@on
 
             val pos = entity.getPosition(event.pt)
             healths.add(
@@ -78,7 +55,7 @@ object DragonHealth : Feature(
                     pos.x,
                     pos.y + 2.0,
                     pos.z,
-                    str,
+                    hp,
                 )
             )
         }
@@ -95,5 +72,9 @@ object DragonHealth : Feature(
             }
             healths.clear()
         }
+    }
+
+    override fun onWorldChange(event: WorldChangeEvent) {
+        maxHp = 1_000_000_000
     }
 }
