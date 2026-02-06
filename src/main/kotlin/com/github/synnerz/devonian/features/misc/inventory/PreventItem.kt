ad95f0122a5575c2691e395f37ec03e0009c0691
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2026-02-06 16:33:23 -0500

    add trading to prevent items

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/inventory/PreventItem.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/inventory/PreventItem.kt
@@ -44,39 +44,36 @@ object PreventItem {
             val slot = event.slot
             if (slot.container !== mc.player?.inventory) return@on
 
-            val selling = isSellScreen(event.screen)
-            val salvage = isSalvageScreen(event.screen)
-            val evn = SlotEvent(slot, slot.item, slot.containerSlot, selling || salvage, event)
+            val type = BadScreen.getType(event.screen)
+            val evn = SlotEvent(slot, slot.item, slot.containerSlot, type != null, event)
             if (!evn.post()) return@on
 
             event.cancel()
-            onCancel("&cPrevented ${if (selling) "selling" else if (salvage) "salvaging" else "moving"} item", evn.actors)
+            onCancel("&cPrevented ${type?.action ?: "moving"} item", evn.actors)
         }
 
         EventBus.on<QuickMoveItemEvent> { event ->
             val slot = event.slot
             if (slot.container !== mc.player?.inventory) return@on
 
-            val selling = isSellScreen(event.screen)
-            val salvage = isSalvageScreen(event.screen)
-            val evn = SlotEvent(slot, slot.item, slot.containerSlot, selling || salvage, event)
+            val type = BadScreen.getType(event.screen)
+            val evn = SlotEvent(slot, slot.item, slot.containerSlot, type != null, event)
             if (!evn.post()) return@on
 
             event.cancel()
-            onCancel("&cPrevented ${if (selling) "selling" else if (salvage) "salvaging" else "moving"} item", evn.actors)
+            onCancel("&cPrevented ${type?.action ?: "moving"} item", evn.actors)
         }
 
         EventBus.on<QuickCraftMoveEvent> { event ->
             val slot = event.slot
             if (slot.container !== mc.player?.inventory) return@on
 
-            val selling = isSellScreen(event.screen)
-            val salvage = isSalvageScreen(event.screen)
-            val evn = SlotEvent(slot, slot.item, slot.containerSlot, selling || salvage, event)
+            val type = BadScreen.getType(event.screen)
+            val evn = SlotEvent(slot, slot.item, slot.containerSlot, type != null, event)
             if (!evn.post()) return@on
 
             event.cancel()
-            onCancel("&cPrevented ${if (selling) "selling" else if (salvage) "salvaging" else "placing"} item", evn.actors)
+            onCancel("&cPrevented ${type?.action ?: "placing"} item", evn.actors)
         }
 
         EventBus.on<SwapItemEvent> { event ->
@@ -85,28 +82,45 @@ object PreventItem {
             if (slot1.container !== mc.player?.inventory) return@on
             if (slot2.container !== mc.player?.inventory) return@on
 
-            val selling = isSellScreen(event.screen)
-            val salvage = isSalvageScreen(event.screen)
-            val evn1 = SlotEvent(slot1, slot1.item, slot1.containerSlot, selling || salvage, event, slot2, false)
-            val evn2 = SlotEvent(slot2, slot2.item, slot2.containerSlot, selling || salvage, event, slot1, true)
+            val type = BadScreen.getType(event.screen)
+            val evn1 = SlotEvent(slot1, slot1.item, slot1.containerSlot, type != null, event, slot2, false)
+            val evn2 = SlotEvent(slot2, slot2.item, slot2.containerSlot, type != null, event, slot1, true)
             if (!evn1.post() && !evn2.post()) return@on
 
             event.cancel()
-            onCancel("&cPrevented moving item", (evn1.actors.toSet() + evn2.actors).toList())
+            onCancel("&cPrevented ${type?.action ?: "moving"} item", (evn1.actors.toSet() + evn2.actors).toList())
         }
     }
 
-    fun isSellScreen(screen: AbstractContainerScreen<*>): Boolean {
-        val hopper = screen.menu.items.getOrNull(49) ?: return false
-        val lore = ItemUtils.lore(hopper, false) ?: return false
-        val first = lore.getOrNull(0) ?: return false
-        if (first == "Click items in your inventory to sell") return true
-        val last = lore.getOrNull(lore.size - 1) ?: return false
-        return last == "Click to buyback!"
-    }
+    enum class BadScreen(val action: String) {
+        Salvage("salvaging") {
+            override fun matches(screen: AbstractContainerScreen<*>): Boolean {
+                return screen.title.string == "Salvage Items"
+            }
+        },
+        Trade("trading") {
+            val titleReg = "^You\\s+\\w+$".toRegex()
 
-    fun isSalvageScreen(screen: AbstractContainerScreen<*>): Boolean {
-        return screen.title.string == "Salvage Items"
+            override fun matches(screen: AbstractContainerScreen<*>): Boolean {
+                return titleReg.matches(screen.title.string)
+            }
+        },
+        Sell("selling") {
+            override fun matches(screen: AbstractContainerScreen<*>): Boolean {
+                val hopper = screen.menu.items.getOrNull(49) ?: return false
+                val lore = ItemUtils.lore(hopper, false) ?: return false
+                val first = lore.getOrNull(0) ?: return false
+                if (first == "Click items in your inventory to sell") return true
+                val last = lore.getOrNull(lore.size - 1) ?: return false
+                return last == "Click to buyback!"
+            }
+        };
+
+        abstract fun matches(screen: AbstractContainerScreen<*>): Boolean
+
+        companion object {
+            fun getType(screen: AbstractContainerScreen<*>) = entries.find { it.matches(screen) }
+        }
     }
 
     @Ordered class SlotEvent(
