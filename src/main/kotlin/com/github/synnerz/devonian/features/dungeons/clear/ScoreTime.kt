dd5d30015e1a6d15690d8a5bc05edfa545a4da1b
Doc <65479309+DocilElm@users.noreply.github.com>
2026-02-07 20:57:59 -0700

    feat: add alert opts to ScoreTime

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/clear/ScoreTime.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/clear/ScoreTime.kt
@@ -5,6 +5,7 @@ import com.github.synnerz.devonian.api.dungeon.Dungeons
 import com.github.synnerz.devonian.api.events.WorldChangeEvent
 import com.github.synnerz.devonian.config.Categories
 import com.github.synnerz.devonian.features.Feature
+import com.github.synnerz.devonian.hud.texthud.Alert
 import com.github.synnerz.devonian.utils.StringUtils
 
 object ScoreTime : Feature(
@@ -14,6 +15,18 @@ object ScoreTime : Feature(
     "catacombs",
     subcategory = "QOL",
 ) {
+    private val SETTING_SHOW_ALERT = addSwitch(
+        "showAlert",
+        true,
+        "Displays an alert whenever S/S+ is reached",
+        "ScoreTime Alert"
+    )
+    private val SETTING_PLAY_SOUND = addSwitch(
+        "playSound",
+        true,
+        "Plays a sound whenever the Alert is displayed (MUST have the alert enabled)",
+        "ScoreTime Sound"
+    )
     private var trigger270 = false
     private var trigger300 = false
 
@@ -24,12 +37,16 @@ object ScoreTime : Feature(
             if (it >= 270 && !trigger270) {
                 trigger270 = true
                 ChatUtils.sendMessage("&eS &breached at &a${StringUtils.formatSeconds(Dungeons.timeElapsed.value.toLong())} &7(${Dungeons.floor.shortName})", true)
+                if (SETTING_SHOW_ALERT.get())
+                    Alert.show("&eS &breached at &a${StringUtils.formatSeconds(Dungeons.timeElapsed.value.toLong())} &7(${Dungeons.floor.shortName})", 1500, SETTING_PLAY_SOUND.get())
                 return@listen
             }
             if (it < 300 || trigger300) return@listen
 
-            ChatUtils.sendMessage("&6S+ &breached at &a${StringUtils.formatSeconds(Dungeons.timeElapsed.value.toLong())} &7(${Dungeons.floor.shortName})", true)
             trigger300 = true
+            ChatUtils.sendMessage("&6S+ &breached at &a${StringUtils.formatSeconds(Dungeons.timeElapsed.value.toLong())} &7(${Dungeons.floor.shortName})", true)
+            if (SETTING_SHOW_ALERT.get())
+                Alert.show("&6S+ &breached at &a${StringUtils.formatSeconds(Dungeons.timeElapsed.value.toLong())} &7(${Dungeons.floor.shortName})", 1500, SETTING_PLAY_SOUND.get())
         }
     }
 
