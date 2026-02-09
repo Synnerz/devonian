b09b624adee4111156c148e7d29979d15e8499c6
Doc <65479309+DocilElm@users.noreply.github.com>
2026-02-08 19:16:06 -0700

    feat: make SecretsSound sound customizable

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/SecretsSound.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/SecretsSound.kt
@@ -1,24 +1,48 @@
 package com.github.synnerz.devonian.features.dungeons
 
+import com.github.synnerz.devonian.api.ChatUtils
 import com.github.synnerz.devonian.api.Scheduler
 import com.github.synnerz.devonian.api.dungeon.DungeonEvent
 import com.github.synnerz.devonian.api.events.ChatEvent
+import com.github.synnerz.devonian.commands.DevonianCommand
 import com.github.synnerz.devonian.config.Categories
 import com.github.synnerz.devonian.features.Feature
-import net.minecraft.sounds.SoundEvents
+import com.github.synnerz.devonian.utils.CustomSounds
+import net.minecraft.core.registries.BuiltInRegistries
 
 object SecretsSound : Feature(
     "secretsSound",
-    "Plays a sound whenever you click, pick up (a secret) or kill a bat (This also plays an anvil sound whenever the chest is locked).",
+    "Plays a sound whenever you click, pick up (a secret) or kill a bat (This also plays an anvil sound whenever the chest is locked). /dv secretsound to customize",
     Categories.DUNGEONS,
     "catacombs",
     subcategory = "QOL",
 ) {
     private val lockedChestRegex = "^That chest is locked!$".toRegex()
-    private val successSound = SoundEvents.BLAZE_HURT
-    private val declineSound = SoundEvents.ANVIL_PLACE
+    private val successOpt = CustomSounds.create("SecretsSoundSuccess", "minecraft:entity.blaze.hurt")
+    private val declineOpt = CustomSounds.create("SecretsSoundDecline", "minecraft:block.anvil.place")
 
     override fun initialize() {
+        DevonianCommand.command.subcommand("secretsound") { _, args ->
+            val type = args.firstOrNull() ?: return@subcommand 0
+            val volume = args.getOrNull(1) as? Float ?: 1f
+            val pitch = args.getOrNull(2) as? Float ?: 1f
+            var soundName = args.getOrNull(3) as? String
+            if (soundName.isNullOrEmpty()) {
+                soundName = if (type == "SUCCESS") "minecraft:entity.blaze.hurt"
+                else "minecraft:block.anvil.place"
+            }
+            if (type == "SUCCESS") successOpt.setValues(soundName, volume, pitch)
+            else declineOpt.setValues(soundName, volume, pitch)
+            ChatUtils.sendMessage("&aSuccessfully set SecretsSound to &6$soundName", true)
+            1
+        }
+            .string("type")
+            .float("volume", 0f, 1f)
+            .float("pitch", 0f, 1f)
+            .greedyString("sound")
+            .suggest("type", *listOf("SUCCESS", "DECLINE").toTypedArray())
+            .suggest("sound", *BuiltInRegistries.SOUND_EVENT.entrySet().map { "${it.value.location.namespace}:${it.value.location.path}" }.toTypedArray())
+
         on<DungeonEvent.SecretPickup> { playSound() }
         on<DungeonEvent.SecretClicked> { playSound() }
         on<DungeonEvent.SecretBatSound> {
@@ -37,10 +61,7 @@ object SecretsSound : Feature(
     }
 
     private fun playSound(declined: Boolean = false) {
-        if (declined) {
-            minecraft.player?.playSound(declineSound, 1f, 1f)
-            return
-        }
-        minecraft.player?.playSound(successSound, 1f, 2f)
+        if (declined) return declineOpt.play()
+        successOpt.play()
     }
 }
