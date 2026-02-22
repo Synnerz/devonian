9c4b6b0324049ac94639bc8c711e7957f3ba5527
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2026-02-21 23:00:55 -0500

    rewrite mimic chest detection to work with bbe

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/clear/BoxMimicChest.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/clear/BoxMimicChest.kt
@@ -1,15 +1,17 @@
 package com.github.synnerz.devonian.features.dungeons.clear
 
+import com.github.synnerz.devonian.api.WorldUtils
+import com.github.synnerz.devonian.api.dungeon.DungeonScanner
 import com.github.synnerz.devonian.api.dungeon.Stages
-import com.github.synnerz.devonian.api.events.PostRenderTileEntityEvent
 import com.github.synnerz.devonian.api.events.RenderWorldEvent
+import com.github.synnerz.devonian.api.events.TickEvent
+import com.github.synnerz.devonian.api.events.WorldChangeEvent
 import com.github.synnerz.devonian.config.Categories
 import com.github.synnerz.devonian.features.Feature
 import com.github.synnerz.devonian.utils.BasicState
 import com.github.synnerz.devonian.utils.render.Render3DImmediate
-import net.minecraft.client.renderer.blockentity.state.ChestRenderState
 import net.minecraft.core.BlockPos
-import net.minecraft.world.level.block.state.properties.ChestType
+import net.minecraft.world.level.block.Blocks
 import java.awt.Color
 
 object BoxMimicChest : Feature(
@@ -30,19 +32,28 @@ object BoxMimicChest : Feature(
         "Mimic Chest Color",
     )
 
-    private val hahaSilly = mutableListOf<BlockPos>()
+    private val chests = mutableListOf<BlockPos>()
 
     override fun initialize() {
-        on<PostRenderTileEntityEvent> { event ->
-            val state = event.entityState as? ChestRenderState ?: return@on
-            if (state.material != ChestRenderState.ChestMaterialType.TRAPPED) return@on
-            if (state.type != ChestType.SINGLE) return@on
+        on<TickEvent> {
+            chests.clear()
 
-            hahaSilly.add(event.entityState.blockPos)
+            val room = DungeonScanner.currentRoom ?: return@on
+            if (!room.hasRotation()) return@on
+
+            val id = room.roomID ?: return@on
+            val waypoints = DungeonWaypoints.waypointsData.getOrNull(id)?.waypoints ?: return@on
+            val chestLocs = waypoints[DungeonWaypoints.WaypointType.CHEST] ?: return@on
+
+            chestLocs.forEach { (cx, y, cz) ->
+                val (x, z) = room.fromComp(cx, cz) ?: return@forEach
+                val bs = WorldUtils.getBlockState(x, y, z) ?: return@forEach
+                if (bs.block === Blocks.TRAPPED_CHEST) chests.add(BlockPos(x, y, z))
+            }
         }
 
         on<RenderWorldEvent> {
-            hahaSilly.forEach {
+            chests.forEach {
                 Render3DImmediate.renderFilledBox(
                     it.x + 0.05, it.y + 0.0, it.z + 0.05,
                     0.9, 0.9,
@@ -50,7 +61,10 @@ object BoxMimicChest : Feature(
                     phase = false,
                 )
             }
-            hahaSilly.clear()
         }
+    }
+
+    override fun onWorldChange(event: WorldChangeEvent) {
+        chests.clear()
     }
 }
