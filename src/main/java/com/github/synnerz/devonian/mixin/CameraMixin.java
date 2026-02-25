716a61c30f0a158c0d65d830d6abb23aaa532c80
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2026-02-24 20:28:12 -0500

    feat: FixRidingCamera

--- src/main/java/com/github/synnerz/devonian/mixin/CameraMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/CameraMixin.java
@@ -1,7 +1,12 @@
 package com.github.synnerz.devonian.mixin;
 
 import com.github.synnerz.devonian.features.misc.ChangeCrouchHeight;
+import com.github.synnerz.devonian.features.misc.FixRidingCamera;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.Camera;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,5 +23,23 @@ public class CameraMixin {
         if (!ChangeCrouchHeight.INSTANCE.isEnabled()) return;
 
         if (ChangeCrouchHeight.INSTANCE.tick((Camera) (Object) this)) ci.cancel();
+    }
+
+    @WrapOperation(
+        method = "setup",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getViewXRot(F)F")
+    )
+    private float devonian$fixRidingCameraX(Entity instance, float f, Operation<Float> original) {
+        if (!FixRidingCamera.INSTANCE.isEnabled() || !(instance instanceof LocalPlayer)) return original.call(instance, f);
+        return instance.getXRot();
+    }
+
+    @WrapOperation(
+        method = "setup",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getViewYRot(F)F")
+    )
+    private float devonian$fixRidingCameraY(Entity instance, float f, Operation<Float> original) {
+        if (!FixRidingCamera.INSTANCE.isEnabled() || !(instance instanceof LocalPlayer)) return original.call(instance, f);
+        return instance.getYRot();
     }
 }
