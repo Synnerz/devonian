a9c5061bd2c0a24c5f45098cd9930d26fb95e103
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2026-03-03 01:17:03 -0500

    rewrite StarsStackSize

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/StarsStackSize.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/StarsStackSize.kt
@@ -2,9 +2,13 @@ package com.github.synnerz.devonian.features.dungeons
 
 import com.github.synnerz.devonian.api.ItemUtils
 import com.github.synnerz.devonian.api.events.PostRenderHotbarSlotEvent
-import com.github.synnerz.devonian.api.events.RenderSlotEvent
+import com.github.synnerz.devonian.api.events.PostRenderSlotEvent
 import com.github.synnerz.devonian.config.Categories
 import com.github.synnerz.devonian.features.Feature
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.world.item.ItemStack
+import java.util.IdentityHashMap
+import kotlin.jvm.optionals.getOrNull
 
 object StarsStackSize : Feature(
     "starsStackSize",
@@ -12,42 +16,42 @@ object StarsStackSize : Feature(
     Categories.DUNGEONS,
     subcategory = "QOL"
 ) {
+    private data class Entry(val str: String, val width: Int) {
+        companion object {
+            val EMPTY = Entry("", 0)
+        }
+    }
+    private val cache = IdentityHashMap<ItemStack, Entry>()
+
+    private fun draw(stack: ItemStack, x: Int, y: Int, ctx: GuiGraphics) {
+        if (stack.isEmpty) return
+
+        val entry = cache.getOrPut(stack) {
+            val extraAttributes = ItemUtils.extraAttributes(stack) ?: return@getOrPut Entry.EMPTY
+            val stars = extraAttributes.getInt("upgrade_level").getOrNull() ?: return@getOrPut Entry.EMPTY
+            val amount = "$stars"
+            val width = minecraft.font.width(amount)
+            return@getOrPut Entry(amount, width)
+        } ?: return
+        if (entry === Entry.EMPTY) return
+
+        ctx.drawString(
+            minecraft.font,
+            entry.str,
+            x + 17 - entry.width,
+            y + 9,
+            -1,
+            true
+        )
+    }
+
     override fun initialize() {
         on<PostRenderHotbarSlotEvent> { event ->
-            val itemStack = event.item
-            val extraAttributes = ItemUtils.extraAttributes(itemStack) ?: return@on
-            val stars = extraAttributes.get("upgrade_level")?.asInt() ?: return@on
-            if (!stars.isPresent) return@on
-            val amount = "${stars.get()}"
-            val width = minecraft.font.width(amount)
-
-            event.ctx.drawString(
-                minecraft.font,
-                amount,
-                event.x + 17 - width,
-                event.y + 9,
-                -1,
-                true
-            )
+            draw(event.item, event.x, event.y, event.ctx)
         }.prio = 30
 
-        on<RenderSlotEvent> { event ->
-            val slot = event.slot
-            val itemStack = slot.item
-            val extraAttributes = ItemUtils.extraAttributes(itemStack) ?: return@on
-            val stars = extraAttributes.get("upgrade_level")?.asInt() ?: return@on
-            if (!stars.isPresent) return@on
-            val amount = "${stars.get()}"
-            val width = minecraft.font.width(amount)
-
-            event.ctx.drawString(
-                minecraft.font,
-                amount,
-                slot.x + 17 - width,
-                slot.y + 9,
-                -1,
-                true
-            )
+        on<PostRenderSlotEvent> { event ->
+            draw(event.slot.item, event.slot.x, event.slot.y, event.ctx)
         }.prio = 30
     }
 }
