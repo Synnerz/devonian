447de6710083af4b351c9888d3dd25bdb73263fa
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2026-03-03 01:24:53 -0500

    add type checking to json

--- src/main/kotlin/com/github/synnerz/devonian/config/json/JsonDataObject.kt
+++ src/main/kotlin/com/github/synnerz/devonian/config/json/JsonDataObject.kt
@@ -4,6 +4,7 @@ import com.github.synnerz.devonian.config.DataObject
 import com.github.synnerz.devonian.utils.PersistentJson
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
 
 class JsonDataObject(val json: JsonObject = JsonObject()) : DataObject() {
     override fun <T> set(key: String, value: T) = apply {
@@ -18,14 +19,36 @@ class JsonDataObject(val json: JsonObject = JsonObject()) : DataObject() {
         }
     }
 
-    override fun getString(key: String): String? = json.get(key)?.asString
-    override fun getInt(key: String): Int? = json.get(key)?.asInt
-    override fun getDouble(key: String): Double? = json.get(key)?.asDouble
-    override fun getFloat(key: String): Float? = json.get(key)?.asFloat
-    override fun getLong(key: String): Long? = json.get(key)?.asLong
-    override fun getBoolean(key: String): Boolean? = json.get(key)?.asBoolean
-    override fun getList(key: String): List<*>? = json.get(key)?.asJsonArray?.toList()
-    override fun getMap(key: String): Map<*, *>? = json.get(key)?.asJsonObject?.asMap()
+    override fun getString(key: String): String? = json.get(key)?.let {
+            if ((it as? JsonPrimitive)?.isString == true) it.asString
+            else null
+        }
+    override fun getInt(key: String): Int? = json.get(key)?.let {
+            if ((it as? JsonPrimitive)?.isNumber == true) it.asInt
+            else null
+        }
+    override fun getDouble(key: String): Double? = json.get(key)?.let {
+            if ((it as? JsonPrimitive)?.isNumber == true) it.asDouble
+            else null
+        }
+    override fun getFloat(key: String): Float? = json.get(key)?.let {
+            if ((it as? JsonPrimitive)?.isNumber == true) it.asFloat
+            else null
+        }
+        override fun getLong(key: String): Long? = json.get(key)?.let {
+            if ((it as? JsonPrimitive)?.isNumber == true) it.asLong
+            else null
+        }
+        override fun getBoolean(key: String): Boolean? = json.get(key)?.let {
+            if ((it as? JsonPrimitive)?.isBoolean == true) it.asBoolean
+            else null
+        }
+    override fun getList(key: String): List<*>? = json.get(key)?.let {
+            (it as? JsonArray)?.asList()
+        }
+    override fun getMap(key: String): Map<*, *>? = json.get(key)?.let {
+            (it as? JsonObject)?.asMap()
+        }
 
     private val cached = mutableMapOf<String, JsonDataObject>()
 
