7c9376dd29116e4a5489dd05f3404b2af6539653
Doc <65479309+DocilElm@users.noreply.github.com>
2026-03-02 22:09:41 -0700

    feat: make SpiritLeapKeys scanner faster

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/SpiritLeapKeys.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/SpiritLeapKeys.kt
@@ -1,16 +1,18 @@
 package com.github.synnerz.devonian.features.dungeons
 
+import com.github.synnerz.devonian.api.Scheduler
 import com.github.synnerz.devonian.api.ScreenUtils
 import com.github.synnerz.devonian.api.dungeon.Dungeons
 import com.github.synnerz.devonian.api.dungeon.Stages
+import com.github.synnerz.devonian.api.events.ClientContainerCloseEvent
 import com.github.synnerz.devonian.api.events.GuiKeyDownEvent
-import com.github.synnerz.devonian.api.events.PacketReceivedEvent
+import com.github.synnerz.devonian.api.events.ServerContainerCloseEvent
+import com.github.synnerz.devonian.api.events.ServerContainerOpenEvent
+import com.github.synnerz.devonian.api.events.ServerContainerSetSlotEvent
 import com.github.synnerz.devonian.api.events.WorldChangeEvent
 import com.github.synnerz.devonian.config.Categories
 import com.github.synnerz.devonian.features.Feature
 import com.github.synnerz.devonian.utils.BasicState
-import net.minecraft.network.protocol.game.ClientboundContainerSetContentPacket
-import net.minecraft.network.protocol.game.ClientboundOpenScreenPacket
 import net.minecraft.world.item.Items
 import org.lwjgl.glfw.GLFW
 
@@ -42,30 +44,40 @@ object SpiritLeapKeys : Feature(
     private var containerId = -1
 
     override fun initialize() {
-        on<PacketReceivedEvent> { event ->
-            val packet = event.packet
-            if (packet is ClientboundOpenScreenPacket) {
-                if (packet.title.string != "Spirit Leap") return@on
-                containerId = packet.containerId
+        on<ServerContainerOpenEvent> { event ->
+            if (event.titleStr == "Spirit Leap") {
+                if (containerId != -1) Scheduler.scheduleTask { playersData.clear() }
+                containerId = event.containerId
+            }
+        }
+
+        on<ServerContainerCloseEvent> {
+            Scheduler.scheduleTask { playersData.clear() }
+            containerId = -1
+        }
+
+        on<ClientContainerCloseEvent> {
+            playersData.clear()
+            containerId = -1
+        }
+
+        on<ServerContainerSetSlotEvent> { event ->
+            if (containerId == -1) return@on
+
+            val idx = event.slot
+            if (idx !in 9..18) return@on
+            if (idx == 17) {
+                Scheduler.scheduleTask { playersData.sortWith(leapComparator) }
+                containerId = -1
                 return@on
             }
-            if (containerId == -1 || packet !is ClientboundContainerSetContentPacket) return@on
 
-            val items = packet.items
-            playersData.clear()
+            val itemStack = event.itemStack
+            if (itemStack.item != Items.PLAYER_HEAD) return@on
+            val name = itemStack.customName?.string ?: return@on
+            val data = Dungeons.players[name] ?: return@on
 
-            for (idx in 9..18) {
-                val itemStack = items.getOrNull(idx) ?: continue
-                if (itemStack.item != Items.PLAYER_HEAD) continue
-                val name = itemStack.customName?.string ?: continue
-
-                val data = Dungeons.players[name] ?: continue
-                playersData.add(CustomLeapGui.LeapPlayer(idx, name, data.role, data.isDead))
-            }
-
-            playersData.sortWith(leapComparator)
-
-            containerId = -1
+            Scheduler.scheduleTask { playersData.add(CustomLeapGui.LeapPlayer(idx, name, data.role, data.isDead)) }
         }
 
         on<GuiKeyDownEvent> { event ->
@@ -85,5 +97,6 @@ object SpiritLeapKeys : Feature(
 
     override fun onWorldChange(event: WorldChangeEvent) {
         playersData.clear()
+        containerId = -1
     }
 }
