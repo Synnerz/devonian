b6aed307bf674ef53c95229064826e76f642ee7a
Doc <65479309+DocilElm@users.noreply.github.com>
2026-03-02 22:27:15 -0700

    internal: add support for loud sounds

--- src/main/kotlin/com/github/synnerz/devonian/utils/CustomSounds.kt
+++ src/main/kotlin/com/github/synnerz/devonian/utils/CustomSounds.kt
@@ -1,13 +1,24 @@
 package com.github.synnerz.devonian.utils
 
 import com.github.synnerz.devonian.Devonian
+import com.github.synnerz.devonian.api.events.EventBus
 import com.github.synnerz.devonian.api.events.SoundPlayEvent
+import com.github.synnerz.devonian.api.events.WorldChangeEvent
 import com.github.synnerz.devonian.config.Config
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.resources.Identifier
 import net.minecraft.sounds.SoundEvent
 
 object CustomSounds {
+    val sounds = mutableListOf<CustomSound>()
+    var queued: CustomSound? = null
+
+    init {
+        EventBus.on<WorldChangeEvent> {
+            queued = null
+        }
+    }
+
     fun create(key: String, default: String): CustomSound = CustomSound(key, default, 1f, 1f)
 
     data class CustomSound(
@@ -31,6 +42,8 @@ object CustomSounds {
                 Config.get<Float>("$key\$Pitch")?.let { setPitch(it) }
                 Config.get<String>(key)?.let { setValue(it) }
             }
+
+            sounds.add(this)
         }
 
         fun setVolume(volume: Float) {
@@ -55,11 +68,29 @@ object CustomSounds {
             setPitch(pitch)
         }
 
+        fun isLoud() = volume > 1f
+
+        fun que() {
+            queued = this
+        }
+
+        fun unque() {
+            queued = null
+        }
+
+        inline fun quePop(block: () -> Unit) {
+            que()
+            block()
+            unque()
+        }
+
         /**
          * * NOTE: call on main thread
          */
         fun play() {
-            soundEvent?.let { Devonian.minecraft.player?.playSound(it, volume, pitch) }
+            quePop {
+                soundEvent?.let { Devonian.minecraft.player?.playSound(it, volume, pitch) }
+            }
         }
 
         /**
@@ -67,12 +98,14 @@ object CustomSounds {
          */
         fun playWithEvent(event: SoundPlayEvent) {
             soundEvent?.let {
-                Devonian.minecraft.level?.playLocalSound(
-                    event.x, event.y, event.z,
-                    it, event.category,
-                    volume, pitch,
-                    false
-                )
+                quePop {
+                    Devonian.minecraft.level?.playLocalSound(
+                        event.x, event.y, event.z,
+                        it, event.category,
+                        volume, pitch,
+                        false
+                    )
+                }
             }
         }
     }
