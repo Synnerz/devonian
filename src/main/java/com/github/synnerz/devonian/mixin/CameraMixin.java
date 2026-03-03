bad7df6d30d6157fda147383aae46cc8cd2fd5cc
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2026-03-03 02:22:19 -0500

    fix sky not rendering

--- src/main/java/com/github/synnerz/devonian/mixin/CameraMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/CameraMixin.java
@@ -2,27 +2,41 @@ package com.github.synnerz.devonian.mixin;
 
 import com.github.synnerz.devonian.features.misc.ChangeCrouchHeight;
 import com.github.synnerz.devonian.features.misc.FixRidingCamera;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.Camera;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.Entity;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Camera.class)
 public class CameraMixin {
+    @Unique
+    private boolean cancelTick = false;
+
     @Inject(
         method = "tick",
-        at = @At("HEAD"),
-        cancellable = true
+        at = @At("HEAD")
     )
     private void devonian$changeCrouchHeight(CallbackInfo ci) {
-        if (!ChangeCrouchHeight.INSTANCE.isEnabled()) return;
+        cancelTick = ChangeCrouchHeight.INSTANCE.tick((Camera) (Object) this);
+    }
 
-        if (ChangeCrouchHeight.INSTANCE.tick((Camera) (Object) this)) ci.cancel();
+    @WrapWithCondition(
+        method = "tick",
+        at = {
+            @At(value = "FIELD", target = "Lnet/minecraft/client/Camera;eyeHeightOld:F", opcode = Opcodes.PUTFIELD),
+            @At(value = "FIELD", target = "Lnet/minecraft/client/Camera;eyeHeight:F", opcode = Opcodes.PUTFIELD),
+        }
+    )
+    private boolean devonian$changeCrouchHeightOv(Camera instance, float value) {
+        return !cancelTick;
     }
 
     @WrapOperation(
