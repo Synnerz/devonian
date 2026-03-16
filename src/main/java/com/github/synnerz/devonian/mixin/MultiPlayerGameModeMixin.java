3fa81c383044bad2d255db711fb115820e1a3261
Doc <65479309+DocilElm@users.noreply.github.com>
2026-03-16 02:58:18 -0700

    internal: maybe fix ss block wrong click

--- src/main/java/com/github/synnerz/devonian/mixin/MultiPlayerGameModeMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/MultiPlayerGameModeMixin.java
@@ -5,14 +5,18 @@ import com.github.synnerz.devonian.features.misc.FixRedVignette;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.border.WorldBorder;
+import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MultiPlayerGameMode.class)
 public class MultiPlayerGameModeMixin {
@@ -25,9 +29,15 @@ public class MultiPlayerGameModeMixin {
         return true;
     }
 
-    @WrapOperation(method = "performUseItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientPacketListener;isFeatureEnabled(Lnet/minecraft/world/flag/FeatureFlagSet;)Z"))
-    private boolean devonian$onItemUse(ClientPacketListener instance, FeatureFlagSet featureFlagSet, Operation<Boolean> original, @Local BlockPos blockPos, @Local ItemStack itemStack) {
-        if (new BlockInteractEvent(itemStack, blockPos).post()) return false;
-        return original.call(instance, featureFlagSet);
+    @Inject(
+            method = "performUseItemOn",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/multiplayer/ClientLevel;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;", shift = At.Shift.AFTER
+            ),
+            cancellable = true
+    )
+    private void devonian$onItemUse(LocalPlayer localPlayer, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir, @Local BlockPos blockPos, @Local ItemStack itemStack) {
+        if (new BlockInteractEvent(itemStack, blockPos).post()) cir.setReturnValue(InteractionResult.PASS);
     }
 }
