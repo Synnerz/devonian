cd3ca528929c0503972db0bead0d46303b67e6eb
Doc <65479309+DocilElm@users.noreply.github.com>
2026-03-15 22:05:50 -0700

    internal: add `fakeSkull` helper to `ItemUtils`

--- src/main/kotlin/com/github/synnerz/devonian/api/ItemUtils.kt
+++ src/main/kotlin/com/github/synnerz/devonian/api/ItemUtils.kt
@@ -1,9 +1,16 @@
 package com.github.synnerz.devonian.api
 
 import com.github.synnerz.devonian.utils.StringUtils.colorCodes
+import com.google.common.collect.ImmutableMultimap
+import com.mojang.authlib.GameProfile
+import com.mojang.authlib.properties.Property
+import com.mojang.authlib.properties.PropertyMap
 import net.minecraft.core.component.DataComponents
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
+import net.minecraft.world.item.component.ResolvableProfile
+import java.util.*
 
 object ItemUtils {
     fun skyblockId(itemStack: ItemStack): String? {
@@ -31,5 +38,21 @@ object ItemUtils {
         if (colorCodes) return lore.map { it.colorCodes() }
 
         return lore.map { it.string }
+    }
+
+    fun fakeSkull(texture: String): ItemStack {
+        val uuid = UUID.randomUUID()
+        val gameProfile = GameProfile(
+            uuid,
+            "devonian\$fakeSkull",
+            PropertyMap(ImmutableMultimap.of(
+                "textures",
+                Property("textures", texture)
+            ))
+        )
+
+        return ItemStack(Items.PLAYER_HEAD).apply {
+            set(DataComponents.PROFILE, ResolvableProfile.createResolved(gameProfile))
+        }
     }
 }
