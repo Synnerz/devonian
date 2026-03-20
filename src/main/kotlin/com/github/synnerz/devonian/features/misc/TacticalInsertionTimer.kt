4d13f5c554888c86b671b4bbbe8691f6b2a518d6
Doc <65479309+DocilElm@users.noreply.github.com>
2026-03-20 02:01:12 -0700

    feat: fix TacticalInsertionTimer not working right

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/TacticalInsertionTimer.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/TacticalInsertionTimer.kt
@@ -3,22 +3,28 @@ package com.github.synnerz.devonian.features.misc
 import com.github.synnerz.devonian.api.ItemUtils
 import com.github.synnerz.devonian.api.events.*
 import com.github.synnerz.devonian.hud.texthud.TextHudFeature
-import net.minecraft.world.InteractionHand
 
 object TacticalInsertionTimer : TextHudFeature(
     "tacticalInsertionTimer",
     "Displays a 3 second timer whenever you right click Tactical Insertion.",
     subcategory = "General",
 ) {
+    private var lastClick = -1
     private var timer = 0
 
     override fun initialize() {
+        on<UseItemEvent> { event ->
+            val itemStack = minecraft.player?.getItemInHand(event.hand) ?: return@on
+            val sbId = ItemUtils.skyblockId(itemStack) ?: return@on
+            if (sbId != "TACTICAL_INSERTION") return@on
+
+            lastClick = EventBus.serverTicks(true)
+        }
+
         on<SoundPlayEvent> { event ->
             if (event.sound != "minecraft:item.flintandsteel.use" || event.pitch != 0.74603176f) return@on
-            if (timer != 0) return@on
-            val heldItem = minecraft.player?.getItemInHand(InteractionHand.MAIN_HAND) ?: return@on
-            val sbId = ItemUtils.skyblockId(heldItem) ?: return@on
-            if (sbId != "TACTICAL_INSERTION") return@on
+            if (timer != 0 || lastClick == -1) return@on
+            if (lastClick - EventBus.serverTicks() > 10) return@on
 
             timer = EventBus.serverTicks() + 60
         }
@@ -47,6 +53,7 @@ object TacticalInsertionTimer : TextHudFeature(
 
     override fun onWorldChange(event: WorldChangeEvent) {
         timer = 0
+        lastClick = -1
     }
 
     override fun getEditText(): List<String> = listOf("&a3.00s")
