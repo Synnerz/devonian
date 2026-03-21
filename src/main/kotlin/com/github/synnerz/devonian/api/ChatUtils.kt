c46fe40853675231df2ead735152fc0b48f8952a
Doc <65479309+DocilElm@users.noreply.github.com>
2026-03-21 14:01:39 -0700

    internal: forgot to do this when changing prefix

--- src/main/kotlin/com/github/synnerz/devonian/api/ChatUtils.kt
+++ src/main/kotlin/com/github/synnerz/devonian/api/ChatUtils.kt
@@ -16,7 +16,7 @@ import java.util.*
 import kotlin.math.roundToInt
 
 object ChatUtils {
-    const val prefix = "&8[&3&lDevonian&8]"
+    const val prefix = "&8&l[&3&lDevonian&8&l]&r"
     val chatLineIds = mutableMapOf<GuiMessage, Int>()
     val lineCache = IdentityHashMap<GuiMessage.Line, GuiMessage>()
     val chatComponentAccessor get() = Minecraft.getInstance().gui.chat as ChatComponentAccessor
