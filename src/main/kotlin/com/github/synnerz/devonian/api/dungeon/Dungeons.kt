2584625312955fb43e467c1f9b7b7b106a529e9a
Doc <65479309+DocilElm@users.noreply.github.com>
2026-05-08 14:37:38 -0700

    internal: fix bat killed event broken during derpy

--- src/main/kotlin/com/github/synnerz/devonian/api/dungeon/Dungeons.kt
+++ src/main/kotlin/com/github/synnerz/devonian/api/dungeon/Dungeons.kt
@@ -3,6 +3,7 @@ package com.github.synnerz.devonian.api.dungeon
 import com.github.synnerz.devonian.Devonian
 import com.github.synnerz.devonian.api.ItemUtils
 import com.github.synnerz.devonian.api.Location
+import com.github.synnerz.devonian.api.MayorApi
 import com.github.synnerz.devonian.api.Scheduler
 import com.github.synnerz.devonian.api.events.*
 import com.github.synnerz.devonian.commands.DevonianCommand
@@ -381,7 +382,7 @@ object Dungeons {
         EventBus.on<EntityDeathEvent> { event ->
             val entity = event.entity
             if (entity is Bat) {
-                if (entity.maxHealth != 100f) return@on
+                if (entity.maxHealth != if (MayorApi.isDerpy()) 200f else 100f) return@on
                 DungeonEvent.SecretBat(entity.x, entity.y, entity.z).post()
                 return@on
             }
