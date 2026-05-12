bd5b8b5d0655f3c4dbc83ad94c54e7f09953e57d
Doc <65479309+DocilElm@users.noreply.github.com>
2026-05-12 11:01:27 -0700

    build: add 26.2 snapshot 7 support

--- src/main/kotlin/com/github/synnerz/devonian/compat/ModMenu.kt
+++ src/main/kotlin/com/github/synnerz/devonian/compat/ModMenu.kt
@@ -1,13 +1,13 @@
 package com.github.synnerz.devonian.compat
 
 import com.github.synnerz.devonian.config.ui.talium.ConfigGui
-import com.terraformersmc.modmenu.api.ConfigScreenFactory
-import com.terraformersmc.modmenu.api.ModMenuApi
-
-internal class ModMenu : ModMenuApi {
-    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> {
-        return ConfigScreenFactory {
-            ConfigGui
-        }
-    }
+//import com.terraformersmc.modmenu.api.ConfigScreenFactory
+//import com.terraformersmc.modmenu.api.ModMenuApi
+// FIXME
+internal class ModMenu/* : ModMenuApi*/ {
+//    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> {
+//        return ConfigScreenFactory {
+//            ConfigGui
+//        }
+//    }
 }
