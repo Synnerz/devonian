a29e0bc327441a159de4062eca354e739aded721
Doc <65479309+DocilElm@users.noreply.github.com>
2026-06-05 12:52:05 -0700

    internal: fix ConfirmDisconnect not working

--- src/main/java/com/github/synnerz/devonian/mixin/PauseScreenMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/PauseScreenMixin.java
@@ -16,20 +16,19 @@ public abstract class PauseScreenMixin {
     @Unique
     long lastClick = -1L;
 
-    // FIXME
-//    @WrapMethod(method = "method_72129")
-//    private void devonian$onDisconnectButton(Operation<Void> original) {
-//        if (!ConfirmDisconnect.INSTANCE.isEnabled()) {
-//            original.call();
-//            return;
-//        }
-//        if (lastClick == -1L || System.currentTimeMillis() - lastClick < ConfirmDisconnect.INSTANCE.getSETTING_THRESHOLD().get()) {
-//            if (disconnectButton != null) disconnectButton.active = true;
-//            lastClick = System.currentTimeMillis();
-//            return;
-//        }
-//
-//        original.call();
-//        lastClick = -1L;
-//    }
+    @WrapMethod(method = "lambda$createPauseMenu$7")
+    private void devonian$onDisconnectButton(Operation<Void> original) {
+        if (!ConfirmDisconnect.INSTANCE.isEnabled()) {
+            original.call();
+            return;
+        }
+        if (lastClick == -1L || System.currentTimeMillis() - lastClick < ConfirmDisconnect.INSTANCE.getSETTING_THRESHOLD().get()) {
+            if (disconnectButton != null) disconnectButton.active = true;
+            lastClick = System.currentTimeMillis();
+            return;
+        }
+
+        original.call();
+        lastClick = -1L;
+    }
 }
