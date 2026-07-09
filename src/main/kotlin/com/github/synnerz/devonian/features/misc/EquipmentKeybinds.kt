6653f7d8327b098339e78b0e96367e9157dcda57
Doc <65479309+DocilElm@users.noreply.github.com>
2026-07-09 12:00:05 -0700

    feat: add EquipmentKeybinds

---
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/EquipmentKeybinds.kt
@@ -0,0 +1,43 @@
+package com.github.synnerz.devonian.features.misc
+
+import com.github.synnerz.devonian.api.ScreenUtils
+import com.github.synnerz.devonian.api.events.GuiClickEvent
+import com.github.synnerz.devonian.api.events.GuiKeyDownEvent
+import com.github.synnerz.devonian.features.Feature
+
+object EquipmentKeybinds : Feature(
+    "equipmentKeybinds",
+    "Whenever inside of equipment gui it will allow you to press the hotbar 1-9 keys to switch through your equipments.",
+    subcategory = "General",
+) {
+    override fun initialize() {
+        on<GuiKeyDownEvent> { event ->
+            val screen = event.screen
+            if (!screen.title.string.endsWith(") Equipment Sets")) return@on
+
+            minecraft.options.keyHotbarSlots.forEachIndexed { idx, v ->
+                if (!v.matches(event.event)) return@forEachIndexed
+
+                event.cancel()
+                ScreenUtils.click((36 + idx).coerceIn(36, 44))
+
+                return@on
+            }
+        }
+
+        on<GuiClickEvent> { event ->
+            if (!event.state) return@on
+
+            val screen = event.screen
+            if (!screen.title.string.endsWith(") Equipment Sets")) return@on
+
+            minecraft.options.keyHotbarSlots.forEachIndexed { idx, v ->
+                if (!v.matchesMouse(event.event)) return@forEachIndexed
+                event.cancel()
+                ScreenUtils.click((36 + idx).coerceIn(36, 44))
+
+                return@on
+            }
+        }
+    }
+}
