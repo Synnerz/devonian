5f26c31b3770499618db4b4bd953c7b2b31e7ff2
Doc <65479309+DocilElm@users.noreply.github.com>
2026-07-25 13:16:46 -0700

    internal: change CustomHypeSound to new command

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/CustomHypeSound.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/CustomHypeSound.kt
@@ -1,12 +1,10 @@
 package com.github.synnerz.devonian.features.misc
 
-import com.github.synnerz.devonian.api.ChatUtils
 import com.github.synnerz.devonian.api.ItemUtils
 import com.github.synnerz.devonian.api.Scheduler
 import com.github.synnerz.devonian.api.events.EventBus
 import com.github.synnerz.devonian.api.events.PacketSentEvent
 import com.github.synnerz.devonian.api.events.SoundPlayEvent
-import com.github.synnerz.devonian.commands.DevonianCommand
 import com.github.synnerz.devonian.features.Feature
 import com.github.synnerz.devonian.utils.CustomSounds
 import net.minecraft.network.protocol.game.ServerboundUseItemPacket
@@ -17,49 +15,12 @@ object CustomHypeSound : Feature(
     "`/dv hypesound`",
     subcategory = "General",
 ) {
-    private val soundOptions = listOf(
-        "minecraft:entity.blaze.hurt",
-        "minecraft:entity.experience_orb.pickup",
-        "minecraft:block.vault.break",
-        "minecraft:entity.elder_guardian.hurt_land",
-        "minecraft:item.totem.use",
-        "minecraft:block.sculk_catalyst.hit",
-        "minecraft:block.ender_chest.close",
-        "minecraft:block.note_block.iron_xylophone",
-    )
-    val customSound = CustomSounds.create("witherBladeSound", "minecraft:block.note_block.iron_xylophone")
+    private val customSound = CustomSounds.create("witherBladeSound", "minecraft:block.note_block.iron_xylophone")
     private val witherBlades = listOf("HYPERION", "VALKYRIE", "SCYLLA", "ASTRAEA")
     private var lastClick = -1
-    var playSound = false
 
     override fun initialize() {
-        DevonianCommand.command.subcommand("hypesound") { _, args ->
-            val volume = args.firstOrNull() as? Float?
-            val pitch = args.getOrNull(1) as? Float?
-            var soundName = args.getOrNull(2) as? String
-            if (soundName.isNullOrEmpty()) soundName = "minecraft:block.note_block.iron_xylophone"
-
-            customSound.setValues(
-                soundName,
-                volume ?: 1f,
-                pitch ?: 1f
-            )
-
-            if (customSound.soundEvent == null) {
-                ChatUtils.sendMessage("&4Cannot find sound: &6$soundName", true)
-                return@subcommand 0
-            }
-
-            ChatUtils.sendMessage("&aSuccessfully set wither blade sound to &6$soundName", true)
-            1
-        }
-            .float("volume", 0f, 10f)
-            .float("pitch", 0f, 10f)
-            .greedyString("sound")
-            .suggest(
-                "sound",
-                *soundOptions.toTypedArray()
-            )
+        customSound.registerCommand("hype")
 
         on<SoundPlayEvent> { event ->
             if (lastClick == -1) return@on
@@ -76,11 +37,7 @@ object CustomHypeSound : Feature(
             if (event.sound != "minecraft:entity.generic.explode" || event.volume != 1f || event.pitch != 1f) return@on
 
             event.cancel()
-            playSound = true
-            Scheduler.scheduleTask {
-                customSound.playWithEvent(event, false)
-                playSound = false
-            }
+            Scheduler.scheduleTask { customSound.playWithEvent(event, false) }
         }
 
         on<PacketSentEvent> { event ->
