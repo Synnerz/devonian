f1a91f63ed60e0a345d38acdd39d7e0fbc67c83d
Doc <65479309+DocilElm@users.noreply.github.com>
2026-08-05 14:22:17 -0700

    feat: fix TriviaSolver answer

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/solvers/TriviaSolver.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/solvers/TriviaSolver.kt
@@ -36,7 +36,7 @@ object TriviaSolver : Feature(
         "What is the status of Livid?" to listOf("Master Necromancer"),
         "What is the status of Sadan?" to listOf("Necromancer Lord"),
         "What is the status of Maxor, Storm, Goldor, and Necron?" to listOf("The Wither Lords"),
-        "How many total Fairy Souls are there?" to listOf("273 Fairy Souls"),
+        "How many total Fairy Souls are there?" to listOf("289 Fairy Souls"),
         "How many Fairy Souls are there in Spider's Den?" to listOf("19 Fairy Souls"),
         "How many Fairy Souls are there in Spiders Den?" to listOf("19 Fairy Souls"),
         "How many Fairy Souls are there in The End?" to listOf("12 Fairy Souls"),
