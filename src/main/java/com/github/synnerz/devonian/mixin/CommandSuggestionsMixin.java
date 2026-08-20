016cbcd5f9e8e2ef7f49f2fe0c700c5a03e20f9f
Doc <65479309+DocilElm@users.noreply.github.com>
2026-08-20 13:10:44 -0700

    feat: add CommandAliases to tab completions

---
+++ src/main/java/com/github/synnerz/devonian/mixin/CommandSuggestionsMixin.java
@@ -0,0 +1,40 @@
+package com.github.synnerz.devonian.mixin;
+
+import com.github.synnerz.devonian.features.misc.chat.CommandAliases;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.ParseResults;
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.minecraft.client.gui.components.CommandSuggestions;
+import net.minecraft.client.multiplayer.ClientSuggestionProvider;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(CommandSuggestions.class)
+public class CommandSuggestionsMixin {
+    @WrapOperation(
+            method = "updateCommandInfo",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/mojang/brigadier/CommandDispatcher;parse(Lcom/mojang/brigadier/StringReader;Ljava/lang/Object;)Lcom/mojang/brigadier/ParseResults;"
+            )
+    )
+    private ParseResults<ClientSuggestionProvider> devonian$onUpdateCommandInfo(
+            CommandDispatcher<ClientSuggestionProvider> instance, StringReader command, Object source,
+            Operation<ParseResults<ClientSuggestionProvider>> original,
+            @Local(name = "commands") CommandDispatcher<ClientSuggestionProvider> cmd
+    ) {
+        var aliases = CommandAliases.INSTANCE.getAliases();
+        if (!aliases.isEmpty()) {
+            aliases.forEach((alias) -> {
+                cmd.register(LiteralArgumentBuilder.literal(alias));
+            });
+            return instance.parse(command, (ClientSuggestionProvider) source);
+        }
+
+        return original.call(instance, command, source);
+    }
+}
