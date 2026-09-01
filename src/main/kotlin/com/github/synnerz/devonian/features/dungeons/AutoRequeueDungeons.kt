5836a22046cd569f7af769217779d1cb78738484
Doc <65479309+DocilElm@users.noreply.github.com>
2026-09-01 12:59:12 -0700

    feat: fix AutoRequeueDungeons not reque outside

--- src/main/kotlin/com/github/synnerz/devonian/features/dungeons/AutoRequeueDungeons.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/dungeons/AutoRequeueDungeons.kt
@@ -2,8 +2,10 @@ package com.github.synnerz.devonian.features.dungeons
 
 import com.github.synnerz.devonian.api.ChatUtils
 import com.github.synnerz.devonian.api.HypixelModApi
+import com.github.synnerz.devonian.api.Location
 import com.github.synnerz.devonian.api.Party
 import com.github.synnerz.devonian.api.dungeon.Dungeons
+import com.github.synnerz.devonian.api.dungeon.FloorType
 import com.github.synnerz.devonian.api.events.ChatChannelEvent
 import com.github.synnerz.devonian.api.events.ChatEvent
 import com.github.synnerz.devonian.api.events.WorldChangeEvent
@@ -18,10 +20,21 @@ object AutoRequeueDungeons : Feature(
     Categories.DUNGEONS,
     subcategory = "QOL",
 ) {
+    private val catacombsFloors = listOf(
+        "catacombs_floor_one",
+        "catacombs_floor_two",
+        "catacombs_floor_three",
+        "catacombs_floor_four",
+        "catacombs_floor_five",
+        "catacombs_floor_six",
+        "catacombs_floor_seven",
+    )
     private val extraStatsRegex = "^ *> EXTRA STATS <\$".toRegex()
     private var needsDowntime = Collections.newSetFromMap<String>(ConcurrentHashMap())
     private var lastQueue = 0
     private var startedRun = false
+    private var lastFloor: FloorType = FloorType.None
+    private var currentParty: String? = null
 
     private fun requeue() {
         if (!Party.isLeader && Party.inParty) return
@@ -30,7 +43,12 @@ object AutoRequeueDungeons : Feature(
             return
         }
 
-        ChatUtils.command("instancerequeue")
+        val cmd = when {
+            Location.area != "catacombs" ->
+                "joindungeon ${if (lastFloor.masterMode) "master_" else ""}${catacombsFloors.getOrNull(lastFloor.floorNum - 1) ?: return}"
+            else -> "instancerequeue"
+        }
+        ChatUtils.command(cmd)
         lastQueue = 0
     }
 
@@ -40,6 +58,15 @@ object AutoRequeueDungeons : Feature(
 
             startedRun = true
             HypixelModApi.requestPartyInfo()
+        }
+
+        on<HypixelModApi.PartyInfoPacket> {
+            if (Party.partyHash != currentParty && needsDowntime.isNotEmpty()) {
+                ChatUtils.sendMessage("&aDowntime has been reset.", true)
+                lastFloor = FloorType.None
+                needsDowntime.clear()
+            }
+            if (!Party.inParty) return@on
             lastQueue = Party.members.size
         }
 
@@ -60,6 +87,9 @@ object AutoRequeueDungeons : Feature(
         on<ChatEvent> { event ->
             if (!extraStatsRegex.matches(event.message)) return@on
 
+            lastFloor = Dungeons.floor
+            currentParty = Party.partyHash
+
             if (needsDowntime.isEmpty()) requeue()
             else ChatUtils.command("pc ${needsDowntime.joinToString(", ")} needs downtime")
         }
@@ -68,9 +98,5 @@ object AutoRequeueDungeons : Feature(
     override fun onWorldChange(event: WorldChangeEvent) {
         startedRun = false
         lastQueue = 0
-
-        if (needsDowntime.isEmpty()) return
-        ChatUtils.sendMessage("&aDowntime has been reset.", true)
-        needsDowntime.clear()
     }
 }
