8b303cf6dcb6b14b610e91a49ca7724e6f621f28
Doc <65479309+DocilElm@users.noreply.github.com>
2026-09-01 15:05:55 -0700

    feat: fix DisableWorldLoadingScreen breaking on reconnect and staying on reconfig screen

--- src/main/kotlin/com/github/synnerz/devonian/features/misc/hiders/DisableWorldLoadingScreen.kt
+++ src/main/kotlin/com/github/synnerz/devonian/features/misc/hiders/DisableWorldLoadingScreen.kt
@@ -1,9 +1,11 @@
 package com.github.synnerz.devonian.features.misc.hiders
 
+import com.github.synnerz.devonian.api.Location
 import com.github.synnerz.devonian.api.events.GuiOpenEvent
 import com.github.synnerz.devonian.config.Categories
 import com.github.synnerz.devonian.features.Feature
 import com.github.synnerz.devonian.mixin.accessor.LevelLoadingScreenAccessor
+import com.github.synnerz.devonian.utils.BasicState
 import net.minecraft.client.gui.GuiGraphicsExtractor
 import net.minecraft.client.gui.screens.LevelLoadingScreen
 import net.minecraft.client.gui.screens.Overlay
@@ -13,6 +15,10 @@ object DisableWorldLoadingScreen : Feature(
     category = Categories.VANILLA_TWEAKS,
     subcategory = "Hider"
 ) {
+    override fun createRequirements(): List<BasicState<Boolean>?> {
+        return super.createRequirements() + listOf(Location.stateInSkyblock)
+    }
+
     private var levelLoadingScreen: LevelLoadingScreenAccessor? = null
 
     override fun initialize() {
@@ -22,6 +28,7 @@ object DisableWorldLoadingScreen : Feature(
                     minecraft.gui.setOverlay(null)
                 return@on
             }
+            if (Location.area == null) return@on
             levelLoadingScreen = event.screen as? LevelLoadingScreenAccessor
             event.cancel()
             minecraft.gui.setScreen(null)
@@ -30,6 +37,7 @@ object DisableWorldLoadingScreen : Feature(
     }
 
     fun onPlayerLoaded() {
+        if (levelLoadingScreen == null) return
         minecraft.gui.setOverlay(null)
     }
 
@@ -38,8 +46,9 @@ object DisableWorldLoadingScreen : Feature(
 
         override fun tick() {
             val screen = levelLoadingScreen ?: return
-            if (screen.loadTracker.isLevelReady || (minecraft.singleplayerServer?.isReady ?: false))
+            if (screen.loadTracker.isLevelReady || (minecraft.singleplayerServer?.isReady ?: false)) {
                 minecraft.gui.setOverlay(null)
+            }
         }
     }
 }
