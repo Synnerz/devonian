428f3c18b76aa8767415c0c3d06ff8bed6fea9eb
Doc <65479309+DocilElm@users.noreply.github.com>
2026-09-19 12:06:00 -0700

    internal: fix KeyRelease/Press not working

--- src/main/java/com/github/synnerz/devonian/mixin/KeyboardHandlerMixin.java
+++ src/main/java/com/github/synnerz/devonian/mixin/KeyboardHandlerMixin.java
@@ -11,9 +11,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-import static org.lwjgl.sdl.SDLEvents.SDL_EVENT_KEY_DOWN;
-import static org.lwjgl.sdl.SDLEvents.SDL_EVENT_KEY_UP;
-
 @Mixin(KeyboardHandler.class)
 public class KeyboardHandlerMixin {
     @Shadow @Final private Minecraft minecraft;
@@ -26,11 +23,11 @@ public class KeyboardHandlerMixin {
         if (minecraft.gui.screen() != null || minecraft.level == null) return;
 
         switch (i) {
-            case SDL_EVENT_KEY_UP:
+            case 0:
                 new KeyReleaseEvent(keyEvent).post();
                 break;
 
-            case SDL_EVENT_KEY_DOWN:
+            case 1:
                 new KeyPressEvent(keyEvent).post();
                 break;
         }
